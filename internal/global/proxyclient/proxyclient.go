6c0f713bca13ae9ef0e813dc9075f9062a84862d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-06 21:10:21 +0800

    update proxyclient.Client

--- internal/global/proxyclient/proxyclient.go
+++ internal/global/proxyclient/proxyclient.go
@@ -5,27 +5,33 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/pelletier/go-toml"
+
 	"project/internal/proxy"
+	"project/internal/proxy/httpproxy"
+	"project/internal/proxy/socks5"
 )
 
 var (
 	ERR_RESERVE_PROXY = errors.New("direct is reserve proxy")
+	ERR_UNKNOWN_MODE  = errors.New("unknown mode")
 )
 
 type Client struct {
-	Mode   proxy.Mode
-	Config interface{}
+	Mode         proxy.Mode
+	Config       string // toml or other
+	proxy.Client `toml:"-"`
 }
 
 type PROXY struct {
-	clients map[string]proxy.Client // key = tag
+	clients map[string]*Client // key = tag
 	rwm     sync.RWMutex
 }
 
 // key = tag
 func New(c map[string]*Client) (*PROXY, error) {
 	p := &PROXY{
-		clients: make(map[string]proxy.Client),
+		clients: make(map[string]*Client),
 	}
 	for tag, client := range c {
 		err := p.Add(tag, client)
@@ -37,7 +43,7 @@ func New(c map[string]*Client) (*PROXY, error) {
 }
 
 // if tag = "" return Direct
-func (this *PROXY) Get(tag string) (proxy.Client, error) {
+func (this *PROXY) Get(tag string) (*Client, error) {
 	if tag == "" || tag == "direct" {
 		return nil, nil
 	}
@@ -50,8 +56,8 @@ func (this *PROXY) Get(tag string) (proxy.Client, error) {
 	}
 }
 
-func (this *PROXY) Clients() map[string]proxy.Client {
-	clients := make(map[string]proxy.Client)
+func (this *PROXY) Clients() map[string]*Client {
+	clients := make(map[string]*Client)
 	defer this.rwm.RUnlock()
 	this.rwm.RLock()
 	for tag, p := range this.clients {
@@ -64,14 +70,31 @@ func (this *PROXY) Add(tag string, c *Client) error {
 	if tag == "" || tag == "direct" {
 		return ERR_RESERVE_PROXY
 	}
-	client, err := proxy.Load_Client(c.Mode, c.Config)
-	if err != nil {
-		return err
+	switch c.Mode {
+	case proxy.SOCKS5:
+		var conf []*socks5.Config
+		err := toml.Unmarshal([]byte(c.Config), &conf)
+		if err != nil {
+			return err
+		}
+		pc, err := socks5.New_Client(conf...)
+		if err != nil {
+			return err
+		}
+		c.Client = pc
+	case proxy.HTTP:
+		pc, err := httpproxy.New_Client(c.Config)
+		if err != nil {
+			return err
+		}
+		c.Client = pc
+	default:
+		return ERR_UNKNOWN_MODE
 	}
 	defer this.rwm.Unlock()
 	this.rwm.Lock()
 	if _, exist := this.clients[tag]; !exist {
-		this.clients[tag] = client
+		this.clients[tag] = c
 		return nil
 	} else {
 		return fmt.Errorf("proxy: %s already exists", tag)
@@ -90,10 +113,4 @@ func (this *PROXY) Delete(tag string) error {
 	} else {
 		return fmt.Errorf("proxy: %s doesn't exist", tag)
 	}
-}
-
-func (this *PROXY) Destroy() {
-	this.rwm.Lock()
-	this.clients = make(map[string]proxy.Client)
-	this.rwm.Unlock()
 }
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -2,26 +2,16 @@ package proxy
 
 import (
 	"context"
-	"errors"
 	"net"
 	"net/http"
 	"time"
-
-	"project/internal/proxy/httpproxy"
-	"project/internal/proxy/socks5"
 )
 
-type Mode uint8
+type Mode string
 
 const (
-	SOCKS5 Mode = iota
-	HTTP
-)
-
-var (
-	ERR_UNKNOWN_MODE              = errors.New("unknown mode")
-	ERR_INVALID_SOCKS5_CONFIG     = errors.New("invalid socks5 config")
-	ERR_INVALID_HTTP_PROXY_CONFIG = errors.New("invalid http proxy config")
+	SOCKS5 Mode = "socks5"
+	HTTP   Mode = "http"
 )
 
 type Client interface {
@@ -30,27 +20,4 @@ type Client interface {
 	Dial_Timeout(network, address string, timeout time.Duration) (net.Conn, error)
 	HTTP(transport *http.Transport)
 	Info() string
-}
-
-type Server interface {
-	Start() error
-	Stop() error
-	Info() string
-}
-
-func Load_Client(mode Mode, config interface{}) (Client, error) {
-	switch mode {
-	case SOCKS5:
-		if config, ok := config.([]*socks5.Config); ok {
-			return socks5.New_Client(config...)
-		}
-		return nil, ERR_INVALID_SOCKS5_CONFIG
-	case HTTP:
-		if config, ok := config.(string); ok {
-			return httpproxy.New_Client(config)
-		}
-		return nil, ERR_INVALID_HTTP_PROXY_CONFIG
-	default:
-		return nil, ERR_UNKNOWN_MODE
-	}
 }
