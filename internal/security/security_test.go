495f737b44e435242747ca1ef349e24b7aafb890
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-07 20:56:38 +0800

    add simple test

---
+++ internal/security/security_test.go
@@ -0,0 +1,14 @@
+package security
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_Flush_Slice(t *testing.T) {
+	b := []byte{1, 2, 3, 4}
+	Flush_Slice(b)
+	if !bytes.Equal(b, bytes.Repeat([]byte{0}, 4)) {
+		t.Fatal("flush slice failed")
+	}
+}
