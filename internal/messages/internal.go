c261e17221872cd5fd9134adac2987904e629a8d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-22 22:18:37 +0800

    Listener add Tag

--- internal/messages/internal.go
+++ internal/messages/internal.go
@@ -8,11 +8,13 @@ import (
 )
 
 type Bootstrap struct {
+	Tag    string
 	Mode   bootstrap.Mode
 	Config []byte
 }
 
 type Listener struct {
+	Tag     string
 	Mode    xnet.Mode
 	Config  []byte
 	Timeout time.Duration // start timeout
