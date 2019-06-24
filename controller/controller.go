8ca4badaf7df7f0ca07667053af9e59281b2498c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-24 16:41:11 +0800

    add controller component

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,21 @@
 package controller
+
+type CONTROLLER struct {
+	config *Config
+}
+
+func New(c *Config) (*CONTROLLER, error) {
+	controller := &CONTROLLER{config: c}
+
+	return controller, nil
+}
+
+func (this *CONTROLLER) Main() error {
+
+	return nil
+}
+
+func (this *CONTROLLER) Exit() error {
+
+	return nil
+}
