2ee2b42a39182dbf71dc2d923390cb623e2e76ad
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-25 17:35:02 +0800

    update logger test

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -5,10 +5,10 @@ import (
 )
 
 func Test_Logger(t *testing.T) {
-	Test.Printf(DEBUG, "test src", "test format %s", "str")
-	Test.Print(DEBUG, "test src", "test print log")
-	Test.Println(DEBUG, "test src", "test println log")
-	Discard.Printf(DEBUG, "test src", "test format %s", "str")
-	Discard.Print(DEBUG, "test src", "test print log")
-	Discard.Println(DEBUG, "test src", "test println log")
+	Test.Printf(DEBUG, "test src", "test format %s", "test log")
+	Test.Print(DEBUG, "test src", "test print", "test log")
+	Test.Println(DEBUG, "test src", "test println", "test log")
+	Discard.Printf(DEBUG, "test src", "test format %s", "test log")
+	Discard.Print(DEBUG, "test src", "test print", "test log")
+	Discard.Println(DEBUG, "test src", "test println", "test log")
 }
