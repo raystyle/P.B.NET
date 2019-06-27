6fe0b542900254636335fd042d50f7ce07326fae
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-27 17:54:23 +0800

    add controller global & test

--- controller/global.go
+++ controller/global.go
@@ -2,33 +2,92 @@ package controller
 
 import (
 	"sync"
+	"time"
+
+	"github.com/pelletier/go-toml"
+	"github.com/pkg/errors"
 
 	"project/internal/global/dnsclient"
 	"project/internal/global/proxyclient"
 	"project/internal/global/timesync"
-	"project/internal/security"
 )
 
 type global struct {
-	ctx        *CONTROLLER
 	proxy      *proxyclient.PROXY
 	dns        *dnsclient.DNS
 	timesync   *timesync.TIMESYNC
 	object     map[uint32]interface{}
 	object_rwm sync.RWMutex
-	conf_err   error
-	conf_once  sync.Once
-	wg         sync.WaitGroup
 }
 
-func new_global(ctx *CONTROLLER) (*global, error) {
-	// db := ctx.database
-	// <security> basic
-	memory := security.New_Memory()
-	memory.Padding()
-
+func new_global(ctx *CTRL, c *Config) (*global, error) {
+	// load proxy clients
+	pcs, err := ctx.Select_Proxy_Client()
+	if err != nil {
+		return nil, errors.Wrap(err, "load proxy clients failed")
+	}
+	l := len(pcs)
+	tpcs := make(map[string]*proxyclient.Client, l)
+	for i := 0; i < l; i++ {
+		tpcs[pcs[i].Tag] = &proxyclient.Client{
+			Mode:   pcs[i].Mode,
+			Config: pcs[i].Config,
+		}
+	}
+	p, err := proxyclient.New(tpcs)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	// load dns clients
+	dcs, err := ctx.Select_DNS_Client()
+	if err != nil {
+		return nil, errors.Wrap(err, "load dns clients failed")
+	}
+	l = len(dcs)
+	tdcs := make(map[string]*dnsclient.Client, l)
+	for i := 0; i < l; i++ {
+		tdcs[dcs[i].Tag] = &dnsclient.Client{
+			Method:  dcs[i].Method,
+			Address: dcs[i].Address,
+		}
+	}
+	d, err := dnsclient.New(p, tdcs, c.DNS_Cache_Deadline)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	// load timesync
+	ts, err := ctx.Select_Timesync()
+	if err != nil {
+		return nil, errors.Wrap(err, "load timesync clients failed")
+	}
+	l = len(ts)
+	tts := make(map[string]*timesync.Client, l)
+	for i := 0; i < l; i++ {
+		c := &timesync.Client{}
+		err := toml.Unmarshal([]byte(ts[i].Config), c)
+		if err != nil {
+			return nil, errors.Wrapf(err, "load timesync: %s failed", ts[i].Tag)
+		}
+		tts[ts[i].Tag] = c
+	}
+	t, err := timesync.New(p, d, ctx, tts, c.Timesync_Interval)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
 	g := &global{
-		ctx: ctx,
+		proxy:    p,
+		dns:      d,
+		timesync: t,
+		object:   make(map[uint32]interface{}),
 	}
 	return g, nil
+}
+
+// about internal
+func (this *global) Start_Timesync() error {
+	return this.timesync.Start()
+}
+
+func (this *global) Now() time.Time {
+	return this.timesync.Now().Local()
 }
---
+++ controller/global_test.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_new_global(t *testing.T) {
+	ctrl := test_gen_ctrl(t)
+	global, err := new_global(ctrl, test_gen_config())
+	require.Nil(t, err, err)
+	err = global.Start_Timesync()
+	require.Nil(t, err, err)
+	t.Log("now: ", global.Now())
+}
