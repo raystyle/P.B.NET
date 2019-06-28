43fa7718b6f6a1f3b2c3b9aac479f720a2e773fd
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-06-28 22:43:25 +0800

    update controller config & test

--- controller/config.go
+++ controller/config.go
@@ -5,20 +5,20 @@ import (
 )
 
 type Config struct {
-	Log_Level string
+	Log_Level string `toml:"log_level"`
 
 	// global
-	DNS_Cache_Deadline time.Duration
-	Timesync_Interval  time.Duration
+	DNS_Cache_Deadline time.Duration `toml:"dns_cache_deadline"`
+	Timesync_Interval  time.Duration `toml:"timesync_interval"`
 
 	// database
-	Dialect           string // "mysql"
-	DSN               string
-	DB_Log_Path       string
-	DB_Max_Open_Conns int
-	DB_Max_Idle_Conn  int
-	GORM_Log_Path     string
-	GORM_Detailed_Log bool
+	Dialect           string `toml:"dialect"` // "mysql"
+	DSN               string `toml:"dsn"`
+	DB_Log_Path       string `toml:"db_log_path"`
+	DB_Max_Open_Conns int    `toml:"db_max_open_conns"`
+	DB_Max_Idle_Conns int    `toml:"db_max_idle_conns"`
+	GORM_Log_Path     string `toml:"gorm_log_path"`
+	GORM_Detailed_Log bool   `toml:"gorm_detailed_log"`
 }
 
 const object_key_max uint32 = 1048575
--- controller/config_test.go
+++ controller/config_test.go
@@ -16,10 +16,12 @@ func test_gen_config() *Config {
 		DNS_Cache_Deadline: 3 * time.Minute,
 		Timesync_Interval:  15 * time.Minute,
 		// database
-		Dialect:       test_dialect,
-		DSN:           test_dsn,
-		DB_Log_Path:   "../app/log/database.log",
-		GORM_Log_Path: "../app/log/gorm.log",
+		Dialect:           test_dialect,
+		DSN:               test_dsn,
+		DB_Max_Open_Conns: 16,
+		DB_Max_Idle_Conns: 16,
+		DB_Log_Path:       "../app/log/database.log",
+		GORM_Log_Path:     "../app/log/gorm.log",
 	}
 	return c
 }
