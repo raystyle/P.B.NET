a8ed76075475560f1be816ce631e6a45726db0b8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-07-09 22:10:20 +0800

    format note

--- internal/protocol/command.go
+++ internal/protocol/command.go
@@ -1,12 +1,9 @@
 package protocol
 
+// -----------------------Controller--------------------------
 const (
 	CTRL_HEARTBEAT uint8 = 0x00 + iota
-)
-
-//-----------------------Controller--------------------------
-const (
-	CTRL_REPLY uint8 = 0x10 + iota
+	CTRL_REPLY
 	CTRL_BROADCAST_TOKEN
 	CTRL_BROADCAST
 	CTRL_SYNC_SEND_TOKEN
@@ -17,27 +14,24 @@ const (
 	CTRL_SYNC_QUERY
 )
 
-//node authentication
+// node authentication
 const (
 	CTRL_QUERY_CERTIFICATE uint8 = 0x20 + iota
 	CTRL_TRUST_NODE_REQUEST
 	CTRL_TRUST_NODE_DATA
 )
 
-//query nodes
+// query nodes
 const (
 	CTRL_QUERY_GUID uint8 = 0x30 + iota
 	CTRL_QUERY_STATUS
 	CTRL_QUERY_ALL_NODES
 )
 
+// --------------------------Node-----------------------------
 const (
 	NODE_HEARTBEAT uint8 = 0x00 + iota
-)
-
-//--------------------------Node-----------------------------
-const (
-	NODE_REPLY uint8 = 0x10 + iota
+	NODE_REPLY
 	NODE_BROADCAST_TOKEN
 	NODE_BROADCAST
 	NODE_SYNC_SEND_TOKEN
@@ -48,12 +42,12 @@ const (
 	NODE_SYNC_QUERY
 )
 
-//node authentication
+// node authentication
 const (
 	NODE_QUERY_CERTIFICATE uint8 = 0x20 + iota
 )
 
-//query nodes
+// query nodes
 const (
 	NODE_QUERY_GUID uint8 = 0x30 + iota
 	NODE_QUERY_ALL_NODES
