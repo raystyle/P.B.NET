07c9cf6e027251716efed24f7648174362e17684
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-09-05 17:04:36 +0800

    proxy: add Pool, remove global/proxyclient

---
+++ internal/proxy/pool.go
@@ -0,0 +1,138 @@
+package proxy
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/pelletier/go-toml"
+
+	"project/internal/proxy/httpproxy"
+	"project/internal/proxy/socks5"
+)
+
+type Mode = string
+
+const (
+	Socks5 Mode = "socks5"
+	HTTP   Mode = "http"
+)
+
+var (
+	ErrReserveProxy = errors.New("direct is reserve proxy")
+	ErrUnknownMode  = errors.New("unknown mode")
+)
+
+type client interface {
+	Dial(network, address string) (net.Conn, error)
+	DialContext(ctx context.Context, network, address string) (net.Conn, error)
+	DialTimeout(network, address string, timeout time.Duration) (net.Conn, error)
+	HTTP(transport *http.Transport)
+	Info() string
+}
+
+type Client struct {
+	Mode   Mode   `toml:"mode"`
+	Config string `toml:"config"` // toml or other, see proxy/*
+	client
+}
+
+type Pool struct {
+	clients map[string]*Client // key = tag
+	rwm     sync.RWMutex
+}
+
+// NewPool is for x.global
+func NewPool(c map[string]*Client) (*Pool, error) {
+	p := &Pool{
+		clients: make(map[string]*Client),
+	}
+	for tag, client := range c {
+		err := p.Add(tag, client)
+		if err != nil {
+			return nil, fmt.Errorf("add proxy client %s failed: %s", tag, err)
+		}
+	}
+	return p, nil
+}
+
+// if tag = "" return Direct
+func (p *Pool) Get(tag string) (*Client, error) {
+	if tag == "" || tag == "direct" {
+		return nil, nil
+	}
+	p.rwm.RLock()
+	defer p.rwm.RUnlock()
+	if p, exist := p.clients[tag]; exist {
+		return p, nil
+	} else {
+		return nil, fmt.Errorf("proxy client: %s doesn't exist", tag)
+	}
+}
+
+func (p *Pool) Clients() map[string]*Client {
+	clients := make(map[string]*Client)
+	p.rwm.RLock()
+	for tag, client := range p.clients {
+		clients[tag] = client
+	}
+	p.rwm.RUnlock()
+	return clients
+}
+
+func (p *Pool) Add(tag string, client *Client) error {
+	if tag == "" || tag == "direct" {
+		return ErrReserveProxy
+	}
+	switch client.Mode {
+	case Socks5:
+		conf := &struct {
+			Clients []*socks5.Config
+		}{}
+		err := toml.Unmarshal([]byte(client.Config), conf)
+		if err != nil {
+			return err
+		}
+		pc, err := socks5.NewClient(conf.Clients...)
+		if err != nil {
+			return err
+		}
+		client.client = pc
+	case HTTP:
+		pc, err := httpproxy.NewClient(client.Config)
+		if err != nil {
+			return err
+		}
+		client.client = pc
+	default:
+		return ErrUnknownMode
+	}
+	// <security> set client config to ""
+	client.Config = ""
+	p.rwm.Lock()
+	defer p.rwm.Unlock()
+	if _, exist := p.clients[tag]; !exist {
+		p.clients[tag] = client
+		return nil
+	} else {
+		return fmt.Errorf("proxy: %s already exists", tag)
+	}
+}
+
+func (p *Pool) Delete(tag string) error {
+	if tag == "" || tag == "direct" {
+		return ErrReserveProxy
+	}
+	p.rwm.Lock()
+	defer p.rwm.Unlock()
+	if _, exist := p.clients[tag]; exist {
+		delete(p.clients, tag)
+		return nil
+	} else {
+		return fmt.Errorf("proxy: %s doesn't exist", tag)
+	}
+}
--- internal/proxy/pool_test.go
+++ internal/proxy/pool_test.go
@@ -1,1 +1,88 @@
 package proxy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	tagSocks5 = "test_socks5_client"
+	tagHTTP   = "test_http_proxy_client"
+)
+
+func TestPool(t *testing.T) {
+	clients := make(map[string]*Client)
+	// create socks5 client config(s5c)
+	s5c := `
+        [[Clients]]
+          Address = "localhost:0"
+          Network = "tcp"
+          Password = "123456"
+          Username = "admin"
+        
+        [[Clients]]
+          Address = "localhost:0"
+          Network = "tcp"
+          Password = "123456"
+          Username = "admin"
+`
+	clients[tagSocks5] = &Client{
+		Mode:   Socks5,
+		Config: s5c,
+	}
+	clients[tagHTTP] = &Client{
+		Mode:   HTTP,
+		Config: "http://admin:123456@localhost:0",
+	}
+	// make
+	pool, err := NewPool(clients)
+	require.NoError(t, err)
+	// get
+	pc, err := pool.Get(tagSocks5)
+	require.NoError(t, err)
+	require.NotNil(t, pc)
+	// get nil
+	pc, err = pool.Get("")
+	require.NoError(t, err)
+	require.Nil(t, pc)
+	// get failed
+	pc, err = pool.Get("not exists")
+	require.Error(t, err)
+	require.Nil(t, pc)
+	// list
+	for k := range pool.Clients() {
+		t.Log("client:", k)
+	}
+	// add reserve
+	err = pool.Add("", nil)
+	require.Equal(t, ErrReserveProxy, err)
+	// add exist
+	err = pool.Add(tagSocks5, &Client{
+		Mode:   Socks5,
+		Config: s5c},
+	)
+	require.Error(t, err)
+	// unknown mode
+	err = pool.Add("unknown mode", &Client{
+		Mode:   "unknown mode",
+		Config: s5c},
+	)
+	require.Equal(t, ErrUnknownMode, err)
+	// delete
+	err = pool.Delete(tagHTTP)
+	require.NoError(t, err)
+	// delete reserve
+	err = pool.Delete("")
+	require.Equal(t, ErrReserveProxy, err)
+	// delete doesn't exist
+	err = pool.Delete(tagHTTP)
+	require.Error(t, err)
+	// New failed == add failed
+	clients[tagSocks5] = &Client{
+		Mode:   Socks5,
+		Config: "invalid toml config"}
+	pool, err = NewPool(clients)
+	require.Error(t, err)
+	require.Nil(t, pool)
+}
