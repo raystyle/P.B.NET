e159781d082a8d87bcec545a82bf530e8e078ee6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-09-06 15:01:20 +0800

    dns/client: move global/dnsclient to here and change function signature

---
+++ internal/dns/client.go
@@ -0,0 +1,224 @@
+package dns
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"project/internal/options"
+	"project/internal/proxy"
+)
+
+type Mode string
+
+const (
+	Custom Mode = "custom"
+	System Mode = "system"
+)
+
+// resolve method
+type Method = string
+
+const (
+	UDP Method = "udp"
+	TCP Method = "tcp"
+	TLS Method = "tls" // DNS-Over-TLS
+	DOH Method = "doh" // DNS-Over-HTTPS
+)
+
+var (
+	ErrUnknownMode = errors.New("unknown mode")
+)
+
+type Server struct {
+	Method  Method `toml:"method"`
+	Address string `toml:"address"`
+}
+
+type Client struct {
+	pool       *proxy.Pool
+	deadline   time.Duration      // cache expire time
+	servers    map[string]*Server // key = tag
+	serversRWM sync.RWMutex
+	caches     map[string]*cache // key = domain name
+	cachesRWM  sync.RWMutex
+}
+
+func NewClient(p *proxy.Pool, s map[string]*Server, deadline time.Duration) (*Client, error) {
+	client := Client{
+		pool:    p,
+		servers: make(map[string]*Server),
+		caches:  make(map[string]*cache),
+	}
+	// add clients
+	for tag, server := range s {
+		err := client.Add(tag, server)
+		if err != nil {
+			return nil, fmt.Errorf("add dns server %s failed: %s", tag, err)
+		}
+	}
+	// set deadline
+	if deadline < 1 {
+		deadline = defaultCacheDeadline
+	}
+	err := client.SetCacheDeadline(deadline)
+	if err != nil {
+		return nil, err
+	}
+	return &client, nil
+}
+
+// TODO test
+func (c *Client) Test() error {
+	return nil
+}
+
+type Options struct {
+	Mode      Mode                  `toml:"mode"`   // default is custom
+	ServerTag string                `toml:"tag"`    // if != "" use selected dns client
+	ProxyTag  string                `toml:"proxy"`  // proxy tag
+	Type      Type                  `toml:"type"`   // default is IPv4
+	Method    Method                `toml:"method"` // default is UDP if != "" ignore it
+	Network   string                `toml:"network"`
+	Timeout   time.Duration         `toml:"timeout"`
+	Header    http.Header           `toml:"header"`    // about DOH
+	Transport options.HTTPTransport `toml:"transport"` // about DOH
+
+	dial      func(network, address string) (net.Conn, error) // for proxy, useless for doh
+	transport *http.Transport                                 // about DOH
+}
+
+// select custom or system to resolve dns
+// set domain & options
+func (c *Client) Resolve(domain string, opts *Options) ([]string, error) {
+	if opts == nil {
+		opts = new(Options)
+	}
+	_type := opts.Type
+	if _type == "" {
+		_type = IPv4
+	}
+	// first query caches
+	cache := c.queryCache(domain, _type)
+	if cache != nil {
+		return cache, nil
+	}
+	var (
+		result []string
+		err    error
+	)
+	switch opts.Mode {
+	case "", Custom:
+		// apply doh options(http Transport)
+		if opts.Method == DOH {
+			opts.transport, err = opts.Transport.Apply()
+			if err != nil {
+				return nil, err
+			}
+		}
+		// set proxy
+		p, err := c.pool.Get(opts.ProxyTag)
+		if err != nil {
+			return nil, err
+		}
+		if p != nil {
+			switch opts.Method {
+			case "", UDP, TCP, TLS:
+				opts.dial = p.Dial
+			case DOH:
+				p.HTTP(opts.transport)
+			default:
+				return nil, ErrUnknownMethod
+			}
+		}
+		// check tag exist
+		if opts.ServerTag != "" {
+			c.serversRWM.RLock()
+			defer c.serversRWM.RUnlock()
+			if server, ok := c.servers[opts.ServerTag]; !ok {
+				return nil, fmt.Errorf("dns server: %s doesn't exist", opts.ServerTag)
+			} else {
+				opts.Method = server.Method
+				return resolve(server.Address, domain, opts)
+			}
+		}
+		// query dns
+		_method := opts.Method
+		if _method == "" {
+			_method = defaultMethod
+		}
+		// copy map
+		servers := make(map[string]*Server)
+		c.serversRWM.RLock()
+		for tag, server := range c.servers {
+			servers[tag] = server
+		}
+		c.serversRWM.RUnlock()
+		for _, server := range servers {
+			if server.Method == _method {
+				result, err = resolve(server.Address, domain, opts)
+				if err == nil {
+					break
+				}
+			}
+		}
+	case System:
+		result, err = systemResolve(domain, _type)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, ErrUnknownMode
+	}
+	// update cache
+	if result != nil {
+		switch _type {
+		case IPv4:
+			c.updateCache(domain, result, nil)
+		case IPv6:
+			c.updateCache(domain, nil, result)
+		}
+		return result, nil
+	}
+	return nil, ErrNoResolveResult
+}
+
+func (c *Client) Servers() map[string]*Server {
+	servers := make(map[string]*Server)
+	c.serversRWM.RLock()
+	for tag, server := range c.servers {
+		servers[tag] = server
+	}
+	c.serversRWM.RUnlock()
+	return servers
+}
+
+func (c *Client) Add(tag string, server *Server) error {
+	switch server.Method {
+	case UDP, TCP, TLS, DOH:
+	default:
+		return ErrUnknownMethod
+	}
+	c.serversRWM.Lock()
+	defer c.serversRWM.Unlock()
+	if _, ok := c.servers[tag]; !ok {
+		c.servers[tag] = server
+		return nil
+	} else {
+		return fmt.Errorf("dns server: %s already exists", tag)
+	}
+}
+
+func (c *Client) Delete(tag string) error {
+	c.serversRWM.Lock()
+	defer c.serversRWM.Unlock()
+	if _, ok := c.servers[tag]; ok {
+		delete(c.servers, tag)
+		return nil
+	} else {
+		return fmt.Errorf("dns server: %s doesn't exist", tag)
+	}
+}
---
+++ internal/dns/client_test.go
@@ -0,0 +1,179 @@
+package dns
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/logger"
+	"project/internal/proxy"
+	"project/internal/proxy/httpproxy"
+	"project/internal/proxy/socks5"
+)
+
+const (
+	proxySocks5 = "test_socks5_client"
+	proxyHTTP   = "test_http_proxy_client"
+)
+
+func TestClient(t *testing.T) {
+	// start socks5 proxy server(s5s)
+	s5sOpts := &socks5.Options{
+		Username: "admin",
+		Password: "123456",
+	}
+	s5s, err := socks5.NewServer("test_socks5", logger.Test, s5sOpts)
+	require.NoError(t, err)
+	err = s5s.ListenAndServe("localhost:0", 0)
+	require.NoError(t, err)
+	defer func() {
+		err = s5s.Stop()
+		require.NoError(t, err)
+	}()
+	// start http proxy server(hps)
+	hpsOpts := &httpproxy.Options{
+		Username: "admin",
+		Password: "123456",
+	}
+	hps, err := httpproxy.NewServer("test_http_proxy", logger.Test, hpsOpts)
+	require.NoError(t, err)
+	err = hps.ListenAndServe("localhost:0", 0)
+	require.NoError(t, err)
+	defer func() {
+		err = hps.Stop()
+		require.NoError(t, err)
+	}()
+	// create proxy clients
+	proxyClients := make(map[string]*proxy.Client)
+	// socks5
+	_, port, err := net.SplitHostPort(hps.Addr())
+	require.NoError(t, err)
+	proxyClients[proxySocks5] = &proxy.Client{
+		Mode: proxy.Socks5,
+		Config: `
+        [[Clients]]
+          Address = "localhost:` + port + `"
+          Network = "tcp"
+          Password = "123456"
+          Username = "admin"
+    `}
+	// http
+	_, port, err = net.SplitHostPort(hps.Addr())
+	require.NoError(t, err)
+	proxyClients[proxyHTTP] = &proxy.Client{
+		Mode:   proxy.HTTP,
+		Config: "http://admin:123456@localhost:" + port,
+	}
+	// make proxy pool
+	pool, err := proxy.NewPool(proxyClients)
+	require.NoError(t, err)
+	// create dns servers
+	servers := make(map[string]*Server)
+	add := func(tag string, method Method, address string) {
+		servers[tag] = &Server{
+			Method:  method,
+			Address: address,
+		}
+	}
+	// google
+	add("udp_google", UDP, "8.8.8.8:53")
+	add("tcp_google", TCP, "8.8.8.8:53")
+	add("tls_google_domain", TLS, "dns.google:853|8.8.8.8,8.8.4.4")
+	// cloudflare
+	add("udp_cloudflare", UDP, "1.0.0.1:53")
+	add("tcp_cloudflare_ipv6", TCP, "[2606:4700:4700::1001]:53")
+	add("tls_cloudflare_domain", TLS, "cloudflare-dns.com:853|1.0.0.1")
+	// doh
+	add("doh_mozilla", DOH, "https://mozilla.cloudflare-dns.com/dns-query")
+	// make dns client
+	client, err := NewClient(pool, servers, 0)
+	require.NoError(t, err)
+	// resolve with default options
+	ipList, err := client.Resolve(domain, nil)
+	require.NoError(t, err)
+	t.Log("use default options", ipList)
+	// resolve with tag
+	opts := Options{ServerTag: "tcp_google"}
+	ipList, err = client.Resolve(domain, &opts)
+	require.NoError(t, err)
+	t.Log("with tag", ipList)
+	// client.FlushCache()
+}
+
+/*
+	//ipv4
+	opt := &dns.Options{
+		Method:  dns.TLS,
+		Network: "tcp",
+		Type:    dns.IPv4,
+		Timeout: time.Second * 2,
+	}
+	ip_list, err := client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("TLS", ip_list)
+	opt.Method = dns.TCP
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("TCP", ip_list)
+	opt.Method = dns.UDP
+	opt.Network = "udp"
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("UDP", ip_list)
+	//ipv6
+	opt = &dns.Options{
+		Method:  dns.TLS,
+		Network: "tcp",
+		Type:    dns.IPv6,
+		Timeout: time.Second * 2,
+	}
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("TLS", ip_list)
+	opt.Method = dns.TCP
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("TCP", ip_list)
+	opt.Method = dns.UDP
+	opt.Network = "udp"
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("UDP", ip_list)
+	//invalid deadline
+	_, err = New(-1, nil)
+	require.NoError(t, err)
+	//not exist domain
+	opt.Method = dns.TCP
+	opt.Network = "tcp"
+	ip_list, err = client_pool.Resolve("asdasdasf1561asf651af.com", opt)
+	require.Equal(t, err, dns.ErrNoResolveResult, err)
+	//flush cache
+	client_pool.FlushCache()
+	opt.Method = dns.TLS
+	opt.Network = "tcp"
+	opt.Type = dns.IPv6
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("flush cache IPv6", ip_list)
+	opt.Type = dns.IPv4
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("flush cache IPv4", ip_list)
+	//deadline
+	require.NotNil(t, client_pool.SetCacheDeadline(-1))
+	require.Nil(t, client_pool.SetCacheDeadline(1))
+	time.Sleep(time.Second * 2)
+	ip_list, err = client_pool.Resolve(domain, opt)
+	require.NoError(t, err)
+	t.Log("cache deadline IPv4", ip_list)
+	//update cache
+	client_pool.update_cache("xxx.com", nil, nil)
+	//delete
+	for tag := range pool {
+		err = client_pool.Delete(tag)
+		require.NoError(t, err)
+		err = client_pool.Delete(tag)
+		require.NoError(t, err)
+	}
+*/
