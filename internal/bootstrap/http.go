ae8ba0dc3a0749f8b3a9216afba6d45a0c5ed173
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-09-07 11:09:57 +0800

    bootstrap/http.go: refactor function signature, improve test

--- internal/bootstrap/http.go
+++ internal/bootstrap/http.go
@@ -10,182 +10,180 @@ import (
 	"time"
 
 	"github.com/pelletier/go-toml"
-	"github.com/vmihailenco/msgpack"
+	"github.com/vmihailenco/msgpack/v4"
 
 	"project/internal/convert"
 	"project/internal/crypto/aes"
 	"project/internal/crypto/ed25519"
 	"project/internal/dns"
-	"project/internal/global/dnsclient"
 	"project/internal/options"
 	"project/internal/random"
 	"project/internal/security"
 )
 
 var (
-	ERR_NO_RESPONSE            = errors.New("no response")
-	ERR_INVALID_HEADER         = errors.New("invalid signature header")
-	ERR_INVALID_SIGNATURE_SIZE = errors.New("invalid signature size")
-	ERR_INVALID_SIGNATURE      = errors.New("invalid signature")
+	ErrNoResponse           = errors.New("no response")
+	ErrInvalidHeader        = errors.New("invalid signature header")
+	ErrInvalidSignatureSize = errors.New("invalid signature size")
+	ErrInvalidSignature     = errors.New("invalid signature")
 )
 
 type HTTP struct {
-	Request   options.HTTP_Request   `toml:"request"`
-	Transport options.HTTP_Transport `toml:"transport"`
-	Timeout   time.Duration          `toml:"timeout"`
-	Proxy     string                 `toml:"proxy"`
-	DNS_Opts  dnsclient.Options      `toml:"dnsclient"`
+	Request   options.HTTPRequest   `toml:"request"`
+	Transport options.HTTPTransport `toml:"transport"`
+	Timeout   time.Duration         `toml:"timeout"`
+	Proxy     string                `toml:"proxy"`
+	DNSOpts   dns.Options           `toml:"dns_options"`
 
 	// encrypt&decrypt generate data(nodes) hex
-	AES_Key string `toml:"aes_key"`
-	AES_IV  string `toml:"aes_iv"`
+	AESKey string `toml:"aes_key"`
+	AESIV  string `toml:"aes_iv"`
 
 	// for resolve verify  hex
 	PublicKey string `toml:"publickey"`
 
 	// for generate&marshal
-	PrivateKey ed25519.PrivateKey `toml:"-"`
+	PrivateKey ed25519.PrivateKey `toml:"-"` // <security>
 
 	// runtime
-	proxy    proxy_pool
-	resolver dns_resolver
+	proxy    proxyPool
+	resolver dnsResolver
 
 	// self encrypt all options
-	opts_enc []byte
-	cryptor  *aes.CBC_Cryptor
+	optsEnc []byte
+	cbc     *aes.CBC
 }
 
-func New_HTTP(p proxy_pool, d dns_resolver) *HTTP {
+func NewHTTP(p proxyPool, r dnsResolver) *HTTP {
 	return &HTTP{
-		resolver: d,
+		resolver: r,
 		proxy:    p,
 	}
 }
 
-func (this *HTTP) Validate() error {
-	_, err := this.Request.Apply()
+func (h *HTTP) Validate() error {
+	_, err := h.Request.Apply()
 	if err != nil {
 		return err
 	}
-	_, err = this.Transport.Apply()
+	_, err = h.Transport.Apply()
 	if err != nil {
 		return err
 	}
-	aes_key, err := hex.DecodeString(this.AES_Key)
+	aesKey, err := hex.DecodeString(h.AESKey)
 	if err != nil {
 		return err
 	}
-	aes_iv, err := hex.DecodeString(this.AES_IV)
+	aesIV, err := hex.DecodeString(h.AESIV)
 	if err != nil {
 		return err
 	}
-	_, err = aes.New_CBC_Cryptor(aes_key, aes_iv)
-	security.Flush_Bytes(aes_key)
-	security.Flush_Bytes(aes_iv)
+	_, err = aes.NewCBC(aesKey, aesIV)
+	security.FlushBytes(aesKey)
+	security.FlushBytes(aesIV)
 	return err
 }
 
-func (this *HTTP) Generate(nodes []*Node) (string, error) {
-	err := this.Validate()
+func (h *HTTP) Generate(nodes []*Node) (string, error) {
+	err := h.Validate()
 	if err != nil {
 		return "", err
 	}
 	data, err := msgpack.Marshal(nodes)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
 	// confuse
-	nodes_data := bytes.Buffer{}
-	generator := random.New()
+	nodesData := bytes.Buffer{}
+	generator := random.New(0)
 	i := 0
 	for i = 4; i < len(data); i += 4 {
-		nodes_data.Write(generator.Bytes(8))
-		nodes_data.Write(data[i-4 : i])
+		nodesData.Write(generator.Bytes(8))
+		nodesData.Write(data[i-4 : i])
 	}
 	end := data[i-4:]
 	if end != nil {
-		nodes_data.Write(generator.Bytes(8))
-		nodes_data.Write(end)
+		nodesData.Write(generator.Bytes(8))
+		nodesData.Write(end)
 	}
 	// sign
-	signature := ed25519.Sign(this.PrivateKey, nodes_data.Bytes())
+	signature := ed25519.Sign(h.PrivateKey, nodesData.Bytes())
 	buffer := bytes.Buffer{}
-	// signature size + signature(nodes_data) + nodes_data
-	buffer.Write(convert.Uint16_Bytes(uint16(len(signature))))
+	// signature size + signature(nodesData) + nodesData
+	buffer.Write(convert.Uint16ToBytes(uint16(len(signature))))
 	buffer.Write(signature)
-	buffer.Write(nodes_data.Bytes())
+	buffer.Write(nodesData.Bytes())
 	// encrypt
-	key, err := hex.DecodeString(this.AES_Key)
+	key, err := hex.DecodeString(h.AESKey)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	iv, err := hex.DecodeString(this.AES_IV)
+	iv, err := hex.DecodeString(h.AESIV)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	cipherdata, err := aes.CBC_Encrypt(buffer.Bytes(), key, iv)
+	cipherdata, err := aes.CBCEncrypt(buffer.Bytes(), key, iv)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
 	return base64.StdEncoding.EncodeToString(cipherdata), nil
 }
 
-func (this *HTTP) Marshal() ([]byte, error) {
-	err := this.Validate()
+func (h *HTTP) Marshal() ([]byte, error) {
+	err := h.Validate()
 	if err != nil {
 		return nil, err
 	}
-	publickey := this.PrivateKey.PublicKey()
-	this.PublicKey = hex.EncodeToString(publickey)
-	return toml.Marshal(this)
+	publickey := h.PrivateKey.PublicKey()
+	h.PublicKey = hex.EncodeToString(publickey)
+	return toml.Marshal(h)
 }
 
-func (this *HTTP) Unmarshal(data []byte) error {
-	h := &HTTP{}
-	err := toml.Unmarshal(data, h)
+func (h *HTTP) Unmarshal(data []byte) error {
+	tempHTTP := &HTTP{}
+	err := toml.Unmarshal(data, tempHTTP)
 	if err != nil {
 		return err
 	}
-	err = h.Validate()
+	err = tempHTTP.Validate()
 	if err != nil {
 		return err
 	}
 	// encrypt all options
-	memory := security.New_Memory()
+	memory := security.NewMemory()
 	defer memory.Flush()
-	rand := random.New()
-	key := rand.Bytes(aes.BIT256)
-	iv := rand.Bytes(aes.IV_SIZE)
-	this.cryptor, err = aes.New_CBC_Cryptor(key, iv)
+	rand := random.New(0)
+	key := rand.Bytes(aes.Bit256)
+	iv := rand.Bytes(aes.IVSize)
+	h.cbc, err = aes.NewCBC(key, iv)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	security.Flush_Bytes(key)
-	security.Flush_Bytes(iv)
+	security.FlushBytes(key)
+	security.FlushBytes(iv)
 	memory.Padding()
-	b, err := msgpack.Marshal(h)
+	b, err := msgpack.Marshal(tempHTTP)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	h = nil // <security>
+	tempHTTP = nil // <security>
 	memory.Padding()
-	this.opts_enc, err = this.cryptor.Encrypt(b)
+	h.optsEnc, err = h.cbc.Encrypt(b)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	security.Flush_Bytes(b)
+	security.FlushBytes(b)
 	return nil
 }
 
-func (this *HTTP) Resolve() ([]*Node, error) {
-	opts, err := this.apply_options()
+func (h *HTTP) Resolve() ([]*Node, error) {
+	opts, err := h.applyOptions()
 	if err != nil {
 		return nil, err
 	}
 	// dns
 	hostname := opts.req.URL.Hostname()
-	dns_opts := &opts.h.DNS_Opts
-	ip_list, err := this.resolver.Resolve(hostname, dns_opts)
+	ipList, err := h.resolver.Resolve(hostname, &opts.h.DNSOpts)
 	if err != nil {
 		return nil, err
 	}
@@ -198,79 +196,79 @@ func (this *HTTP) Resolve() ([]*Node, error) {
 	if port != "" {
 		port = ":" + port
 	}
-	switch dns_opts.Type {
-	case "", dns.IPV4:
-		for i := 0; i < len(ip_list); i++ {
-			opts.req.URL.Host = ip_list[i] + port
-			info, err := this.do(opts.req, opts.hc)
+	switch opts.h.DNSOpts.Type {
+	case "", dns.IPv4:
+		for i := 0; i < len(ipList); i++ {
+			opts.req.URL.Host = ipList[i] + port
+			info, err := do(opts.req, opts.hc)
 			if err == nil {
-				return this.resolve(opts.h, info)
+				return resolve(opts.h, info)
 			}
 		}
-	case dns.IPV6:
-		for i := 0; i < len(ip_list); i++ {
-			opts.req.URL.Host = "[" + ip_list[i] + "]" + port
-			info, err := this.do(opts.req, opts.hc)
+	case dns.IPv6:
+		for i := 0; i < len(ipList); i++ {
+			opts.req.URL.Host = "[" + ipList[i] + "]" + port
+			info, err := do(opts.req, opts.hc)
 			if err == nil {
-				return this.resolve(opts.h, info)
+				return resolve(opts.h, info)
 			}
 		}
 	default:
-		panic(&fpanic{M: M_HTTP, E: dns.ERR_INVALID_TYPE})
+		panic(&fPanic{Mode: ModeHTTP, Err: dns.ErrInvalidType})
 	}
-	return nil, ERR_NO_RESPONSE
+	return nil, ErrNoResponse
 }
 
-type http_opts struct {
+type httpOpts struct {
 	req *http.Request
 	hc  *http.Client
 	h   *HTTP
 }
 
-func (this *HTTP) apply_options() (*http_opts, error) {
+func (h *HTTP) applyOptions() (*httpOpts, error) {
 	// decrypt all options
-	memory := security.New_Memory()
+	memory := security.NewMemory()
 	defer memory.Flush()
-	b, err := this.cryptor.Decrypt(this.opts_enc)
+	b, err := h.cbc.Decrypt(h.optsEnc)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	h := &HTTP{}
-	err = msgpack.Unmarshal(b, h)
+	tempHTTP := &HTTP{}
+	err = msgpack.Unmarshal(b, tempHTTP)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	security.Flush_Bytes(b)
+	security.FlushBytes(b)
 	memory.Padding()
 	// apply options
-	req, err := h.Request.Apply()
+	req, err := tempHTTP.Request.Apply()
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	tr, err := h.Transport.Apply()
+	tr, err := tempHTTP.Transport.Apply()
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
 	tr.TLSClientConfig.ServerName = req.URL.Hostname()
 	// set proxy
-	proxy, err := this.proxy.Get(this.Proxy)
+	proxy, err := h.proxy.Get(h.Proxy)
 	if err != nil {
 		return nil, err
 	}
 	if proxy != nil {
 		proxy.HTTP(tr)
 	}
-	return &http_opts{
+	return &httpOpts{
 		req: req,
 		hc: &http.Client{
 			Transport: tr,
-			Timeout:   h.Timeout,
+			Timeout:   tempHTTP.Timeout,
 		},
-		h: h,
+		h: tempHTTP,
 	}, nil
 }
 
-func (this *HTTP) do(req *http.Request, c *http.Client) (string, error) {
+func do(req *http.Request, c *http.Client) (string, error) {
 	resp, err := c.Do(req)
 	if err != nil {
 		return "", err
@@ -286,72 +284,72 @@ func (this *HTTP) do(req *http.Request, c *http.Client) (string, error) {
 	return string(b), nil
 }
 
-func (this *HTTP) resolve(h *HTTP, info string) ([]*Node, error) {
+func resolve(h *HTTP, info string) ([]*Node, error) {
 	cipherdata, err := base64.StdEncoding.DecodeString(info)
 	if err != nil {
 		return nil, err
 	}
-	aes_key, err := hex.DecodeString(h.AES_Key)
+	aesKey, err := hex.DecodeString(h.AESKey)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	h.AES_Key = ""
-	aes_iv, err := hex.DecodeString(h.AES_IV)
+	h.AESKey = ""
+	aesIV, err := hex.DecodeString(h.AESIV)
 	if err != nil {
-		panic(&fpanic{M: M_HTTP, E: err})
+		panic(&fPanic{Mode: ModeHTTP, Err: err})
 	}
-	h.AES_IV = ""
-	data, err := aes.CBC_Decrypt(cipherdata, aes_key, aes_iv)
+	h.AESIV = ""
+	data, err := aes.CBCDecrypt(cipherdata, aesKey, aesIV)
 	if err != nil {
 		return nil, err
 	}
-	security.Flush_Bytes(aes_key)
-	security.Flush_Bytes(aes_iv)
-	// signature size + signature(nodes_data) + nodes_data
+	security.FlushBytes(aesKey)
+	security.FlushBytes(aesIV)
+	// signature size + signature(nodesData) + nodesData
 	l := len(data)
 	if l < 2 {
-		return nil, ERR_INVALID_HEADER
+		return nil, ErrInvalidHeader
 	}
-	signature_size := int(convert.Bytes_Uint16(data[:2]))
-	if l < 2+signature_size {
-		return nil, ERR_INVALID_SIGNATURE_SIZE
+	signatureSize := int(convert.BytesToUint16(data[:2]))
+	if l < 2+signatureSize {
+		return nil, ErrInvalidSignatureSize
 	}
-	signature := data[2 : 2+signature_size]
-	nodes_data := data[2+signature_size:]
+	signature := data[2 : 2+signatureSize]
+	nodesData := data[2+signatureSize:]
 	// verify
 	pub, err := hex.DecodeString(h.PublicKey)
 	if err != nil {
 		return nil, err
 	}
 	h.PublicKey = ""
-	publickey, err := ed25519.Import_PublicKey(pub)
+	publickey, err := ed25519.ImportPublicKey(pub)
 	if err != nil {
 		return nil, err
 	}
-	if !ed25519.Verify(publickey, nodes_data, signature) {
-		return nil, ERR_INVALID_SIGNATURE
+	if !ed25519.Verify(publickey, nodesData, signature) {
+		return nil, ErrInvalidSignature
 	}
-	security.Flush_Bytes(pub)
-	// deconfuse
-	nodes_buffer := bytes.Buffer{}
-	l = len(nodes_data)
+	security.FlushBytes(pub)
+	// confuse
+	nodesBuffer := bytes.Buffer{}
+	l = len(nodesData)
 	i := 0
 	for i = 0; i < l; i += 12 {
-		if len(nodes_data[i:]) > 11 {
-			nodes_buffer.Write(nodes_data[i+8 : i+12])
+		if len(nodesData[i:]) > 11 {
+			nodesBuffer.Write(nodesData[i+8 : i+12])
 		}
 	}
 	if i != l {
-		if len(nodes_data[i-12:]) > 8 {
-			nodes_buffer.Write(nodes_data[i-4:]) // i+8-12
+		if len(nodesData[i-12:]) > 8 {
+			nodesBuffer.Write(nodesData[i-4:]) // i+8-12
 		}
 	}
-	nodes_bytes := nodes_buffer.Bytes()
+	nodesBytes := nodesBuffer.Bytes()
 	var nodes []*Node
-	err = msgpack.Unmarshal(nodes_bytes, &nodes)
+	err = msgpack.Unmarshal(nodesBytes, &nodes)
 	if err != nil {
 		return nil, err
 	}
-	security.Flush_Bytes(nodes_bytes)
+	security.FlushBytes(nodesBytes)
 	return nodes, nil
 }
--- internal/bootstrap/http_test.go
+++ internal/bootstrap/http_test.go
@@ -2,91 +2,122 @@ package bootstrap
 
 import (
 	"crypto/tls"
+	"net"
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"project/internal/crypto/aes"
 	"project/internal/crypto/cert"
 	"project/internal/crypto/ed25519"
-	"project/internal/dns"
 )
 
-func Test_HTTP(t *testing.T) {
+func TestHTTP(t *testing.T) {
 	// init mock proxy pool
-	proxy_pool := new(mock_proxy_pool)
-	proxy_pool.Init(t)
-	defer proxy_pool.Close()
+	proxyPool := newMockProxyPool(t)
+	defer proxyPool.Close()
 	// generate bootstrap nodes info
-	nodes := test_generate_nodes()
+	nodes := testGenerateNodes()
 	// --------------------------http---------------------------
-	HTTP := test_generate_http(t, proxy_pool)
+	HTTP := testGenerateHTTP(t, proxyPool)
 	info, err := HTTP.Generate(nodes)
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	t.Log("(http) bootstrap nodes info:", info)
 	// init mock http server
-	s := new(http.Server)
-	port := test_start_http_server(t, s, info)
-	defer func() { _ = s.Close() }()
+	httpServer := http.Server{
+		Addr: "localhost:0",
+	}
+	port := testStartHTTPServer(t, &httpServer, info)
+	defer func() { _ = httpServer.Close() }()
 	// config
-	HTTP.Request.URL = "http://" + test_domain + ":" + port
+	HTTP.Request.URL = "http://localhost:" + port
 	// marshal
 	b, err := HTTP.Marshal()
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	// unmarshal
-	HTTP = New_HTTP(proxy_pool, new(mock_resolver))
+	HTTP = NewHTTP(proxyPool, new(mockDNSResolver))
 	err = HTTP.Unmarshal(b)
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	resolved, err := HTTP.Resolve()
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	require.Equal(t, nodes, resolved)
 	// --------------------------https--------------------------
-	HTTP = test_generate_http(t, proxy_pool)
+	HTTP = testGenerateHTTP(t, proxyPool)
 	info, err = HTTP.Generate(nodes)
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	t.Log("(https) bootstrap nodes info:", info)
 	// init mock https server
-	cert_config := &cert.Config{
-		DNSNames:    []string{"localhost"},
-		IPAddresses: []string{"127.0.0.1", "::1"},
+	certCfg := &cert.Config{
+		DNSNames: []string{"localhost"},
 	}
-	c, k, err := cert.Generate(nil, nil, cert_config)
-	require.Nil(t, err, err)
-	tls_cert, err := tls.X509KeyPair(c, k)
-	require.Nil(t, err, err)
-	s = &http.Server{
+	c, k, err := cert.Generate(nil, nil, certCfg)
+	require.NoError(t, err)
+	tlsCert, err := tls.X509KeyPair(c, k)
+	require.NoError(t, err)
+	httpsServer := http.Server{
+		Addr: "localhost:0",
 		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{tls_cert},
+			Certificates: []tls.Certificate{tlsCert},
 		}}
-	port = test_start_http_server(t, s, info)
-	defer func() { _ = s.Close() }()
+	port = testStartHTTPServer(t, &httpsServer, info)
+	defer func() { _ = httpsServer.Close() }()
 	// config
-	HTTP.Request.URL = "https://" + test_domain + ":" + port
+	HTTP.Request.URL = "https://localhost:" + port
 	// add cert to trust
 	HTTP.Transport.TLSClientConfig.RootCAs = []string{string(c)}
-	// set ipv6
-	HTTP.DNS_Opts.Type = dns.IPV6
 	// marshal
 	b, err = HTTP.Marshal()
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	// unmarshal
-	HTTP = New_HTTP(proxy_pool, new(mock_resolver))
+	HTTP = NewHTTP(proxyPool, new(mockDNSResolver))
 	err = HTTP.Unmarshal(b)
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	resolved, err = HTTP.Resolve()
-	require.Nil(t, err, err)
+	require.NoError(t, err)
 	require.Equal(t, nodes, resolved)
 }
 
-func test_generate_http(t *testing.T, p *mock_proxy_pool) *HTTP {
-	HTTP := New_HTTP(p, new(mock_resolver))
-	HTTP.AES_Key = strings.Repeat("FF", aes.BIT256)
-	HTTP.AES_IV = strings.Repeat("FF", aes.IV_SIZE)
+func testGenerateHTTP(t *testing.T, p *mockProxyPool) *HTTP {
+	HTTP := NewHTTP(p, new(mockDNSResolver))
+	HTTP.AESKey = strings.Repeat("FF", aes.Bit256)
+	HTTP.AESIV = strings.Repeat("FF", aes.IVSize)
 	// generate privatekey
-	privatekey, err := ed25519.Generate_Key()
-	require.Nil(t, err, err)
+	privatekey, err := ed25519.GenerateKey()
+	require.NoError(t, err)
 	HTTP.PrivateKey = privatekey
 	return HTTP
+}
+
+// return port
+func testStartHTTPServer(t *testing.T, s *http.Server, info string) string {
+	l, err := net.Listen("tcp", s.Addr)
+	require.NoError(t, err)
+	data := []byte(info)
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(data)
+	})
+	s.Handler = serveMux
+	errChan := make(chan error, 1)
+	go func() {
+		if s.TLSConfig == nil {
+			errChan <- s.Serve(l)
+		} else {
+			errChan <- s.ServeTLS(l, "", "")
+		}
+	}()
+	// start
+	select {
+	case err := <-errChan:
+		t.Fatal(err)
+	case <-time.After(250 * time.Millisecond):
+	}
+	// get port
+	_, port, err := net.SplitHostPort(l.Addr().String())
+	require.NoError(t, err)
+	return port
 }
