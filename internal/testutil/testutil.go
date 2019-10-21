90c57c32145a52604d758c88a2dbf2e8402677e4
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-10-21 14:34:03 +0800

    testutil: add TLSConfigPair, GenerateData, Conn, DeployHTTPSServer, improve test

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -1,12 +1,27 @@
 package testutil
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"runtime"
+	"sync"
+	"time"
 
 	"github.com/stretchr/testify/require"
+
+	"project/internal/crypto/cert"
 )
+
+// PPROF is used to open pprof
+func PPROF() {
+	go func() {
+		_ = http.ListenAndServe("127.0.0.1:1999", nil)
+	}()
+}
 
 func isDestroyed(object interface{}, gcNum int) bool {
 	destroyed := false
@@ -15,16 +30,114 @@ func isDestroyed(object interface{}, gcNum int) bool {
 	})
 	for i := 0; i < gcNum; i++ {
 		runtime.GC()
+		time.Sleep(100 * time.Millisecond)
 	}
 	return destroyed
 }
 
+// IsDestroyed is used to check if the object has been recycled by the GC
 func IsDestroyed(t require.TestingT, object interface{}, gcNum int) {
 	require.True(t, isDestroyed(object, gcNum), "object not destroyed")
 }
 
-func PPROF() {
+// TLSConfigPair is used to build server and client tls.Config
+func TLSConfigPair(t require.TestingT) (server, client *tls.Config) {
+	ca, err := cert.GenerateCA(nil)
+	require.NoError(t, err)
+	certCfg := &cert.Config{
+		DNSNames:    []string{"localhost"},
+		IPAddresses: []string{"127.0.0.1", "::1"},
+	}
+	// server
+	kp, err := cert.Generate(ca.Certificate, ca.PrivateKey, certCfg)
+	require.NoError(t, err)
+	tlsCert, err := tls.X509KeyPair(kp.EncodeToPEM())
+	require.NoError(t, err)
+	server = &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+	// client
+	client = &tls.Config{RootCAs: x509.NewCertPool()}
+	client.RootCAs.AddCert(ca.Certificate)
+	return
+}
+
+// GenerateData is used to generate test data: []byte{0, 1, .... 254, 255}
+func GenerateData() []byte {
+	testdata := make([]byte, 256)
+	for i := 0; i < 256; i++ {
+		testdata[i] = byte(i)
+	}
+	return testdata
+}
+
+// Conn is used to client & server Conn Read() Write() and Close()
+// if close == true, IsDestroyed will be run after Conn.Close()
+//
+// if Conn about TLS and use net.Pipe(), set close = false
+// server, client := net.Pipe()
+// tlsServer = tls.Server(server, cfg)
+// Conn(tlsServer, xxx, false) must set false
+func Conn(t require.TestingT, server, client net.Conn, close bool) {
+	write := func(conn net.Conn) {
+		data := GenerateData()
+		_, err := conn.Write(data)
+		require.NoError(t, err)
+		require.Equal(t, GenerateData(), data)
+	}
+	read := func(conn net.Conn) {
+		data := make([]byte, 256)
+		_, err := io.ReadFull(conn, data)
+		require.NoError(t, err)
+		require.Equal(t, GenerateData(), data)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// server
 	go func() {
-		_ = http.ListenAndServe("127.0.0.1:8080", nil)
+		defer wg.Done()
+		read(server)
+		write(server)
+		write(server)
+		read(server)
+		if close {
+			require.NoError(t, server.Close())
+			IsDestroyed(t, server, 1)
+		}
 	}()
+	// client
+	write(client)
+	read(client)
+	read(client)
+	write(client)
+	if close {
+		require.NoError(t, client.Close())
+		IsDestroyed(t, client, 1)
+	}
+	wg.Wait()
+}
+
+// DeployHTTPSServer is used to deploy a https server for test
+// if deploy success, will return server port
+func DeployHTTPSServer(t require.TestingT, server *http.Server, kp *cert.KeyPair) string {
+	listener, err := net.Listen("tcp", server.Addr)
+	require.NoError(t, err)
+
+	tlsCert, err := tls.X509KeyPair(kp.EncodeToPEM())
+	require.NoError(t, err)
+	server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+
+	// run
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- server.ServeTLS(listener, "", "")
+	}()
+	select {
+	case err = <-errChan:
+	case <-time.After(250 * time.Millisecond):
+	}
+	require.NoError(t, err)
+
+	// get port
+	_, port, err := net.SplitHostPort(listener.Addr().String())
+	require.NoError(t, err)
+	return port
 }
--- internal/testutil/testutil_test.go
+++ internal/testutil/testutil_test.go
@@ -1,13 +1,28 @@
 package testutil
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/crypto/cert"
 )
+
+func TestPPROF(t *testing.T) {
+	PPROF()
+}
 
 func TestIsDestroyed(t *testing.T) {
 	a := 1
-	fmt.Println(a)
+	n, err := fmt.Fprintln(ioutil.Discard, a)
+	require.Equal(t, n, 2)
+	require.NoError(t, err)
 	if !isDestroyed(&a, 1) {
 		t.Fatal("doesn't destroyed")
 	}
@@ -16,5 +31,51 @@ func TestIsDestroyed(t *testing.T) {
 	if isDestroyed(&b, 1) {
 		t.Fatal("destroyed")
 	}
-	fmt.Println(b)
+	n, err = fmt.Fprintln(ioutil.Discard, b)
+	require.Equal(t, n, 2)
+	require.NoError(t, err)
+
+	c := 3
+	n, err = fmt.Fprintln(ioutil.Discard, c)
+	require.Equal(t, n, 2)
+	require.NoError(t, err)
+	IsDestroyed(t, &c, 1)
+}
+
+func TestTLSConfigPair(t *testing.T) {
+	serverCfg, clientCfg := TLSConfigPair(t)
+	listener, err := tls.Listen("tcp", "localhost:0", serverCfg)
+	require.NoError(t, err)
+	var server net.Conn
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		server, err = listener.Accept()
+		require.NoError(t, err)
+		// must Handshake
+		require.NoError(t, server.(*tls.Conn).Handshake())
+	}()
+	client, err := tls.Dial("tcp", listener.Addr().String(), clientCfg)
+	require.NoError(t, err)
+	wg.Wait()
+	Conn(t, server, client, true)
+}
+
+func TestConn(t *testing.T) {
+	server, client := net.Pipe()
+	Conn(t, server, client, true)
+}
+
+func TestDeployHTTPSServer(t *testing.T) {
+	certCfg := cert.Config{
+		DNSNames:    []string{"localhost"},
+		IPAddresses: []string{"127.0.0.1", "::1"},
+	}
+	kp, err := cert.Generate(nil, nil, &certCfg)
+	require.NoError(t, err)
+	server := http.Server{Addr: "127.0.0.1:0"}
+	port := DeployHTTPSServer(t, &server, kp)
+	t.Log("https server port:", port)
+	_ = server.Close()
 }
