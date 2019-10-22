41342914cfce373738ddf6f2eb3ac0cc02cbd3b3
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-10-22 09:54:28 +0800

    xnet/internal: add interface Dialer for package: light, xhttp, xtls

--- internal/xnet/internal/internal.go
+++ internal/xnet/internal/internal.go
@@ -22,6 +22,8 @@ func (d *deadlineConn) Write(p []byte) (n int, err error) {
 	return d.Conn.Write(p)
 }
 
+// DeadlineConn is used to return a net.Conn that SetReadDeadline()
+// and SetWriteDeadline() before each Read and Write
 func DeadlineConn(conn net.Conn, deadline time.Duration) net.Conn {
 	dc := deadlineConn{
 		deadline: deadline,
@@ -31,4 +33,9 @@ func DeadlineConn(conn net.Conn, deadline time.Duration) net.Conn {
 		dc.deadline = options.DefaultDeadline
 	}
 	return &dc
+}
+
+type Dialer interface {
+	Dial(network, address string) (net.Conn, error)
+	DialTimeout(network, address string, timeout time.Duration) (net.Conn, error)
 }
