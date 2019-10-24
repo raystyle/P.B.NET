a703d2f640566a83c783d48719f121946ef432e2
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-10-24 14:58:56 +0800

    proxy/http: http proxy client support DialXXX(), add Connect() for proxy chain

--- internal/proxy/http/client.go
+++ internal/proxy/http/client.go
@@ -1,53 +1,204 @@
 package http
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
-)
 
-var (
-	ErrNotSupportDial = errors.New("http proxy not support dial")
+	"github.com/pkg/errors"
+
+	"project/internal/options"
 )
 
 type Client struct {
-	url   *url.URL
-	proxy func(*http.Request) (*url.URL, error)
+	network   string
+	address   string
+	https     bool
+	header    http.Header
+	tlsConfig *tls.Config
+	timeout   time.Duration
+
+	basicAuth string
 }
 
-// config = url "http://username:password@127.0.0.1:8080"
-func NewClient(config string) (*Client, error) {
-	u, err := url.Parse(config)
-	if err != nil {
-		return nil, err
+func NewClient(network, address string, https bool, opts *Options) (*Client, error) {
+	client := Client{
+		network: network,
+		address: address,
+		https:   https,
+		header:  opts.Header.Clone(),
+		timeout: opts.Timeout,
 	}
-	return &Client{
-		url: u,
-		proxy: func(*http.Request) (*url.URL, error) {
-			return u, nil
-		},
-	}, nil
+
+	if client.header == nil {
+		client.header = make(http.Header)
+	}
+
+	// set proxy function for Client.HTTP()
+
+	/*
+		http.Transport{}.Proxy
+
+		urlStr := fmt.Sprintf("")
+
+		u, err := url.Parse()
+		http.ProxyURL()
+
+	*/
+	if https {
+		var err error
+		client.tlsConfig, err = opts.TLSConfig.Apply()
+		if err != nil {
+			return nil, err
+		}
+		if client.tlsConfig.ServerName == "" {
+			colonPos := strings.LastIndex(address, ":")
+			if colonPos == -1 {
+				colonPos = len(address)
+			}
+			hostname := address[:colonPos]
+			c := client.tlsConfig.Clone()
+			c.ServerName = hostname
+			client.tlsConfig = c
+		}
+	}
+
+	if client.timeout < 1 {
+		client.timeout = options.DefaultDialTimeout
+	}
+
+	if opts.Username != "" && opts.Password != "" {
+		auth := url.UserPassword(opts.Username, opts.Password).String()
+		client.basicAuth = "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	} else if opts.Username != "" {
+		auth := url.User(opts.Username).String()
+		client.basicAuth = "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	}
+	return &client, nil
 }
 
-func (c *Client) Dial(_, _ string) (net.Conn, error) {
-	return nil, ErrNotSupportDial
+func (c *Client) Dial(_, address string) (net.Conn, error) {
+	conn, err := (&net.Dialer{Timeout: c.timeout}).Dial(c.network, c.address)
+	if err != nil {
+		return nil, errors.WithMessagef(err,
+			"dial: connect http proxy %s failed", c.address)
+	}
+	if c.https {
+		conn = tls.Client(conn, c.tlsConfig)
+	}
+	_ = conn.SetDeadline(time.Now().Add(c.timeout))
+	err = c.Connect(conn, "", address)
+	if err != nil {
+		_ = conn.Close()
+		return nil, errors.WithMessagef(err,
+			"dial: http proxy %s connect %s failed", c.address, address)
+	}
+	_ = conn.SetDeadline(time.Time{})
+	return conn, nil
 }
 
-func (c *Client) DialContext(_ context.Context, _, _ string) (net.Conn, error) {
-	return nil, ErrNotSupportDial
+func (c *Client) DialContext(ctx context.Context, _, address string) (net.Conn, error) {
+	conn, err := (&net.Dialer{Timeout: c.timeout}).DialContext(ctx, c.network, c.address)
+	if err != nil {
+		return nil, errors.WithMessagef(err,
+			"dial context: connect http proxy %s failed", c.address)
+	}
+	if c.https {
+		conn = tls.Client(conn, c.tlsConfig)
+	}
+	_ = conn.SetDeadline(time.Now().Add(c.timeout))
+	err = c.Connect(conn, "", address)
+	if err != nil {
+		_ = conn.Close()
+		return nil, errors.WithMessagef(err,
+			"dial context: http proxy %s connect %s failed", c.address, address)
+	}
+	_ = conn.SetDeadline(time.Time{})
+	return conn, nil
 }
 
-func (c *Client) DialTimeout(_, _ string, _ time.Duration) (net.Conn, error) {
-	return nil, ErrNotSupportDial
+func (c *Client) DialTimeout(_, address string, timeout time.Duration) (net.Conn, error) {
+	if timeout < 1 {
+		timeout = options.DefaultDialTimeout
+	}
+	conn, err := (&net.Dialer{Timeout: timeout}).Dial(c.network, c.address)
+	if err != nil {
+		return nil, errors.WithMessagef(err,
+			"dial timeout: connect http proxy %s failed", c.address)
+	}
+	if c.https {
+		conn = tls.Client(conn, c.tlsConfig)
+	}
+	_ = conn.SetDeadline(time.Now().Add(timeout))
+	err = c.Connect(conn, "", address)
+	if err != nil {
+		_ = conn.Close()
+		return nil, errors.WithMessagef(err,
+			"dial timeout: http proxy %s connect %s failed", c.address, address)
+	}
+	_ = conn.SetDeadline(time.Time{})
+	return conn, nil
+}
+
+func (c *Client) Connect(conn net.Conn, _, address string) error {
+	// CONNECT github.com:443 HTTP/1.1
+	// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0)
+	// Connection: keep-alive
+	// Host: github.com:443
+	buf := new(bytes.Buffer)
+	_, _ = fmt.Fprintf(buf, "CONNECT %s HTTP/1.1\r\n", address)
+	// header
+	header := c.header.Clone()
+	header.Set("Proxy-Connection", "keep-alive")
+	header.Set("Connection", "keep-alive")
+	if c.basicAuth != "" {
+		header.Set("Proxy-Authorization", c.basicAuth)
+	}
+	// write header
+	for k, v := range header {
+		_, _ = fmt.Fprintf(buf, "%s: %s\r\n", k, v[0])
+	}
+	// host
+	_, _ = fmt.Fprintf(buf, "Host: %s\r\n", address)
+	// end
+	buf.WriteString("\r\n")
+	// write to connection
+	addr := conn.RemoteAddr().String()
+	_, err := io.Copy(conn, buf)
+	if err != nil {
+		return errors.Errorf("failed to write request to %s because %s", addr, err)
+	}
+	// read response
+	resp := make([]byte, connectionEstablishedLen)
+	_, err = io.ReadAtLeast(conn, resp, connectionEstablishedLen)
+	if err != nil {
+		return errors.Errorf("failed to read response to %s because %s", addr, err)
+	}
+	// check response
+	// HTTP/1.0 200 Connection established
+	p := strings.Split(strings.ReplaceAll(string(resp), "\r\n", ""), " ")
+	if len(p) != 4 {
+		return errors.Errorf("http proxy %s failed to connect to %s", addr, address)
+	}
+	// HTTP/1.0 200 Connection established or HTTP/1.1 200 Connection established
+	if p[1] == "200" && p[2] == "Connection" && p[3] == "established" {
+		return nil
+	}
+	return errors.Errorf("http proxy %s failed to connect to %s", addr, address)
 }
 
 func (c *Client) HTTP(t *http.Transport) {
-	t.Proxy = c.proxy
+
 }
 
 func (c *Client) Info() string {
-	return c.url.String()
+	return ""
 }
--- internal/proxy/http/client_test.go
+++ internal/proxy/http/client_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"context"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,38 +15,57 @@ import (
 
 func TestClient(t *testing.T) {
 	server := testGenerateServer(t)
-	require.NoError(t, server.ListenAndServe("localhost:0"))
+	require.NoError(t, server.ListenAndServe("tcp", "localhost:0"))
 	defer func() {
 		require.NoError(t, server.Close())
-		testutil.IsDestroyed(t, server, 2)
+		testutil.IsDestroyed(t, server, 1)
 	}()
-	client, err := NewClient("http://admin:123456@" + server.Address())
+	opts := Options{
+		Username: "admin",
+		Password: "123456",
+	}
+	client, err := NewClient("tcp", server.Address(), false, &opts)
 	require.NoError(t, err)
 	wg := sync.WaitGroup{}
-	get := func(url string) {
+
+	var addresses = []string{
+		"8.8.8.8:53",
+		"cloudflare-dns.com:443",
+	}
+	if testutil.IPv6() {
+		addresses = append(addresses, "[2606:4700::6810:f9f9]:443")
+	}
+
+	for _, address := range addresses {
+		wg.Add(1)
+		go func(address string) {
+			defer wg.Done()
+			conn, err := client.Dial("tcp", address)
+			require.NoError(t, err)
+			_ = conn.Close()
+			conn, err = client.DialContext(context.Background(), "tcp", address)
+			require.NoError(t, err)
+			_ = conn.Close()
+			conn, err = client.DialTimeout("tcp", address, 0)
+			require.NoError(t, err)
+			_ = conn.Close()
+		}(address)
+	}
+
+	// set DialContext
+	wg.Add(1)
+	go func() {
 		defer wg.Done()
-		transport := &http.Transport{}
-		client.HTTP(transport)
-		resp, err := (&http.Client{Transport: transport}).Get(url)
+		transport := &http.Transport{DialContext: client.DialContext}
+		resp, err := (&http.Client{Transport: transport}).Get("https://github.com/")
 		require.NoError(t, err)
 		_, err = io.Copy(ioutil.Discard, resp.Body)
 		require.NoError(t, err)
 		_ = resp.Body.Close()
-		// transport.CloseIdleConnections()
-		transport.Proxy = nil
-	}
-	wg.Add(3)
-	go get("http://github.com/")
-	go get("https://github.com/")
-	go get("https://github.com/")
-	// test other
-	_, err = client.Dial("", "")
-	require.Equal(t, err, ErrNotSupportDial)
-	_, err = client.DialContext(nil, "", "")
-	require.Equal(t, err, ErrNotSupportDial)
-	_, err = client.DialTimeout("", "", 0)
-	require.Equal(t, err, ErrNotSupportDial)
+		transport.CloseIdleConnections()
+	}()
+
 	t.Log(client.Info())
 	wg.Wait()
-	testutil.IsDestroyed(t, client, 2)
+	testutil.IsDestroyed(t, client, 1)
 }
