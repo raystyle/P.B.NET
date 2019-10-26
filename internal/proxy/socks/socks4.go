a2900d12059a59a21708ddb4f8e1b76af0a5dfc9
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-10-26 10:59:14 +0800

    proxy/socks: move socks4 socks5 connect and serve to here, support socks4a

---
+++ internal/proxy/socks/socks4.go
@@ -0,0 +1,223 @@
+package socks
+
+import (
+	"bytes"
+	"crypto/subtle"
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"project/internal/convert"
+	"project/internal/logger"
+	"project/internal/xpanic"
+)
+
+const (
+	version4 uint8 = 0x04
+)
+
+type v4Reply uint8
+
+func (r v4Reply) String() string {
+	switch r {
+	case 0x5b:
+		return "request rejected or failed"
+	case 0x5c:
+		return "request rejected because SOCKS server cannot connect to identd on the client"
+	case 0x5d:
+		return "request rejected because the client program and identd report different user-ids"
+	default:
+		return "unknown reply: " + strconv.Itoa(int(r))
+	}
+}
+
+// http://ftp.icm.edu.pl/packages/socks/socks4/SOCKS4.protocol
+// http://www.openssh.com/txt/socks4a.protocol
+
+func (c *Client) connectSocks4(conn net.Conn, _, address string) error {
+	// check address
+	host, port, err := splitHostPort(address)
+	if err != nil {
+		return err
+	}
+	var (
+		hostData   []byte
+		socks4aExt bool
+	)
+	ip := net.ParseIP(host)
+	if ip != nil {
+		ip4 := ip.To4()
+		if ip4 != nil {
+			hostData = ip4
+		} else {
+			return errors.New("socks4 or socks4a don't support IPv6")
+		}
+	} else if c.disableExt {
+		return errors.Errorf("%s is not a socks4a server", c.protocol)
+	} else {
+		l := len(host)
+		if l > 255 {
+			return errors.New("hostname too long")
+		}
+		hostData = make([]byte, l)
+		copy(hostData, host)
+		socks4aExt = true
+	}
+
+	// handshake
+	_ = conn.SetDeadline(time.Now().Add(c.timeout))
+	buffer := bytes.Buffer{}
+	buffer.WriteByte(version4)
+	buffer.WriteByte(connect)
+	buffer.Write(convert.Uint16ToBytes(port))
+	if socks4aExt { // socks4a ext
+		buffer.Write([]byte{0x00, 0x00, 0x00, 0x01}) // padding IPv4
+	} else {
+		buffer.Write(hostData) // IPv4
+	}
+	// user id
+	buffer.Write(c.userID)
+	buffer.WriteByte(0x00) // NULL
+	// write host
+	if socks4aExt {
+		buffer.Write(hostData)
+		buffer.WriteByte(0x00) // NULL
+	}
+	_, err = conn.Write(buffer.Bytes())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	// read response
+	// version4, reply, port, ip
+	resp := make([]byte, 1+1+2+net.IPv4len)
+	_, err = io.ReadFull(conn, resp)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if resp[0] != 0x00 { // must 0x00 not 0x04
+		return errors.Errorf("invalid version %d", resp[0])
+	}
+	if resp[1] != 0x5a {
+		return errors.New(v4Reply(resp[1]).String())
+	}
+	return nil
+}
+
+// http://ftp.icm.edu.pl/packages/socks/socks4/SOCKS4.protocol
+// http://www.openssh.com/txt/socks4a.protocol
+
+var (
+	v4ReplySucceeded      = []byte{0x00, 0x5a, 0, 0, 0, 0, 0, 0}
+	v4ReplyConnectRefused = []byte{0x00, 0x5b, 0, 0, 0, 0, 0, 0}
+)
+
+func (c *conn) serveSocks4() {
+	const title = "conn.serveSocks4()"
+	var err error
+	defer func() {
+		if r := recover(); r != nil {
+			c.log(logger.Fatal, xpanic.Print(r, title))
+		}
+		if err != nil {
+			c.log(logger.Error, err)
+		}
+		_ = c.conn.Close()
+		// delete conn
+		c.server.connsRWM.Lock()
+		delete(c.server.conns, c.key())
+		c.server.connsRWM.Unlock()
+		c.server.wg.Done()
+	}()
+	// 10 = version(1) + cmd(1) + port(2) + address(4) + 2xNULL(2) maybe
+	// 16 = domain name
+	buffer := make([]byte, 10+16) // prepare
+	_, err = io.ReadAtLeast(c.conn, buffer[:8], 8)
+	if err != nil {
+		return
+	}
+	// check version
+	if buffer[0] != version4 {
+		c.log(logger.Error, "unexpected protocol version")
+		return
+	}
+	// command
+	if buffer[1] != connect {
+		c.log(logger.Error, "unknown command")
+		return
+	}
+	port := convert.BytesToUint16(buffer[2:4])
+	var (
+		domain bool
+		host   string
+	)
+	// check is domain 0.0.0.x is domain mode
+	if bytes.Equal(buffer[4:7], []byte{0x00, 0x00, 0x00}) && buffer[7] != 0x00 {
+		domain = true
+	} else {
+		host = net.IPv4(buffer[4], buffer[5], buffer[6], buffer[7]).String()
+	}
+	var userID []byte
+	for {
+		_, err = c.conn.Read(buffer[:1])
+		if err != nil {
+			return
+		}
+		// find 0x00(end)
+		if buffer[0] == 0x00 {
+			break
+		}
+		userID = append(userID, buffer[0])
+	}
+	// compare user id
+	if subtle.ConstantTimeCompare(c.server.userID, userID) != 1 {
+		c.log(logger.Exploit, fmt.Sprintf("invalid user id: %s", userID))
+		return
+	}
+	if domain { // read domain
+		var dn []byte
+		for {
+			_, err = c.conn.Read(buffer[:1])
+			if err != nil {
+				return
+			}
+			// find 0x00(end)
+			if buffer[0] == 0x00 {
+				break
+			}
+			dn = append(dn, buffer[0])
+		}
+		host = string(dn)
+	}
+	// connect target
+	address := net.JoinHostPort(host, strconv.Itoa(int(port)))
+	c.log(logger.Debug, "connect: "+address)
+	var remoteConn net.Conn
+	remoteConn, err = net.Dial("tcp4", address)
+	if err != nil {
+		_, _ = c.conn.Write(v4ReplyConnectRefused)
+		return
+	}
+	defer func() { _ = remoteConn.Close() }()
+	_, err = c.conn.Write(v4ReplySucceeded)
+	if err != nil {
+		return
+	}
+	// start copy
+	_ = remoteConn.SetDeadline(time.Time{})
+	_ = c.conn.SetDeadline(time.Time{})
+	c.server.wg.Add(1)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				c.log(logger.Fatal, xpanic.Print(r, title))
+			}
+			c.server.wg.Done()
+		}()
+		_, _ = io.Copy(c.conn, remoteConn)
+	}()
+	_, _ = io.Copy(remoteConn, c.conn)
+}
--- internal/proxy/socks/socks5.go
+++ internal/proxy/socks/socks5.go
@@ -1,8 +1,19 @@
 package socks
 
 import (
-	"errors"
+	"bytes"
+	"crypto/subtle"
+	"fmt"
+	"io"
+	"net"
 	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"project/internal/convert"
+	"project/internal/logger"
+	"project/internal/xpanic"
 )
 
 const (
@@ -31,14 +42,9 @@ const (
 	addressNotSupport uint8 = 0x08
 )
 
-var (
-	ErrNotSupportNetwork   = errors.New("support only tcp tcp4 tcp6")
-	ErrNoAcceptableMethods = errors.New("no acceptable authentication methods")
-)
+type v5Reply uint8
 
-type Reply uint8
-
-func (r Reply) String() string {
+func (r v5Reply) String() string {
 	switch r {
 	case 0x01:
 		return "general SOCKS server failure"
@@ -57,6 +63,359 @@ func (r Reply) String() string {
 	case 0x08:
 		return "address type not supported"
 	default:
-		return "unknown code: " + strconv.Itoa(int(r))
+		return "unknown reply: " + strconv.Itoa(int(r))
 	}
+}
+
+// https://www.ietf.org/rfc/rfc1928.txt
+func (c *Client) connectSocks5(conn net.Conn, _, address string) error {
+	_ = conn.SetDeadline(time.Now().Add(c.timeout))
+	host, port, err := splitHostPort(address)
+	if err != nil {
+		return err
+	}
+	// request authentication
+	buffer := bytes.Buffer{}
+	buffer.WriteByte(version5)
+	if c.username == "" {
+		buffer.WriteByte(1)
+		buffer.WriteByte(notRequired)
+	} else {
+		buffer.WriteByte(2)
+		buffer.WriteByte(notRequired)
+		buffer.WriteByte(usernamePassword)
+	}
+	_, err = conn.Write(buffer.Bytes())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	response := make([]byte, 2)
+	_, err = io.ReadFull(conn, response)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if response[0] != version5 {
+		return errors.Errorf("unexpected protocol version %d", response[0])
+	}
+	am := response[1]
+	if am == noAcceptableMethods {
+		return errors.New("no acceptable authentication methods")
+	}
+	// authenticate
+	switch am {
+	case notRequired:
+	case usernamePassword:
+		username := c.username
+		password := c.password
+		if len(username) == 0 || len(username) > 255 {
+			return errors.New("invalid username length")
+		}
+		// https://www.ietf.org/rfc/rfc1929.txt
+		buffer.Reset()
+		buffer.WriteByte(usernamePasswordVersion)
+		buffer.WriteByte(byte(len(username)))
+		buffer.WriteString(username)
+		buffer.WriteByte(byte(len(password)))
+		buffer.WriteString(password)
+		_, err := conn.Write(buffer.Bytes())
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		response := make([]byte, 2)
+		_, err = io.ReadFull(conn, response)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if response[0] != usernamePasswordVersion {
+			return errors.New("invalid username/password version")
+		}
+		if response[1] != statusSucceeded {
+			return errors.New("invalid username/password")
+		}
+	default:
+		return errors.Errorf("unsupported authentication method %d", am)
+	}
+	// send connect target
+	buffer.Reset()
+	buffer.WriteByte(version5)
+	buffer.WriteByte(connect)
+	buffer.WriteByte(reserve)
+	ip := net.ParseIP(host)
+	if ip != nil {
+		ip4 := ip.To4()
+		if ip4 != nil {
+			buffer.WriteByte(ipv4)
+			buffer.Write(ip4)
+		} else {
+			ip6 := ip.To16()
+			if ip6 != nil {
+				buffer.WriteByte(ipv6)
+				buffer.Write(ip6)
+			} else {
+				return errors.New("unknown address type")
+			}
+		}
+	} else {
+		if len(host) > 255 {
+			return errors.New("FQDN too long")
+		}
+		buffer.WriteByte(fqdn)
+		buffer.WriteByte(byte(len(host)))
+		buffer.Write([]byte(host))
+	}
+	buffer.Write(convert.Uint16ToBytes(port))
+	_, err = conn.Write(buffer.Bytes())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	// receive reply
+	response = make([]byte, 4)
+	_, err = io.ReadFull(conn, response)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if response[0] != version5 {
+		return errors.Errorf("unexpected protocol version %d", response[0])
+	}
+	if response[1] != succeeded {
+		return errors.New(v5Reply(response[1]).String())
+	}
+	if response[2] != 0 {
+		return errors.New("non-zero reserved field")
+	}
+	l := 2 // port
+	switch response[3] {
+	case ipv4:
+		l += net.IPv4len
+	case ipv6:
+		l += net.IPv6len
+	case fqdn:
+		_, err = io.ReadFull(conn, response[:1])
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		l += int(response[0])
+	}
+	// grow
+	if cap(response) < l {
+		response = make([]byte, l)
+	} else {
+		response = response[:l]
+	}
+	_, err = io.ReadFull(conn, response)
+	return errors.WithStack(err)
+}
+
+var (
+	v5ReplySucceeded         = []byte{version5, succeeded, reserve, ipv4, 0, 0, 0, 0, 0, 0}
+	v5ReplyConnectRefused    = []byte{version5, connRefused, reserve, ipv4, 0, 0, 0, 0, 0, 0}
+	v5ReplyAddressNotSupport = []byte{version5, addressNotSupport, reserve, ipv4, 0, 0, 0, 0, 0, 0}
+)
+
+func (c *conn) serveSocks5() {
+	const title = "conn.serveSocks5()"
+	var err error
+	defer func() {
+		if r := recover(); r != nil {
+			c.log(logger.Fatal, xpanic.Print(r, title))
+		}
+		if err != nil {
+			c.log(logger.Error, err)
+		}
+		_ = c.conn.Close()
+		// delete conn
+		c.server.connsRWM.Lock()
+		delete(c.server.conns, c.key())
+		c.server.connsRWM.Unlock()
+		c.server.wg.Done()
+	}()
+	buffer := make([]byte, 16) // prepare
+	// read version
+	_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+	if err != nil {
+		return
+	}
+	if buffer[0] != version5 {
+		c.log(logger.Error, "unexpected protocol version")
+		return
+	}
+	// read authentication methods
+	_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+	if err != nil {
+		return
+	}
+	l := int(buffer[0])
+	if l == 0 {
+		c.log(logger.Error, "no authentication method")
+		return
+	}
+	if l > len(buffer) {
+		buffer = make([]byte, l)
+	}
+	_, err = io.ReadAtLeast(c.conn, buffer[:l], l)
+	if err != nil {
+		return
+	}
+	// write authentication method
+	if c.server.username != nil {
+		_, err = c.conn.Write([]byte{version5, usernamePassword})
+		if err != nil {
+			return
+		}
+		// read username and password version
+		_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+		if err != nil {
+			return
+		}
+		if buffer[0] != usernamePasswordVersion {
+			c.log(logger.Error, "unexpected username password version")
+			return
+		}
+		// read username length
+		_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+		if err != nil {
+			return
+		}
+		l = int(buffer[0])
+		if l > len(buffer) {
+			buffer = make([]byte, l)
+		}
+		// read username
+		_, err = io.ReadAtLeast(c.conn, buffer[:l], l)
+		if err != nil {
+			return
+		}
+		username := make([]byte, l)
+		copy(username, buffer[:l])
+		// read password length
+		_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+		if err != nil {
+			return
+		}
+		l = int(buffer[0])
+		if l > len(buffer) {
+			buffer = make([]byte, l)
+		}
+		// read password
+		_, err = io.ReadAtLeast(c.conn, buffer[:l], l)
+		if err != nil {
+			return
+		}
+		password := make([]byte, l)
+		copy(password, buffer[:l])
+		// write username password version
+		_, err = c.conn.Write([]byte{usernamePasswordVersion})
+		if err != nil {
+			return
+		}
+		if subtle.ConstantTimeCompare(c.server.username, username) != 1 ||
+			subtle.ConstantTimeCompare(c.server.password, password) != 1 {
+			l := fmt.Sprintf("invalid username password: %s %s", username, password)
+			c.log(logger.Exploit, l)
+			_, _ = c.conn.Write([]byte{statusFailed})
+			return
+		} else {
+			_, err = c.conn.Write([]byte{statusSucceeded})
+		}
+	} else {
+		_, err = c.conn.Write([]byte{version5, notRequired})
+	}
+	if err != nil {
+		return
+	}
+	// receive connect target
+	// version | cmd | reserve | address type
+	if len(buffer) < 10 {
+		buffer = make([]byte, 4+net.IPv4len+2) // 4 + 4(ipv4) + 2(port)
+	}
+	_, err = io.ReadAtLeast(c.conn, buffer[:4], 4)
+	if err != nil {
+		return
+	}
+	if buffer[0] != version5 {
+		c.log(logger.Exploit, "unexpected connect protocol version")
+		return
+	}
+	if buffer[1] != connect {
+		c.log(logger.Exploit, "unknown command")
+		_, _ = c.conn.Write([]byte{version5, commandNotSupport, reserve})
+		return
+	}
+	if buffer[2] != reserve { // reserve
+		c.log(logger.Exploit, "non-zero reserved field")
+		_, err = c.conn.Write([]byte{version5, noReserve, reserve})
+		return
+	}
+	// read address
+	var host string
+	switch buffer[3] {
+	case ipv4:
+		_, err = io.ReadAtLeast(c.conn, buffer[:net.IPv4len], net.IPv4len)
+		if err != nil {
+			return
+		}
+		host = net.IP(buffer[:net.IPv4len]).String()
+	case ipv6:
+		buffer = make([]byte, net.IPv6len)
+		_, err = io.ReadAtLeast(c.conn, buffer[:net.IPv6len], net.IPv6len)
+		if err != nil {
+			return
+		}
+		host = net.IP(buffer[:net.IPv6len]).String()
+	case fqdn:
+		// get FQDN length
+		_, err = io.ReadAtLeast(c.conn, buffer[:1], 1)
+		if err != nil {
+			return
+		}
+		l = int(buffer[0])
+		if l > len(buffer) {
+			buffer = make([]byte, l)
+		}
+		_, err = io.ReadAtLeast(c.conn, buffer[:l], l)
+		if err != nil {
+			return
+		}
+		host = string(buffer[:l])
+	default:
+		c.log(logger.Exploit, "address type not supported")
+		_, _ = c.conn.Write(v5ReplyAddressNotSupport)
+		return
+	}
+	// get port
+	_, err = io.ReadAtLeast(c.conn, buffer[:2], 2)
+	if err != nil {
+		return
+	}
+	// connect target
+	port := convert.BytesToUint16(buffer[:2])
+	address := net.JoinHostPort(host, strconv.Itoa(int(port)))
+	c.log(logger.Debug, "connect: "+address)
+	var remoteConn net.Conn
+	remoteConn, err = net.Dial("tcp", address)
+	if err != nil {
+		_, _ = c.conn.Write(v5ReplyConnectRefused)
+		return
+	}
+	defer func() { _ = remoteConn.Close() }()
+	// write reply
+	// ipv4 + 0.0.0.0 + 0(port)
+	_, err = c.conn.Write(v5ReplySucceeded)
+	if err != nil {
+		return
+	}
+	// start copy
+	_ = remoteConn.SetDeadline(time.Time{})
+	_ = c.conn.SetDeadline(time.Time{})
+	c.server.wg.Add(1)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				c.log(logger.Fatal, xpanic.Print(r, title))
+			}
+			c.server.wg.Done()
+		}()
+		_, _ = io.Copy(c.conn, remoteConn)
+	}()
+	_, _ = io.Copy(remoteConn, c.conn)
 }
