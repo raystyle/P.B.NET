c340e7d30c9742fc203cd34e87d21822d9cbf417
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-10-30 13:13:24 +0800

    proxy/direct: improve TestDirect(), add check and test IPv4 & IPv6

--- internal/proxy/direct/direct_test.go
+++ internal/proxy/direct/direct_test.go
@@ -5,21 +5,54 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"project/internal/testsuite"
 )
 
 func TestDirect(t *testing.T) {
 	d := Direct{}
-	conn, err := d.Dial("tcp", "github.com:443")
-	require.NoError(t, err)
-	_ = conn.Close()
-	conn, err = d.DialContext(context.Background(), "tcp", "github.com:443")
-	require.NoError(t, err)
-	_ = conn.Close()
-	conn, err = d.DialTimeout("tcp", "github.com:443", 0)
-	require.NoError(t, err)
-	_ = conn.Close()
-	d.HTTP(nil)
+
+	if testsuite.EnableIPv4() {
+		const network = "tcp4"
+
+		addr := testsuite.GetIPv4Address()
+		conn, err := d.Dial(network, addr)
+		require.NoError(t, err)
+		_ = conn.Close()
+
+		addr = testsuite.GetIPv4Address()
+		conn, err = d.DialContext(context.Background(), network, addr)
+		require.NoError(t, err)
+		_ = conn.Close()
+
+		addr = testsuite.GetIPv4Address()
+		conn, err = d.DialTimeout(network, addr, 0)
+		require.NoError(t, err)
+		_ = conn.Close()
+	}
+
+	if testsuite.EnableIPv6() {
+		const network = "tcp6"
+
+		addr := testsuite.GetIPv6Address()
+		conn, err := d.Dial(network, addr)
+		require.NoError(t, err)
+		_ = conn.Close()
+
+		addr = testsuite.GetIPv6Address()
+		conn, err = d.DialContext(context.Background(), network, addr)
+		require.NoError(t, err)
+		_ = conn.Close()
+
+		addr = testsuite.GetIPv6Address()
+		conn, err = d.DialTimeout(network, addr, 0)
+		require.NoError(t, err)
+		_ = conn.Close()
+	}
+
+	// padding
 	_, _ = d.Connect(nil, "", "")
+	d.HTTP(nil)
 	t.Log(d.Timeout())
 	t.Log(d.Server())
 	t.Log(d.Info())
