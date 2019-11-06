57e1c6bd27ccafe72a5a9f284cc016e18e84833b
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-06 13:19:11 +0800

    xnet/quic/quic_test.go: change package name testutil to testsuite, add testsuite check network

--- internal/xnet/quic/quic_test.go
+++ internal/xnet/quic/quic_test.go
@@ -11,17 +11,21 @@ import (
 
 func TestQUIC(t *testing.T) {
 	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
-	listener, err := Listen("udp4", "localhost:0", serverCfg, 0)
-	require.NoError(t, err)
-	addr := listener.Addr().String()
-	testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-		return Dial("udp4", addr, clientCfg, 0)
-	}, true)
+	if testsuite.EnableIPv4() {
+		listener, err := Listen("udp4", "localhost:0", serverCfg, 0)
+		require.NoError(t, err)
+		addr := listener.Addr().String()
+		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+			return Dial("udp4", addr, clientCfg, 0)
+		}, true)
+	}
 
-	listener, err = Listen("udp6", "localhost:0", serverCfg, 0)
-	require.NoError(t, err)
-	addr = listener.Addr().String()
-	testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-		return Dial("udp6", addr, clientCfg, 0)
-	}, true)
+	if testsuite.EnableIPv6() {
+		listener, err := Listen("udp6", "localhost:0", serverCfg, 0)
+		require.NoError(t, err)
+		addr := listener.Addr().String()
+		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+			return Dial("udp6", addr, clientCfg, 0)
+		}, true)
+	}
 }
