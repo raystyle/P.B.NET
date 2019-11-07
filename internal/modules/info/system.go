c15054f8f40b740b7267f81676be56a8ef0de970
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-07 14:54:28 +0800

    modules/info/system.go: add PPID, GoVersion, remove a useless err check

--- internal/modules/info/system.go
+++ internal/modules/info/system.go
@@ -8,40 +8,43 @@ import (
 )
 
 type System struct {
-	IPs      []string
-	OS       string
-	Arch     string
-	PID      int
-	Hostname string
-	Username string
+	IP        []string
+	OS        string
+	Arch      string
+	GoVersion string
+	PID       int
+	PPID      int
+	Hostname  string
+	Username  string
 }
 
 func GetSystemInfo() *System {
 	system := System{}
-	ifaces, err := net.Interfaces()
-	if err == nil {
-		for i := 0; i < len(ifaces); i++ {
-			if ifaces[i].Flags == net.FlagUp|net.FlagBroadcast|net.FlagMulticast {
-				addrs, err := ifaces[i].Addrs()
-				if err != nil {
-					continue
-				}
-				for j := 0; j < len(addrs); j++ {
-					system.IPs = append(system.IPs, addrs[j].String())
-				}
+	ifaces, _ := net.Interfaces()
+	for i := 0; i < len(ifaces); i++ {
+		if ifaces[i].Flags == net.FlagUp|net.FlagBroadcast|net.FlagMulticast {
+			addrs, err := ifaces[i].Addrs()
+			if err != nil {
+				continue
+			}
+			for j := 0; j < len(addrs); j++ {
+				system.IP = append(system.IP, addrs[j].String())
 			}
 		}
 	}
 	system.OS = runtime.GOOS
 	system.Arch = runtime.GOARCH
 	system.PID = os.Getpid()
+	system.PPID = os.Getppid()
+	system.GoVersion = runtime.Version()
+	var err error
 	system.Hostname, err = os.Hostname()
 	if err != nil {
-		system.Hostname = "unknown"
+		system.Hostname = "<unknown>"
 	}
 	u, err := user.Current()
 	if err != nil {
-		system.Username = "unknown"
+		system.Username = "<unknown>"
 	} else {
 		system.Username = u.Username
 	}
