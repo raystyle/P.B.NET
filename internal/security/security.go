4d65c8b63a03d6af9d753ba5feef0560b0ef540b
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-09 10:00:02 +0800

    security: add FlushString to cover string if string has secret, add TestFlushString

--- internal/security/security.go
+++ internal/security/security.go
@@ -1,7 +1,9 @@
 package security
 
 import (
+	"reflect"
 	"sync"
+	"unsafe"
 
 	"project/internal/random"
 )
@@ -54,6 +56,7 @@ func FlushMemory() {
 	memory.Flush()
 }
 
+// FlushBytes is used to cover []byte if []byte has secret
 func FlushBytes(b []byte) {
 	mem := NewMemory()
 	mem.Padding()
@@ -61,4 +64,19 @@ func FlushBytes(b []byte) {
 	randBytes := rand.Bytes(len(b))
 	copy(b, randBytes)
 	mem.Flush()
+}
+
+// FlushString is used to cover string if string has secret
+func FlushString(s *string) {
+	mem := NewMemory()
+	mem.Padding()
+	rand := random.New(0)
+	sh := (*reflect.StringHeader)(unsafe.Pointer(s))
+	randBytes := rand.Bytes(sh.Len)
+	for i := 0; i < sh.Len; i++ {
+		mem.Padding()
+		b := (*byte)(unsafe.Pointer(sh.Data + uintptr(i)))
+		*b = randBytes[i]
+		mem.Flush()
+	}
 }
--- internal/security/security_test.go
+++ internal/security/security_test.go
@@ -2,6 +2,7 @@ package security
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 )
 
@@ -10,6 +11,20 @@ func TestFlushBytes(t *testing.T) {
 	b2 := []byte{1, 2, 3, 4}
 	FlushBytes(b2)
 	if bytes.Equal(b1, b2) {
-		t.Fatal("flush slice failed")
+		t.Fatal("failed to flush bytes")
+	}
+}
+
+func TestFlushString(t *testing.T) {
+	// must use strings.Repeat to generate testdata
+	// if you use this
+	// s1 := "aaa"
+	// s2 := "aaa"
+	// FlushString(&s1) will panic, because it change const
+	s1 := strings.Repeat("a", 10)
+	s2 := strings.Repeat("a", 10)
+	FlushString(&s1)
+	if s1 == s2 {
+		t.Fatal("failed to flush string")
 	}
 }
