df20d710e7fb2ffdfacda63b4dc465c5c3fa5852
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-09 11:58:50 +0800

    crypto/curve25519: remove Deprecated ScalarMult, use X25519 to replace it

--- internal/crypto/curve25519/curve25519.go
+++ internal/crypto/curve25519/curve25519.go
@@ -1,9 +1,6 @@
 package curve25519
 
 import (
-	"crypto/subtle"
-	"errors"
-
 	"golang.org/x/crypto/curve25519"
 )
 
@@ -11,27 +8,8 @@ import (
 var basePoint = []byte{9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-var zeros = make([]byte, 32)
-
 func ScalarMult(in, base []byte) ([]byte, error) {
-	if len(in) != 32 || len(base) != 32 {
-		return nil, errors.New("invalid in or base size")
-	}
-	var (
-		dstArray  [32]byte
-		inArray   [32]byte
-		baseArray [32]byte
-	)
-	for i := 0; i < 32; i++ {
-		inArray[i] = in[i]
-		baseArray[i] = base[i]
-	}
-	curve25519.ScalarMult(&dstArray, &inArray, &baseArray)
-	dst := dstArray[:]
-	if subtle.ConstantTimeCompare(dst, zeros) == 1 {
-		return nil, errors.New("curve25519 public value has wrong order")
-	}
-	return dst, nil
+	return curve25519.X25519(in, base)
 }
 
 func ScalarBaseMult(in []byte) ([]byte, error) {
