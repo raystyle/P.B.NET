19faf449009324895965571e09de48b56d3845b7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-11-12 21:36:41 +0800

    timesync/http_test.go: improve all test about HTTP, add sub tests

--- internal/timesync/http_test.go
+++ internal/timesync/http_test.go
@@ -10,109 +10,152 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"project/internal/dns"
+	"project/internal/testsuite"
 	"project/internal/testsuite/testdns"
 	"project/internal/testsuite/testproxy"
 )
 
 func TestHTTPClient_Query(t *testing.T) {
+	t.Parallel()
+
 	dnsClient, pool, manager := testdns.DNSClient(t)
 	defer func() { require.NoError(t, manager.Close()) }()
-	HTTP := NewHTTP(context.Background(), pool, dnsClient)
-	b, err := ioutil.ReadFile("testdata/http_opts.toml")
-	require.NoError(t, err)
-	require.NoError(t, HTTP.Import(b))
 
-	// simple query
-	now, optsErr, err := HTTP.Query()
-	require.NoError(t, err)
-	require.False(t, optsErr)
-	t.Log("now(HTTP) simple:", now.Local())
+	t.Run("https", func(t *testing.T) {
+		HTTP := NewHTTP(context.Background(), pool, dnsClient)
 
-	// http
-	HTTP.Request.URL = "http://test-ipv6.com/"
-	now, optsErr, err = HTTP.Query()
-	require.NoError(t, err)
-	require.False(t, optsErr)
-	t.Log("now(HTTP) http:", now.Local())
+		b, err := ioutil.ReadFile("testdata/http_opts.toml")
+		require.NoError(t, err)
+		require.NoError(t, HTTP.Import(b))
 
-	// with proxy
-	HTTP.ProxyTag = testproxy.TagBalance
-	HTTP.Request.URL = "http://test-ipv6.com:80/"
-	now, optsErr, err = HTTP.Query()
-	require.NoError(t, err)
-	require.False(t, optsErr)
-	t.Log("now(HTTP): with proxy", now.Local())
+		now, optsErr, err := HTTP.Query()
+		require.NoError(t, err)
+		require.False(t, optsErr)
+		t.Log("now(HTTP) simple:", now.Local())
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
+
+	t.Run("http with proxy", func(t *testing.T) {
+		HTTP := NewHTTP(context.Background(), pool, dnsClient)
+
+		HTTP.ProxyTag = testproxy.TagBalance
+		HTTP.Request.URL = "http://ds.vm2.test-ipv6.com:80/"
+
+		now, optsErr, err := HTTP.Query()
+		require.NoError(t, err)
+		require.False(t, optsErr)
+		t.Log("now(HTTP): with proxy", now.Local())
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
 }
 
 func TestHTTPClient_Query_Failed(t *testing.T) {
+	t.Parallel()
+
 	dnsClient, pool, manager := testdns.DNSClient(t)
 	defer func() { require.NoError(t, manager.Close()) }()
-	HTTP := NewHTTP(context.Background(), pool, dnsClient)
-	b, err := ioutil.ReadFile("testdata/http_opts.toml")
-	require.NoError(t, err)
-	require.NoError(t, HTTP.Import(b))
 
-	// invalid request
-	HTTP.Request.Post = "foo data"
-	_, optsErr, err := HTTP.Query()
-	require.Error(t, err)
-	require.True(t, optsErr)
+	newHTTP := func(t *testing.T) *HTTP {
+		HTTP := NewHTTP(context.Background(), pool, dnsClient)
+		HTTP.Request.URL = "test.com"
+		HTTP.Transport.TLSClientConfig.InsecureLoadFromSystem = true
+		return HTTP
+	}
 
-	HTTP.Request.Post = ""
+	t.Run("invalid request", func(t *testing.T) {
+		HTTP := newHTTP(t)
 
-	// invalid transport
-	HTTP.Transport.TLSClientConfig.RootCAs = []string{"foo data"}
-	_, optsErr, err = HTTP.Query()
-	require.Error(t, err)
-	require.True(t, optsErr)
+		HTTP.Request.Post = "foo data"
 
-	HTTP.Transport.TLSClientConfig.RootCAs = nil
+		_, optsErr, err := HTTP.Query()
+		require.Error(t, err)
+		require.True(t, optsErr)
 
-	// doesn't exist proxy
-	HTTP.ProxyTag = "foo proxy"
-	_, optsErr, err = HTTP.Query()
-	require.Error(t, err)
-	require.True(t, optsErr)
+		testsuite.IsDestroyed(t, HTTP)
+	})
 
-	HTTP.ProxyTag = ""
+	t.Run("invalid transport", func(t *testing.T) {
+		HTTP := newHTTP(t)
 
-	// invalid domain name
-	HTTP.Request.URL = "http://asdasd1516ads.com/"
-	_, optsErr, err = HTTP.Query()
-	require.Error(t, err)
-	require.True(t, optsErr)
+		HTTP.Transport.TLSClientConfig.RootCAs = []string{"foo data"}
 
-	// all failed
-	HTTP.Request.URL = "https://github.com:8989/"
-	HTTP.Timeout = time.Second
-	_, optsErr, err = HTTP.Query()
-	require.Error(t, err)
-	require.False(t, optsErr)
+		_, optsErr, err := HTTP.Query()
+		require.Error(t, err)
+		require.True(t, optsErr)
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
+
+	t.Run("doesn't exist proxy", func(t *testing.T) {
+		HTTP := newHTTP(t)
+
+		HTTP.ProxyTag = "foo proxy"
+
+		_, optsErr, err := HTTP.Query()
+		require.Error(t, err)
+		require.True(t, optsErr)
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
+
+	t.Run("invalid domain name", func(t *testing.T) {
+		HTTP := newHTTP(t)
+
+		HTTP.Request.URL = "http://test/"
+
+		_, optsErr, err := HTTP.Query()
+		require.Error(t, err)
+		require.True(t, optsErr)
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
+
+	t.Run("all failed", func(t *testing.T) {
+		HTTP := newHTTP(t)
+
+		HTTP.Request.URL = "https://github.com:8989/"
+		HTTP.Timeout = time.Second
+
+		_, optsErr, err := HTTP.Query()
+		require.Error(t, err)
+		require.False(t, optsErr)
+
+		testsuite.IsDestroyed(t, HTTP)
+	})
 }
 
 func TestGetHeaderDate(t *testing.T) {
+	t.Parallel()
+
 	client := &http.Client{
 		Transport: new(http.Transport),
 		Timeout:   10 * time.Second,
 	}
-	r, err := http.NewRequest(http.MethodGet, "http://test-ipv6.com/", nil)
-	require.NoError(t, err)
-	now, err := getHeaderDate(r, client)
-	require.NoError(t, err)
-	t.Log(now)
 
-	// https
-	r, err = http.NewRequest(http.MethodGet, "https://cloudflare-dns.com/", nil)
-	require.NoError(t, err)
-	now, err = getHeaderDate(r, client)
-	require.NoError(t, err)
-	t.Log(now)
+	t.Run("http", func(t *testing.T) {
+		r, err := http.NewRequest(http.MethodGet, "http://test-ipv6.com/", nil)
+		require.NoError(t, err)
+		now, err := getHeaderDate(r, client)
+		require.NoError(t, err)
+		t.Log(now.Local())
+	})
 
-	// failed to query date
-	r, err = http.NewRequest(http.MethodGet, "http://asdasd1516ads.com/", nil)
-	require.NoError(t, err)
-	_, err = getHeaderDate(r, client)
-	require.Error(t, err)
+	t.Run("https", func(t *testing.T) {
+		r, err := http.NewRequest(http.MethodGet, "https://cloudflare-dns.com/", nil)
+		require.NoError(t, err)
+		now, err := getHeaderDate(r, client)
+		require.NoError(t, err)
+		t.Log(now.Local())
+	})
+
+	t.Run("failed to query date", func(t *testing.T) {
+		r, err := http.NewRequest(http.MethodGet, "http://test/", nil)
+		require.NoError(t, err)
+		_, err = getHeaderDate(r, client)
+		require.Error(t, err)
+	})
 }
 
 func TestHTTPOptions(t *testing.T) {
@@ -128,7 +171,7 @@ func TestHTTPOptions(t *testing.T) {
 	}{
 		{expected: 15 * time.Second, actual: HTTP.Timeout},
 		{expected: "balance", actual: HTTP.ProxyTag},
-		{expected: "http://abc.com/", actual: HTTP.Request.URL},
+		{expected: "http://test.com/", actual: HTTP.Request.URL},
 		{expected: 2, actual: HTTP.Transport.MaxIdleConns},
 		{expected: dns.ModeSystem, actual: HTTP.DNSOpts.Mode},
 	}
