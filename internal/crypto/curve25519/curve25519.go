8fb9e9a5a1ad8f15ff9959b65a8ee93e8c750064
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-16 10:00:48 +0800

    crypto/curve25519: update TestScalarBaseMult, add TestKeyExchange

--- internal/crypto/curve25519/curve25519.go
+++ internal/crypto/curve25519/curve25519.go
@@ -4,14 +4,10 @@ import (
 	"golang.org/x/crypto/curve25519"
 )
 
-// basePoint is the x coordinate of the generator of the curve.
-var basePoint = []byte{9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
 func ScalarMult(in, base []byte) ([]byte, error) {
 	return curve25519.X25519(in, base)
 }
 
 func ScalarBaseMult(in []byte) ([]byte, error) {
-	return ScalarMult(in, basePoint)
+	return ScalarMult(in, curve25519.Basepoint)
 }
--- internal/crypto/curve25519/curve25519_test.go
+++ internal/crypto/curve25519/curve25519_test.go
@@ -7,23 +7,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const expectedHex = "89161fde887b2b53de549af483940106ecc114d6982daa98256de23bdf77661a"
-
 func TestScalarBaseMult(t *testing.T) {
-	var (
-		out []byte
-		err error
-	)
-	in := make([]byte, 32)
-	in[0] = 1
+	x := make([]byte, 32)
+	x[0] = 1
+
 	for i := 0; i < 200; i++ {
-		out, err = ScalarBaseMult(in)
-		require.NoError(t, err)
-		in, out = out, in
+		var err error
+		x, err = ScalarBaseMult(x)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
-	result := hex.EncodeToString(in)
+
+	result := hex.EncodeToString(x)
+	const expectedHex = "89161fde887b2b53de549af483940106ecc114d6982daa98256de23bdf77661a"
 	require.Equal(t, expectedHex, result)
-	// key exchange
+}
+
+func TestKeyExchange(t *testing.T) {
 	cPri := make([]byte, 32)
 	cPri[0] = 199
 	cPub, err := ScalarBaseMult(cPri)
@@ -38,6 +39,7 @@ func TestScalarBaseMult(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, cKey, sKey)
 	t.Log(cKey)
+
 	// invalid in size
 	cPub, err = ScalarBaseMult(nil)
 	require.Error(t, err)
