a3924f4630e82fb8c86952a6590d1b00aed61fe3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-11-19 20:26:31 +0800

    proxy/pool.go: reduce the cyclomatic complexity of the function Pool.add()

--- internal/proxy/pool.go
+++ internal/proxy/pool.go
@@ -44,77 +44,21 @@ func (p *Pool) add(client *Client) error {
 	if client.Tag == ModeDirect {
 		return errors.New("direct is the reserve proxy client tag")
 	}
+	var err error
 	switch client.Mode {
 	case ModeSocks:
-		opts := new(socks.Options)
-		if client.Options != "" {
-			err := toml.Unmarshal([]byte(client.Options), opts)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-		}
-		c, err := socks.NewClient(client.Network, client.Address, opts)
-		if err != nil {
-			return err
-		}
-		client.client = c
+		err = p.addSocks(client)
 	case ModeHTTP:
-		opts := new(http.Options)
-		if client.Options != "" {
-			err := toml.Unmarshal([]byte(client.Options), opts)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-		}
-		c, err := http.NewClient(client.Network, client.Address, opts)
-		if err != nil {
-			return err
-		}
-		client.client = c
+		err = p.addHTTP(client)
 	case ModeChain:
-		tags := struct {
-			Tags []string `toml:"tags"`
-		}{} // client tags
-		err := toml.Unmarshal([]byte(client.Options), &tags)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		var clients []*Client
-		for i := 0; i < len(tags.Tags); i++ {
-			client, err := p.Get(tags.Tags[i])
-			if err != nil {
-				return err
-			}
-			clients = append(clients, client)
-		}
-		c, err := NewChain(client.Tag, clients...)
-		if err != nil {
-			return err
-		}
-		client.client = c
+		err = p.addChain(client)
 	case ModeBalance:
-		tags := struct {
-			Tags []string `toml:"tags"`
-		}{} // client tags
-		err := toml.Unmarshal([]byte(client.Options), &tags)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		var clients []*Client
-		for i := 0; i < len(tags.Tags); i++ {
-			client, err := p.Get(tags.Tags[i])
-			if err != nil {
-				return err
-			}
-			clients = append(clients, client)
-		}
-		c, err := NewBalance(client.Tag, clients...)
-		if err != nil {
-			return err
-		}
-		client.client = c
+		err = p.addBalance(client)
 	default:
 		return errors.Errorf("unknown mode %s", client.Mode)
+	}
+	if err != nil {
+		return err
 	}
 	p.rwm.Lock()
 	defer p.rwm.Unlock()
@@ -124,6 +68,86 @@ func (p *Pool) add(client *Client) error {
 	} else {
 		return errors.Errorf("proxy client %s already exists", client.Tag)
 	}
+}
+
+func (p *Pool) addSocks(client *Client) error {
+	opts := new(socks.Options)
+	if client.Options != "" {
+		err := toml.Unmarshal([]byte(client.Options), opts)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	c, err := socks.NewClient(client.Network, client.Address, opts)
+	if err != nil {
+		return err
+	}
+	client.client = c
+	return nil
+}
+
+func (p *Pool) addHTTP(client *Client) error {
+	opts := new(http.Options)
+	if client.Options != "" {
+		err := toml.Unmarshal([]byte(client.Options), opts)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	c, err := http.NewClient(client.Network, client.Address, opts)
+	if err != nil {
+		return err
+	}
+	client.client = c
+	return nil
+}
+
+func (p *Pool) addChain(client *Client) error {
+	tags := struct {
+		Tags []string `toml:"tags"`
+	}{} // client tags
+	err := toml.Unmarshal([]byte(client.Options), &tags)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	var clients []*Client
+	for i := 0; i < len(tags.Tags); i++ {
+		client, err := p.Get(tags.Tags[i])
+		if err != nil {
+			return err
+		}
+		clients = append(clients, client)
+	}
+	c, err := NewChain(client.Tag, clients...)
+	if err != nil {
+		return err
+	}
+	client.client = c
+	return nil
+}
+
+func (p *Pool) addBalance(client *Client) error {
+	tags := struct {
+		Tags []string `toml:"tags"`
+	}{} // client tags
+	err := toml.Unmarshal([]byte(client.Options), &tags)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	var clients []*Client
+	for i := 0; i < len(tags.Tags); i++ {
+		client, err := p.Get(tags.Tags[i])
+		if err != nil {
+			return err
+		}
+		clients = append(clients, client)
+	}
+	c, err := NewBalance(client.Tag, clients...)
+	if err != nil {
+		return err
+	}
+	client.client = c
+	return nil
 }
 
 // Delete is used to delete proxy client
