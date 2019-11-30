39760bd02b61dce621f40833dbcce422c63f8fe2
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-11-30 16:39:35 +0800

    update test data about ProxyClients, DNSServers and TimeSyncerClients

--- testdata/global.go
+++ testdata/global.go
@@ -7,47 +7,63 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"project/internal/dns"
+	"project/internal/logger"
 	"project/internal/proxy"
+	"project/internal/proxy/socks"
 	"project/internal/timesync"
 )
 
-func ProxyClients(t require.TestingT) map[string]*proxy.Client {
-	clients := make(map[string]*proxy.Client)
-	config, err := ioutil.ReadFile("../internal/proxy/testdata/socks_opts.toml")
+// ProxyClientTag is the tag of the proxy client in ProxyClients()
+const ProxyClientTag = "test_socks5"
+
+// ProxyClients is used to deploy a test proxy server
+// and return corresponding proxy client
+func ProxyClients(t require.TestingT) []*proxy.Client {
+	// deploy test proxy server
+	server, err := socks.NewServer("test", logger.Test, nil)
 	require.NoError(t, err)
-	clients["socks5"] = &proxy.Client{
-		Mode:    proxy.ModeSocks,
-		Options: string(config),
-	}
-	config, err = ioutil.ReadFile("../internal/proxy/testdata/http_opts.toml")
+	err = server.ListenAndServe("tcp", "localhost:0")
 	require.NoError(t, err)
-	clients["http"] = &proxy.Client{
-		Mode:    proxy.ModeHTTP,
-		Options: string(config),
+	return []*proxy.Client{
+		{
+			Tag:     ProxyClientTag,
+			Mode:    proxy.ModeSocks,
+			Network: "tcp",
+			Address: server.Address(),
+		},
 	}
-	return clients
 }
 
+// DNSServers is used to provide test DNS servers
 func DNSServers(t require.TestingT) map[string]*dns.Server {
 	servers := make(map[string]*dns.Server)
-	b, err := ioutil.ReadFile("../internal/dns/testdata/server.toml")
-	require.NoError(t, err)
-	err = toml.Unmarshal(b, &servers)
-	require.NoError(t, err)
+	files := [...]string{
+		"../internal/dns/testdata/dns_ds.toml",
+	}
+	for i := 0; i < 3; i++ {
+		b, err := ioutil.ReadFile(files[i])
+		require.NoError(t, err)
+		s := make(map[string]*dns.Server)
+		require.NoError(t, toml.Unmarshal(b, &s))
+		for tag, server := range s {
+			servers[tag] = server
+		}
+	}
 	return servers
 }
 
+// TimeSyncerClients is used to provide test time syncer clients
 func TimeSyncerClients(t require.TestingT) map[string]*timesync.Client {
 	clients := make(map[string]*timesync.Client)
 	config, err := ioutil.ReadFile("../internal/timesync/testdata/http_opts.toml")
 	require.NoError(t, err)
-	clients["http"] = &timesync.Client{
+	clients["test_http"] = &timesync.Client{
 		Mode:   timesync.ModeHTTP,
 		Config: config,
 	}
-	config, err = ioutil.ReadFile("../internal/timesync/testdata/ntp.toml")
+	config, err = ioutil.ReadFile("../internal/timesync/testdata/ntp_opts.toml")
 	require.NoError(t, err)
-	clients["ntp"] = &timesync.Client{
+	clients["test_ntp"] = &timesync.Client{
 		Mode:   timesync.ModeNTP,
 		Config: config,
 	}
--- testdata/listener.go
+++ testdata/listener.go
@@ -1,31 +1,13 @@
 package testdata
 
 import (
-	"io/ioutil"
-
 	"github.com/stretchr/testify/require"
 
 	"project/internal/messages"
-	"project/internal/xnet"
 )
 
 func Listeners(t require.TestingT) []*messages.Listener {
 	var listeners []*messages.Listener
-	config, err := ioutil.ReadFile("../internal/xnet/testdata/tls.toml")
-	require.NoError(t, err)
-	listener := &messages.Listener{
-		Tag:    "tls",
-		Mode:   xnet.ModeTLS,
-		Config: config,
-	}
-	listeners = append(listeners, listener)
-	config, err = ioutil.ReadFile("../internal/xnet/testdata/light.toml")
-	require.NoError(t, err)
-	listener = &messages.Listener{
-		Tag:    "light",
-		Mode:   xnet.ModeLight,
-		Config: config,
-	}
-	listeners = append(listeners, listener)
+
 	return listeners
 }
