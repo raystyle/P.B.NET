eb1caf3d49a5c302bafad3ff42047e0e5cc24ce1
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-12-07 09:44:33 +0800

    xnet/conn.go: bug fix: add defer before c.rwm.Unlock() in Conn.Write()

--- internal/xnet/conn.go
+++ internal/xnet/conn.go
@@ -58,7 +58,7 @@ func (c *Conn) Read(b []byte) (int, error) {
 func (c *Conn) Write(b []byte) (int, error) {
 	n, err := c.Conn.Write(b)
 	c.rwm.Lock()
-	c.rwm.Unlock()
+	defer c.rwm.Unlock()
 	c.sent += n
 	return n, err
 }
