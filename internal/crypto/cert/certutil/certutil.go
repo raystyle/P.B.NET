6147e0413d29dcabe77f03ed6862e9baf89d5c94
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-12-09 23:43:56 +0800

    crypto/cert/certutil: add ParseCertificate, ParsePrivateKey and ParseCertificates

---
+++ internal/crypto/cert/certutil/certutil.go
@@ -0,0 +1,80 @@
+package certutil
+
+import (
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidPEMBlock = errors.New("invalid PEM block")
+)
+
+// ParseCertificate is used to parse certificate from PEM
+func ParseCertificate(pemBlock []byte) (*x509.Certificate, error) {
+	block, _ := pem.Decode(pemBlock)
+	if block == nil {
+		return nil, ErrInvalidPEMBlock
+	}
+	if block.Type != "CERTIFICATE" {
+		return nil, fmt.Errorf("invalid PEM block type: %s", block.Type)
+	}
+	return x509.ParseCertificate(block.Bytes)
+}
+
+// ParsePrivateKey is used to parse private key from PEM
+// it support RSA ECDSA and ED25519
+func ParsePrivateKey(pemBlock []byte) (interface{}, error) {
+	block, _ := pem.Decode(pemBlock)
+	if block == nil {
+		return nil, ErrInvalidPEMBlock
+	}
+	if block.Type != "PRIVATE KEY" {
+		return nil, fmt.Errorf("invalid PEM block type: %s", block.Type)
+	}
+	if key, err := x509.ParsePKCS1PrivateKey(block.Bytes); err == nil {
+		return key, nil
+	}
+	if key, err := x509.ParsePKCS8PrivateKey(block.Bytes); err == nil {
+		switch key := key.(type) {
+		case *rsa.PrivateKey, *ecdsa.PrivateKey, ed25519.PrivateKey:
+			return key, nil
+		default:
+			return nil, errors.New("found unknown private key type in PKCS#8 wrapping")
+		}
+	}
+	if key, err := x509.ParseECPrivateKey(block.Bytes); err == nil {
+		return key, nil
+	}
+	return nil, errors.New("failed to parse private key")
+}
+
+// ParseCertificates is used to parse certificates from PEM
+func ParseCertificates(pemBlock []byte) ([]*x509.Certificate, error) {
+	var (
+		certs []*x509.Certificate
+		block *pem.Block
+	)
+	for {
+		block, pemBlock = pem.Decode(pemBlock)
+		if block == nil {
+			return nil, ErrInvalidPEMBlock
+		}
+		if block.Type != "CERTIFICATE" {
+			return nil, fmt.Errorf("invalid PEM block type: %s", block.Type)
+		}
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		certs = append(certs, cert)
+		if len(pemBlock) == 0 {
+			break
+		}
+	}
+	return certs, nil
+}
--- internal/crypto/cert/certutil/certutil_test.go
+++ internal/crypto/cert/certutil/certutil_test.go
@@ -1,11 +1,69 @@
 package certutil
 
 import (
+	"io/ioutil"
 	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestParseCertificates(t *testing.T) {
+	certPEMBlock, err := ioutil.ReadFile("testdata/certs.pem")
+	require.NoError(t, err)
+	certs, err := ParseCertificates(certPEMBlock)
+	require.NoError(t, err)
+	t.Log(certs[0].Issuer)
+	t.Log(certs[1].Issuer)
+
+	// parse invalid PEM data
+	_, err = ParseCertificates([]byte{0, 1, 2, 3})
+	require.Equal(t, ErrInvalidPEMBlock, err)
+
+	// invalid Type
+	certPEMBlock = []byte(`
+-----BEGIN INVALID TYPE-----
+-----END INVALID TYPE-----
+`)
+	_, err = ParseCertificates(certPEMBlock)
+	require.EqualError(t, err, "invalid PEM block type: INVALID TYPE")
+
+	// invalid certificate data
+	certPEMBlock = []byte(`
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+`)
+	_, err = ParseCertificates(certPEMBlock)
+	require.Error(t, err)
+}
+
+func TestParseCertificate(t *testing.T) {
+	certPEMBlock, err := ioutil.ReadFile("testdata/certs.pem")
+	require.NoError(t, err)
+	cert, err := ParseCertificate(certPEMBlock)
+	require.NoError(t, err)
+	t.Log(cert.Issuer)
+
+	// parse invalid PEM data
+	_, err = ParseCertificate([]byte{0, 1, 2, 3})
+	require.Equal(t, ErrInvalidPEMBlock, err)
+
+	// invalid Type
+	certPEMBlock = []byte(`
+-----BEGIN INVALID TYPE-----
+-----END INVALID TYPE-----
+`)
+	_, err = ParseCertificate(certPEMBlock)
+	require.EqualError(t, err, "invalid PEM block type: INVALID TYPE")
+
+	// invalid certificate data
+	certPEMBlock = []byte(`
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+`)
+	_, err = ParseCertificate(certPEMBlock)
+	require.Error(t, err)
+}
 
 func TestSystemCertPool(t *testing.T) {
 	wg := sync.WaitGroup{}
