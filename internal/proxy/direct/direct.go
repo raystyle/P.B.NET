d3838d76a466a09daf329071bb741d726d67c95b
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-12-14 20:18:22 +0800

    proxy/direct/direct.go: add annotion for all methods of Direct, remove package options

--- internal/proxy/direct/direct.go
+++ internal/proxy/direct/direct.go
@@ -5,42 +5,50 @@ import (
 	"net"
 	"net/http"
 	"time"
-
-	"project/internal/options"
 )
+
+const dialTimeout = 30 * time.Second
 
 // Direct implement internal/proxy.client
 type Direct struct{}
 
+// Dial is used to Dial with default dial timeout
 func (d Direct) Dial(network, address string) (net.Conn, error) {
-	return (&net.Dialer{Timeout: options.DefaultDialTimeout}).Dial(network, address)
+	return (&net.Dialer{Timeout: dialTimeout}).Dial(network, address)
 }
 
+// DialContext is used to DialContext with default dial timeout
 func (d Direct) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
-	return (&net.Dialer{Timeout: options.DefaultDialTimeout}).DialContext(ctx, network, address)
+	return (&net.Dialer{Timeout: dialTimeout}).DialContext(ctx, network, address)
 }
 
+// DialTimeout is used to Dial with timeout, if timeout < 1, use the default dial timeout
 func (d Direct) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
 	if timeout < 1 {
-		timeout = options.DefaultDialTimeout
+		timeout = dialTimeout
 	}
 	return (&net.Dialer{Timeout: timeout}).Dial(network, address)
 }
 
+// Connect is a padding function
 func (d Direct) Connect(_ context.Context, conn net.Conn, _, _ string) (net.Conn, error) {
 	return conn, nil
 }
 
+// HTTP is a padding function
 func (d Direct) HTTP(_ *http.Transport) {}
 
+// Timeout is used to return timeout
 func (d Direct) Timeout() time.Duration {
-	return options.DefaultDialTimeout
+	return dialTimeout
 }
 
+// Server is a padding function
 func (d Direct) Server() (string, string) {
 	return "", ""
 }
 
+// Info is a padding function
 func (d Direct) Info() string {
 	return "direct"
 }
