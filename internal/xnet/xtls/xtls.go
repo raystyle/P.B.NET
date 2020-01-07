f53653970545e9b8b00c96b03c73903edfc99490
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-01-07 20:41:35 +0800

    xnet/xtls: add client.Handshake() in DialContext(), add TestConnWithContext()

--- internal/xnet/xtls/xtls.go
+++ internal/xnet/xtls/xtls.go
@@ -14,7 +14,6 @@ import (
 
 const (
 	defaultDialTimeout = 30 * time.Second
-	defaultNextProto   = "http/1.1"
 )
 
 // Conn is light.Conn
@@ -22,14 +21,12 @@ type Conn = light.Conn
 
 // Server is used to wrap a conn to server side conn
 func Server(ctx context.Context, conn net.Conn, cfg *tls.Config, timeout time.Duration) *Conn {
-	tlsConn := tls.Server(conn, cfg)
-	return light.Server(ctx, tlsConn, timeout)
+	return light.Server(ctx, tls.Server(conn, cfg), timeout)
 }
 
 // Client is used to wrap a conn to client side conn
 func Client(ctx context.Context, conn net.Conn, cfg *tls.Config, timeout time.Duration) *Conn {
-	tlsConn := tls.Client(conn, cfg)
-	return light.Client(ctx, tlsConn, timeout)
+	return light.Client(ctx, tls.Client(conn, cfg), timeout)
 }
 
 // Listen is used to listen a inner listener
@@ -39,14 +36,11 @@ func Listen(
 	config *tls.Config,
 	timeout time.Duration,
 ) (net.Listener, error) {
-	if len(config.NextProtos) == 0 {
-		config.NextProtos = []string{defaultNextProto}
-	}
-	tl, err := tls.Listen(network, address, config)
+	listener, err := tls.Listen(network, address, config)
 	if err != nil {
 		return nil, err
 	}
-	return light.NewListener(tl, timeout), nil
+	return light.NewListener(listener, timeout), nil
 }
 
 // Dial is used to dial a connection with context.Background()
@@ -76,14 +70,11 @@ func DialContext(
 	if dialContext == nil {
 		dialContext = new(net.Dialer).DialContext
 	}
-	dialCtx, cancel := context.WithTimeout(ctx, timeout)
+	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
-	rawConn, err := dialContext(dialCtx, network, address)
+	rawConn, err := dialContext(ctx, network, address)
 	if err != nil {
 		return nil, err
-	}
-	if len(config.NextProtos) == 0 {
-		config.NextProtos = []string{defaultNextProto}
 	}
 	if config.ServerName == "" {
 		colonPos := strings.LastIndex(address, ":")
@@ -95,5 +86,10 @@ func DialContext(
 		c.ServerName = hostname
 		config = c
 	}
-	return light.Client(ctx, tls.Client(rawConn, config), timeout), nil
+	client := light.Client(ctx, tls.Client(rawConn, config), timeout)
+	err = client.Handshake()
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
--- internal/xnet/xtls/xtls_test.go
+++ internal/xnet/xtls/xtls_test.go
@@ -13,11 +13,34 @@ import (
 func TestListenAndDial(t *testing.T) {
 	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
 	if testsuite.IPv4Enabled {
+		listener, err := Listen("tcp4", "localhost:0", serverCfg.Clone(), 0)
+		require.NoError(t, err)
+		address := listener.Addr().String()
+		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+			return Dial("tcp4", address, clientCfg.Clone(), 0, nil)
+		}, true)
+	}
+
+	if testsuite.IPv6Enabled {
+		listener, err := Listen("tcp6", "localhost:0", serverCfg.Clone(), 0)
+		require.NoError(t, err)
+		address := listener.Addr().String()
+		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+			return Dial("tcp6", address, clientCfg.Clone(), 0, nil)
+		}, true)
+	}
+}
+
+func TestListenAndDialContext(t *testing.T) {
+	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
+	if testsuite.IPv4Enabled {
 		listener, err := Listen("tcp4", "localhost:0", serverCfg, 0)
 		require.NoError(t, err)
 		address := listener.Addr().String()
 		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			return Dial("tcp4", address, clientCfg, 0, nil)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			return DialContext(ctx, "tcp4", address, clientCfg, 0, nil)
 		}, true)
 	}
 
@@ -26,12 +49,14 @@ func TestListenAndDial(t *testing.T) {
 		require.NoError(t, err)
 		address := listener.Addr().String()
 		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			return Dial("tcp6", address, clientCfg, 0, nil)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			return DialContext(ctx, "tcp6", address, clientCfg, 0, nil)
 		}, true)
 	}
 }
 
-func TestConn(t *testing.T) {
+func TestConnWithBackground(t *testing.T) {
 	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
 	clientCfg.ServerName = "localhost"
 
@@ -48,4 +73,32 @@ func TestConn(t *testing.T) {
 	testsuite.ConnCS(t, server, client, false)
 	testsuite.IsDestroyed(t, server)
 	testsuite.IsDestroyed(t, client)
+}
+
+func TestConnSCWithContext(t *testing.T) {
+	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
+	clientCfg.ServerName = "localhost"
+
+	server, client := net.Pipe()
+	sCtx, sCancel := context.WithCancel(context.Background())
+	defer sCancel()
+	server = Server(sCtx, server, serverCfg, 0)
+	cCtx, cCancel := context.WithCancel(context.Background())
+	defer cCancel()
+	client = Client(cCtx, client, clientCfg, 0)
+	testsuite.ConnSC(t, server, client, false)
+}
+
+func TestConnCSWithContext(t *testing.T) {
+	serverCfg, clientCfg := testsuite.TLSConfigPair(t)
+	clientCfg.ServerName = "localhost"
+
+	server, client := net.Pipe()
+	sCtx, sCancel := context.WithCancel(context.Background())
+	defer sCancel()
+	server = Server(sCtx, server, serverCfg, 0)
+	cCtx, cCancel := context.WithCancel(context.Background())
+	defer cCancel()
+	client = Client(cCtx, client, clientCfg, 0)
+	testsuite.ConnCS(t, client, server, false)
 }
