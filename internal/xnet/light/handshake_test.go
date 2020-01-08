08b42e7500428bd291b17020a97ae4798217447a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-01-08 20:22:14 +0800

    xnet/light: add handshake_test.go, improve TestListenAndDial() and TestListenAndDialContext()

---
+++ internal/xnet/light/handshake_test.go
@@ -0,0 +1,13 @@
+package light
+
+import (
+	"testing"
+)
+
+func TestConn_clientHandshake(t *testing.T) {
+
+}
+
+func TestConn_serverHandshake(t *testing.T) {
+
+}
--- internal/xnet/light/light_test.go
+++ internal/xnet/light/light_test.go
@@ -10,71 +10,46 @@ import (
 	"project/internal/testsuite"
 )
 
-// prevent block in Dial()
-type tListener struct {
-	net.Listener
-}
-
-func (l *tListener) Accept() (net.Conn, error) {
-	conn, err := l.Listener.Accept()
-	if err != nil {
-		return nil, err
-	}
-	err = conn.(*Conn).Handshake()
-	if err != nil {
-		return nil, err
-	}
-	return conn, err
-}
-
 func TestListenAndDial(t *testing.T) {
-	if testsuite.IPv4Enabled {
-		const network = "tcp4"
-		listener, err := Listen(network, "127.0.0.1:0", 0)
-		require.NoError(t, err)
-		listener = &tListener{Listener: listener}
-		address := listener.Addr().String()
-		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			return Dial(network, address, 0, nil)
-		}, true)
-	}
+	gm := testsuite.MarkGoRoutines(t)
+	defer gm.Compare()
 
-	if testsuite.IPv6Enabled {
-		const network = "tcp6"
-		listener, err := Listen(network, "[::1]:0", 0)
-		require.NoError(t, err)
-		listener = &tListener{Listener: listener}
-		address := listener.Addr().String()
-		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			return Dial(network, address, 0, nil)
-		}, true)
+	if testsuite.IPv4Enabled {
+		testListenAndDial(t, "tcp4")
 	}
+	if testsuite.IPv6Enabled {
+		testListenAndDial(t, "tcp6")
+	}
+}
+
+func testListenAndDial(t *testing.T, network string) {
+	listener, err := Listen(network, "localhost:0", 0)
+	require.NoError(t, err)
+	address := listener.Addr().String()
+	testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+		return Dial(network, address, 0, nil)
+	}, true)
 }
 
 func TestListenAndDialContext(t *testing.T) {
-	if testsuite.IPv4Enabled {
-		const network = "tcp4"
-		listener, err := Listen(network, "127.0.0.1:0", 0)
-		require.NoError(t, err)
-		listener = &tListener{Listener: listener}
-		address := listener.Addr().String()
-		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-			return DialContext(ctx, network, address, 0, nil)
-		}, true)
-	}
+	gm := testsuite.MarkGoRoutines(t)
+	defer gm.Compare()
 
-	if testsuite.IPv6Enabled {
-		const network = "tcp6"
-		listener, err := Listen(network, "[::1]:0", 0)
-		require.NoError(t, err)
-		listener = &tListener{Listener: listener}
-		address := listener.Addr().String()
-		testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-			return DialContext(ctx, network, address, 0, nil)
-		}, true)
+	if testsuite.IPv4Enabled {
+		testListenAndDialContext(t, "tcp4")
 	}
+	if testsuite.IPv6Enabled {
+		testListenAndDialContext(t, "tcp6")
+	}
+}
+
+func testListenAndDialContext(t *testing.T, network string) {
+	listener, err := Listen(network, "localhost:0", 0)
+	require.NoError(t, err)
+	address := listener.Addr().String()
+	testsuite.ListenerAndDial(t, listener, func() (net.Conn, error) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		return DialContext(ctx, network, address, 0, nil)
+	}, true)
 }
