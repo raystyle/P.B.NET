2dc53c4eeb6482fa230bc320f487112d4b96fa0b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-01-18 14:39:32 +0800

    testsuite/mock: refactor mockListener, add mockResponseWriter and mockConn

--- internal/testsuite/mock.go
+++ internal/testsuite/mock.go
@@ -1,8 +1,12 @@
 package testsuite
 
 import (
+	"bufio"
 	"errors"
+	"io"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,24 +28,33 @@ func (mockListenerAddr) String() string {
 	return "mockListenerAddr-String"
 }
 
-type mockListenerWithError struct{}
-
-func (ml mockListenerWithError) Accept() (net.Conn, error) {
-	return nil, ErrMockListener
+type mockListener struct {
+	error bool
+	panic bool
 }
 
-func (mockListenerWithError) Close() error {
+func (l mockListener) Accept() (net.Conn, error) {
+	if l.error {
+		return nil, ErrMockListener
+	}
+	if l.panic {
+		panic(MockListenerPanic)
+	}
+	return nil, nil
+}
+
+func (l mockListener) Close() error {
 	return nil
 }
 
-func (mockListenerWithError) Addr() net.Addr {
+func (l mockListener) Addr() net.Addr {
 	return new(mockListenerAddr)
 }
 
 // NewMockListenerWithError is used to create a mock listener
 // that return a custom error call Accept()
 func NewMockListenerWithError() net.Listener {
-	return new(mockListenerWithError)
+	return &mockListener{error: true}
 }
 
 // IsMockListenerError is used to confirm err is ErrMockListener
@@ -49,28 +62,82 @@ func IsMockListenerError(t testing.TB, err error) {
 	require.Equal(t, ErrMockListener, err)
 }
 
-type mockListenerWithPanic struct{}
-
-func (ml mockListenerWithPanic) Accept() (net.Conn, error) {
-	defer func() { panic(MockListenerPanic) }()
-	return nil, nil
-}
-
-func (mockListenerWithPanic) Close() error {
-	return nil
-}
-
-func (mockListenerWithPanic) Addr() net.Addr {
-	return new(mockListenerAddr)
-}
-
 // NewMockListenerWithPanic is used to create a mock listener
 // that panic when call Accept()
 func NewMockListenerWithPanic() net.Listener {
-	return new(mockListenerWithPanic)
+	return &mockListener{panic: true}
 }
 
 // IsMockListenerPanic is used to confirm err.Error() is MockListenerPanic
 func IsMockListenerPanic(t testing.TB, err error) {
 	require.Contains(t, err.Error(), MockListenerPanic)
+}
+
+type mockResponseWriter struct {
+	hijack bool
+	conn   net.Conn
+}
+
+func (mockResponseWriter) Header() http.Header {
+	return nil
+}
+
+func (mockResponseWriter) Write([]byte) (int, error) {
+	return 0, nil
+}
+
+func (mockResponseWriter) WriteHeader(int) {}
+
+func (rw mockResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if rw.hijack {
+		return nil, nil, errors.New("failed to hijack")
+	}
+	return rw.conn, nil, nil
+}
+
+type mockConn struct {
+	net.Conn
+	server net.Conn
+}
+
+func (c mockConn) Read([]byte) (int, error) {
+	defer func() { panic("Read() panic") }()
+	return 0, nil
+}
+
+func (c mockConn) Close() error {
+	defer func() { panic("Close() panic") }()
+	_ = c.Conn.Close()
+	_ = c.server.Close()
+	return nil
+}
+
+// NewMockResponseWriterWithFailedToHijack is used to create a mock
+// http.ResponseWriter that implemented http.Hijacker, if call Hijack()
+// it will return an error
+func NewMockResponseWriterWithFailedToHijack() http.ResponseWriter {
+	return &mockResponseWriter{hijack: true}
+}
+
+// NewMockResponseWriterWithFailedToWrite is used to create a mock
+// http.ResponseWriter that implemented http.Hijacker, if use hijacked
+// connection, it will return an error
+func NewMockResponseWriterWithFailedToWrite() http.ResponseWriter {
+	server, client := net.Pipe()
+	_ = client.Close()
+	_ = server.Close()
+	return &mockResponseWriter{conn: client}
+}
+
+// NewMockResponseWriterWithMockConn is used to create a mock
+// http.ResponseWriter that implemented http.Hijacker, if use hijacked
+// connection, call Read() or Close(), it will panic
+func NewMockResponseWriterWithMockConn() http.ResponseWriter {
+	server, client := net.Pipe()
+	go func() { _, _ = io.Copy(ioutil.Discard, server) }()
+	mc := mockConn{
+		Conn:   client,
+		server: server,
+	}
+	return &mockResponseWriter{conn: &mc}
 }
--- internal/testsuite/mock_test.go
+++ internal/testsuite/mock_test.go
@@ -1,1 +1,23 @@
 package testsuite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockListener_Accept(t *testing.T) {
+	listener := new(mockListener)
+	conn, err := listener.Accept()
+	require.Nil(t, conn)
+	require.Nil(t, err)
+}
+
+func TestMockResponseWriter(t *testing.T) {
+	rw := new(mockResponseWriter)
+	require.Nil(t, rw.Header())
+	n, err := rw.Write(nil)
+	require.Equal(t, 0, n)
+	require.Nil(t, err)
+	rw.WriteHeader(0)
+}
