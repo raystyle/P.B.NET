201fa9be0e3427feda66558ccdcf7983f34c12aa
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-01-19 19:22:33 +0800

    proxy: delete useless testdata, add socks4.toml, update testGenerateProxyGroup()

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -1,11 +1,9 @@
 package proxy
 
 import (
-	"io/ioutil"
 	"testing"
 	"time"
 
-	"github.com/pelletier/go-toml"
 	"github.com/stretchr/testify/require"
 
 	"project/internal/logger"
@@ -22,7 +20,7 @@ func (g groups) Clients() []*Client {
 	for ri := 0; ri < 3+random.Int(10); ri++ {
 		i := 0
 		for _, group := range g {
-			if i < 4 {
+			if i < 5 {
 				clients[i] = group.client
 			} else {
 				break
@@ -55,119 +53,149 @@ func (g *group) Close(t *testing.T) {
 func testGenerateProxyGroup(t *testing.T) groups {
 	groups := make(map[string]*group)
 
-	// add socks5
-	socksOpts := &socks.Options{
+	const (
+		tag      = "test"
+		network  = "tcp"
+		sAddress = "localhost:0"
+	)
+
+	// add socks5 server
+	socks5Opts := &socks.Options{
 		Username: "admin1",
 		Password: "1234561",
 	}
-	socks5Server, err := socks.NewServer("test", logger.Test, socksOpts)
+	socks5Server, err := socks.NewSocks5Server(tag, logger.Test, socks5Opts)
 	require.NoError(t, err)
-	require.NoError(t, socks5Server.ListenAndServe("tcp", "localhost:0"))
-	address := socks5Server.Address()
-	socks5Client, err := socks.NewClient("tcp", address, socksOpts)
+	go func() {
+		err := socks5Server.ListenAndServe(network, sAddress)
+		require.NoError(t, err)
+	}()
+
+	// add socks4a server
+	socks4aOpts := &socks.Options{
+		UserID: "admin2",
+	}
+	socks4aServer, err := socks.NewSocks4aServer(tag, logger.Test, socks4aOpts)
+	require.NoError(t, err)
+	go func() {
+		err := socks4aServer.ListenAndServe(network, sAddress)
+		require.NoError(t, err)
+	}()
+
+	// add socks4 server
+	socks4Opts := &socks.Options{
+		UserID: "admin3",
+	}
+	socks4Server, err := socks.NewSocks4Server(tag, logger.Test, socks4Opts)
+	require.NoError(t, err)
+	go func() {
+		err := socks4Server.ListenAndServe(network, sAddress)
+		require.NoError(t, err)
+	}()
+
+	// add http proxy server
+	httpOpts := &http.Options{
+		Username: "admin4",
+		Password: "1234564",
+	}
+	httpServer, err := http.NewHTTPServer(tag, logger.Test, httpOpts)
+	require.NoError(t, err)
+	go func() {
+		err := httpServer.ListenAndServe(network, sAddress)
+		require.NoError(t, err)
+	}()
+
+	// add https proxy server
+	serverCfg, clientCfg := testsuite.TLSConfigOptionPair(t)
+	httpsOpts := &http.Options{
+		Username: "admin5",
+		Password: "1234565",
+	}
+	httpsOpts.Server.TLSConfig = serverCfg
+	httpsServer, err := http.NewHTTPSServer(tag, logger.Test, httpsOpts)
+	require.NoError(t, err)
+	go func() {
+		err := httpsServer.ListenAndServe(network, sAddress)
+		require.NoError(t, err)
+	}()
+
+	// wait Serve
+	time.Sleep(250 * time.Millisecond)
+
+	// add socks5 client
+	address := socks5Server.Addresses()[0].String()
+	socks5Client, err := socks.NewSocks5Client(network, address, socks5Opts)
 	require.NoError(t, err)
 	groups["socks5"] = &group{
 		server: socks5Server,
 		client: &Client{
 			Tag:     "socks5-c",
-			Mode:    ModeSocks,
-			Network: "tcp",
+			Mode:    ModeSocks5,
+			Network: network,
 			Address: address,
 			client:  socks5Client,
 		},
 	}
 
-	// add socks4a
-	socksOpts = &socks.Options{
-		Socks4: true,
-		UserID: "admin2",
-	}
-	socks4aServer, err := socks.NewServer("test", logger.Test, socksOpts)
-	require.NoError(t, err)
-	require.NoError(t, socks4aServer.ListenAndServe("tcp", "localhost:0"))
-	address = socks4aServer.Address()
-	socks4aClient, err := socks.NewClient("tcp", address, socksOpts)
+	// add socks4a client
+	address = socks4aServer.Addresses()[0].String()
+	socks4aClient, err := socks.NewSocks4aClient(network, address, socks4aOpts)
 	require.NoError(t, err)
 	groups["socks4a"] = &group{
 		server: socks4aServer,
 		client: &Client{
 			Tag:     "socks4a-c",
-			Mode:    ModeSocks,
-			Network: "tcp",
+			Mode:    ModeSocks4a,
+			Network: network,
 			Address: address,
 			client:  socks4aClient,
 		},
 	}
 
-	// add http
-	httpOpts := &http.Options{
-		Username: "admin3",
-		Password: "1234563",
-	}
-	httpServer, err := http.NewServer("test", logger.Test, httpOpts)
+	// add socks4 client
+	address = socks4Server.Addresses()[0].String()
+	socks4Client, err := socks.NewSocks4Client(network, address, socks4Opts)
 	require.NoError(t, err)
-	require.NoError(t, httpServer.ListenAndServe("tcp", "localhost:0"))
-	address = httpServer.Address()
-	httpClient, err := http.NewClient("tcp", address, httpOpts)
+	groups["socks4"] = &group{
+		server: socks4Server,
+		client: &Client{
+			Tag:     "socks4-c",
+			Mode:    ModeSocks4,
+			Network: network,
+			Address: address,
+			client:  socks4Client,
+		},
+	}
+
+	// add http proxy client
+	address = httpServer.Addresses()[0].String()
+	httpClient, err := http.NewHTTPClient(network, address, httpOpts)
 	require.NoError(t, err)
 	groups["http"] = &group{
 		server: httpServer,
 		client: &Client{
 			Tag:     "http-c",
 			Mode:    ModeHTTP,
-			Network: "tcp",
+			Network: network,
 			Address: address,
 			client:  httpClient,
 		},
 	}
 
-	// add https
-	serverCfg, clientCfg := testsuite.TLSConfigOptionPair(t)
-	httpOpts = &http.Options{
-		HTTPS:    true,
-		Username: "admin4",
-		Password: "1234564",
-	}
-	httpOpts.Server.TLSConfig = serverCfg
-	httpsServer, err := http.NewServer("test", logger.Test, httpOpts)
-	require.NoError(t, err)
-	require.NoError(t, httpsServer.ListenAndServe("tcp", "localhost:0"))
-	address = httpsServer.Address()
-	httpOpts.TLSConfig = clientCfg
-	httpsClient, err := http.NewClient("tcp", address, httpOpts)
+	// add https proxy client
+	address = httpsServer.Addresses()[0].String()
+	httpsOpts.TLSConfig = clientCfg
+	httpsClient, err := http.NewHTTPSClient(network, address, httpsOpts)
 	require.NoError(t, err)
 	groups["https"] = &group{
 		server: httpsServer,
 		client: &Client{
 			Tag:     "https-c",
-			Mode:    ModeHTTP,
-			Network: "tcp",
+			Mode:    ModeHTTPS,
+			Network: network,
 			Address: address,
 			client:  httpsClient,
 		},
 	}
 	return groups
-}
-
-func TestSocksOptions(t *testing.T) {
-	data, err := ioutil.ReadFile("testdata/socks_opts.toml")
-	require.NoError(t, err)
-	socksOpts := socks.Options{}
-	require.NoError(t, toml.Unmarshal(data, &socksOpts))
-
-	testdata := [...]*struct {
-		expected interface{}
-		actual   interface{}
-	}{
-		{expected: true, actual: socksOpts.Socks4},
-		{expected: "admin", actual: socksOpts.Username},
-		{expected: "123456", actual: socksOpts.Password},
-		{expected: time.Minute, actual: socksOpts.Timeout},
-		{expected: "test", actual: socksOpts.UserID},
-		{expected: true, actual: socksOpts.DisableSocks4A},
-		{expected: 100, actual: socksOpts.MaxConns},
-	}
-	for _, td := range testdata {
-		require.Equal(t, td.expected, td.actual)
-	}
 }
