3c147ffd9e122338454e578ceaacd755d6d78556
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-01-25 21:48:58 +0800

    protocol/certificate: improve controller certificate, remove node address, use PublicKey

---
+++ internal/protocol/certificate.go
@@ -0,0 +1,149 @@
+package protocol
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net"
+
+	"project/internal/crypto/ed25519"
+	"project/internal/crypto/rand"
+	"project/internal/guid"
+)
+
+const certificateSize = guid.Size + ed25519.PublicKeySize + 2*ed25519.SignatureSize
+
+// Certificate is used to verify node
+type Certificate struct {
+	GUID      []byte // node GUID
+	PublicKey ed25519.PublicKey
+
+	// with Node GUID: common connect node,
+	// client known Node GUID.
+	// with Controller GUID: role register, Controller trust node..
+	// that client don't known Node GUID.
+	Signatures [2][]byte
+}
+
+// Encode is used to encode certificate to bytes
+func (cert *Certificate) Encode() []byte {
+	b := make([]byte, certificateSize)
+	offset := 0
+	copy(b, cert.GUID)
+	offset += guid.Size
+	copy(b[offset:], cert.PublicKey)
+	offset += ed25519.PublicKeySize
+	copy(b[offset:], cert.Signatures[0])
+	offset += ed25519.SignatureSize
+	copy(b[offset:], cert.Signatures[1])
+	return b
+}
+
+// Decode is used to decode bytes to certificate
+func (cert *Certificate) Decode(b []byte) error {
+	if len(b) != certificateSize {
+		return errors.New("invalid certificate size")
+	}
+	// initialize slice
+	cert.GUID = make([]byte, guid.Size)
+	cert.PublicKey = make([]byte, ed25519.PublicKeySize)
+	cert.Signatures[0] = make([]byte, ed25519.SignatureSize)
+	cert.Signatures[1] = make([]byte, ed25519.SignatureSize)
+	// copy
+	begin := 0
+	end := guid.Size
+	copy(cert.GUID, b[begin:end])
+	begin = end
+	end = begin + ed25519.PublicKeySize
+	copy(cert.PublicKey, b[begin:end])
+	begin = end
+	end = begin + ed25519.SignatureSize
+	copy(cert.Signatures[0], b[begin:end])
+	copy(cert.Signatures[1], b[end:])
+	return nil
+}
+
+// VerifySignatureWithNodeGUID is used to verify node
+// certificate signature with Node GUID
+func (cert *Certificate) VerifySignatureWithNodeGUID(pub ed25519.PublicKey) bool {
+	msg := make([]byte, guid.Size+ed25519.PublicKeySize)
+	copy(msg, cert.GUID)
+	copy(msg[guid.Size:], cert.PublicKey)
+	return ed25519.Verify(pub, msg, cert.Signatures[0])
+}
+
+// VerifySignatureWithCTRLGUID is used to verify node
+// certificate signature with Controller GUID
+func (cert *Certificate) VerifySignatureWithCTRLGUID(pub ed25519.PublicKey) bool {
+	msg := make([]byte, guid.Size+ed25519.PublicKeySize)
+	copy(msg, CtrlGUID)
+	copy(msg[guid.Size:], cert.PublicKey)
+	return ed25519.Verify(pub, msg, cert.Signatures[1])
+}
+
+// IssueCertificate is used to issue node certificate,
+// use controller private key to sign generated certificate
+func IssueCertificate(cert *Certificate, pri ed25519.PrivateKey) error {
+	if len(cert.GUID) != guid.Size {
+		return errors.New("invalid guid size")
+	}
+	if len(cert.PublicKey) != ed25519.PublicKeySize {
+		return errors.New("invalid public key size")
+	}
+	msg := make([]byte, guid.Size+ed25519.PublicKeySize)
+	// sign with Node GUID
+	copy(msg, cert.GUID)
+	copy(msg[guid.Size:], cert.PublicKey)
+	cert.Signatures[0] = ed25519.Sign(pri, msg)
+	// sign with Controller GUID
+	copy(msg, CtrlGUID)
+	cert.Signatures[1] = ed25519.Sign(pri, msg)
+	return nil
+}
+
+// VerifyCertificate is used to verify node certificate
+// if errors != nil, role must log with level Exploit
+func VerifyCertificate(conn net.Conn, pub ed25519.PublicKey, guid []byte) (bool, error) {
+	// receive node certificate
+	buf := make([]byte, certificateSize)
+	_, err := io.ReadFull(conn, buf)
+	if err != nil {
+		return false, nil
+	}
+	var cert Certificate
+	_ = cert.Decode(buf) // no error
+	// verify certificate signature
+	var ok bool
+	if bytes.Compare(CtrlGUID, guid) == 0 {
+		ok = cert.VerifySignatureWithCTRLGUID(pub)
+	} else {
+		// verify node guid
+		if bytes.Compare(cert.GUID, guid) != 0 {
+			return false, errors.New("guid in certificate is different")
+		}
+		ok = cert.VerifySignatureWithNodeGUID(pub)
+	}
+	if !ok {
+		return false, errors.New("invalid certificate signature")
+	}
+	// send challenge to verify public key
+	challenge := buf[ed25519.SignatureSize : ed25519.SignatureSize+32]
+	_, err = io.ReadFull(rand.Reader, challenge)
+	if err != nil {
+		return false, nil
+	}
+	_, err = conn.Write(challenge)
+	if err != nil {
+		return false, nil
+	}
+	// receive challenge signature
+	signature := buf[:ed25519.SignatureSize]
+	_, err = io.ReadFull(conn, signature)
+	if err != nil {
+		return false, nil
+	}
+	if !ed25519.Verify(cert.PublicKey, challenge, signature) {
+		return false, errors.New("invalid challenge signature")
+	}
+	return true, nil
+}
---
+++ internal/protocol/certificate_test.go
@@ -0,0 +1,280 @@
+package protocol
+
+import (
+	"bytes"
+	"io"
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/crypto/ed25519"
+	"project/internal/crypto/rand"
+	"project/internal/guid"
+	"project/internal/testsuite"
+)
+
+func TestIssueCertificate(t *testing.T) {
+	// generate Controller private key
+	privateKey, err := ed25519.GenerateKey()
+	require.NoError(t, err)
+	nodeGUID := bytes.Repeat([]byte{2}, guid.Size)
+
+	// issue a Node certificate
+	cert := Certificate{
+		GUID:      nodeGUID,
+		PublicKey: bytes.Repeat([]byte{3}, ed25519.PublicKeySize),
+	}
+	err = IssueCertificate(&cert, privateKey)
+	require.NoError(t, err)
+
+	// print certificate signature
+	require.Equal(t, ed25519.SignatureSize, len(cert.Signatures[0]))
+	require.Equal(t, ed25519.SignatureSize, len(cert.Signatures[1]))
+	t.Log("signature:", cert.Signatures)
+
+	t.Run("invalid guid size", func(t *testing.T) {
+		err = IssueCertificate(new(Certificate), nil)
+		require.EqualError(t, err, "invalid guid size")
+	})
+
+	t.Run("invalid public key size", func(t *testing.T) {
+		cert := Certificate{GUID: nodeGUID}
+		err = IssueCertificate(&cert, nil)
+		require.EqualError(t, err, "invalid public key size")
+	})
+}
+
+func TestCertificate_VerifySignature(t *testing.T) {
+	// generate Controller private key
+	privateKey, err := ed25519.GenerateKey()
+	require.NoError(t, err)
+	nodeGUID := bytes.Repeat([]byte{2}, guid.Size)
+
+	// issue a Node certificate
+	cert := Certificate{
+		GUID:      nodeGUID,
+		PublicKey: bytes.Repeat([]byte{3}, ed25519.PublicKeySize),
+	}
+	err = IssueCertificate(&cert, privateKey)
+	require.NoError(t, err)
+
+	// encode and decode
+	certBytes := cert.Encode()
+	require.NoError(t, err)
+	recvCert := Certificate{}
+	err = recvCert.Decode(nil)
+	require.EqualError(t, err, "invalid certificate size")
+	err = recvCert.Decode(certBytes)
+	require.NoError(t, err)
+	require.Equal(t, cert, recvCert)
+
+	t.Run("with node guid", func(t *testing.T) {
+		require.True(t, recvCert.VerifySignatureWithNodeGUID(privateKey.PublicKey()))
+	})
+
+	t.Run("with controller guid", func(t *testing.T) {
+		require.True(t, recvCert.VerifySignatureWithCTRLGUID(privateKey.PublicKey()))
+	})
+}
+
+func TestVerifyCertificate(t *testing.T) {
+	// generate Controller private key
+	ctrlPrivateKey, err := ed25519.GenerateKey()
+	require.NoError(t, err)
+	// generate Node private key
+	nodePrivateKey, err := ed25519.GenerateKey()
+	require.NoError(t, err)
+	nodeGUID := bytes.Repeat([]byte{2}, guid.Size)
+
+	// issue a Node certificate
+	cert := Certificate{
+		GUID:      nodeGUID,
+		PublicKey: nodePrivateKey.PublicKey(),
+	}
+	err = IssueCertificate(&cert, ctrlPrivateKey)
+	require.NoError(t, err)
+	certBytes := cert.Encode()
+	require.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+
+	serverAck := func(conn net.Conn) {
+		defer wg.Done()
+		// send certificate
+		_, err := conn.Write(certBytes)
+		require.NoError(t, err)
+		// receive role challenge data
+		challenge := make([]byte, 32)
+		_, err = io.ReadFull(conn, challenge)
+		require.NoError(t, err)
+		// signature challenge data (ha ha, remember check challenge size)
+		signature := ed25519.Sign(nodePrivateKey, challenge)
+		_, err = conn.Write(signature)
+		require.NoError(t, err)
+	}
+
+	t.Run("with node guid", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, server.Close())
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go serverAck(server)
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.NoError(t, err)
+		require.True(t, ok)
+	})
+
+	t.Run("with ctrl guid", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, server.Close())
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go serverAck(server)
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), CtrlGUID)
+		require.NoError(t, err)
+		require.True(t, ok)
+	})
+
+	t.Run("failed to receive certificate", func(t *testing.T) {
+		server, client := net.Pipe()
+		require.NoError(t, server.Close())
+		defer func() {
+			err := client.Close()
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, nil, nil)
+		require.NoError(t, err)
+		require.False(t, ok)
+	})
+
+	t.Run("different node guid", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, server.Close())
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := server.Write(bytes.Repeat([]byte{0}, certificateSize))
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, nil, nodeGUID)
+		require.EqualError(t, err, "guid in certificate is different")
+		require.False(t, ok)
+	})
+
+	t.Run("invalid certificate signature", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, server.Close())
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cert := make([]byte, certificateSize)
+			copy(cert, nodeGUID)
+			_, err := server.Write(cert)
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.EqualError(t, err, "invalid certificate signature")
+		require.False(t, ok)
+	})
+
+	t.Run("failed to generate challenge data", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, server.Close())
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := server.Write(certBytes)
+			require.NoError(t, err)
+		}()
+
+		// patch
+		patchFunc := func(_ interface{}, _ []byte) (int, error) {
+			return 0, testsuite.ErrMonkey
+		}
+		pg := testsuite.PatchInstanceMethod(rand.Reader, "Read", patchFunc)
+		defer pg.Unpatch()
+
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.NoError(t, err)
+		require.False(t, ok)
+	})
+
+	t.Run("failed to send challenge data", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := server.Write(certBytes)
+			require.NoError(t, err)
+			err = server.Close()
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.NoError(t, err)
+		require.False(t, ok)
+	})
+
+	t.Run("failed to receive challenge signature", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := server.Write(certBytes)
+			require.NoError(t, err)
+			// read challenge data
+			challenge := make([]byte, 32)
+			_, err = io.ReadFull(server, challenge)
+			require.NoError(t, err)
+			err = server.Close()
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.NoError(t, err)
+		require.False(t, ok)
+	})
+
+	t.Run("invalid challenge signature", func(t *testing.T) {
+		server, client := net.Pipe()
+		defer func() {
+			require.NoError(t, client.Close())
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := server.Write(certBytes)
+			require.NoError(t, err)
+			// read challenge data
+			challenge := make([]byte, 32)
+			_, err = io.ReadFull(server, challenge)
+			require.NoError(t, err)
+			_, err = server.Write(bytes.Repeat([]byte{0}, ed25519.SignatureSize))
+			require.NoError(t, err)
+		}()
+		ok, err := VerifyCertificate(client, ctrlPrivateKey.PublicKey(), nodeGUID)
+		require.EqualError(t, err, "invalid challenge signature")
+		require.False(t, ok)
+	})
+
+	wg.Wait()
+}
