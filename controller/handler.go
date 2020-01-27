0dfd9e80a744a2757f5045772bc4bcc01eb2bd58
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-01-27 14:03:34 +0800

    handler.go: improve handler.OnNodeSend() and handler.OnBeaconSend(), add Cancel()

--- controller/handler.go
+++ controller/handler.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"context"
+	"sync"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/vmihailenco/msgpack/v4"
 
 	"project/internal/convert"
@@ -12,6 +16,18 @@ import (
 
 type handler struct {
 	ctx *CTRL
+
+	wg      sync.WaitGroup
+	context context.Context
+	cancel  context.CancelFunc
+}
+
+func newHandler(ctx *CTRL) *handler {
+	h := handler{
+		ctx: ctx,
+	}
+	h.context, h.cancel = context.WithCancel(context.Background())
+	return &h
 }
 
 func (h *handler) logf(l logger.Level, format string, log ...interface{}) {
@@ -21,6 +37,16 @@ func (h *handler) logf(l logger.Level, format string, log ...interface{}) {
 func (h *handler) log(l logger.Level, log ...interface{}) {
 	h.ctx.logger.Println(l, "handler", log...)
 }
+
+func (h *handler) Cancel() {
+	h.cancel()
+}
+
+func (h *handler) Close() {
+	h.ctx = nil
+}
+
+// ----------------------------------------Node Send-----------------------------------------------
 
 func (h *handler) OnNodeSend(send *protocol.Send) {
 	defer func() {
@@ -35,21 +61,74 @@ func (h *handler) OnNodeSend(send *protocol.Send) {
 	}
 	switch convert.BytesToUint32(send.Message[:4]) {
 	case messages.CMDNodeRegisterRequest:
+		h.handleNodeRegisterRequest(send)
+	case messages.CMDBeaconRegisterRequest:
+		h.handleBeaconRegisterRequest(send)
 	case messages.CMDTest:
-		var testMsg []byte
-		err := msgpack.Unmarshal(send.Message[4:], &testMsg)
-		if err != nil {
-			h.logf(logger.Exploit, "node %X send invalid test message: %X",
-				send.RoleGUID, send.Message[4:])
-			return
-		}
-		if h.ctx.Test.NodeSend != nil {
-			h.ctx.Test.NodeSend <- testMsg
-		}
+		h.handleNodeSendTestMessage(send)
 	default:
 		h.logf(logger.Exploit, "node %X send unknown message", send.RoleGUID)
 	}
 }
+
+func (h *handler) handleNodeRegisterRequest(send *protocol.Send) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := xpanic.Error(r, "handler.handleNodeRegisterRequest")
+			h.log(logger.Fatal, err)
+		}
+	}()
+	var nrr messages.NodeRegisterRequest
+	err := msgpack.Unmarshal(send.Message[4:], &nrr)
+	if err != nil {
+		const format = "node %X send invalid node register request: %X"
+		h.logf(logger.Exploit, format, send.RoleGUID, send.Message[4:])
+		return
+	}
+	spew.Dump(nrr)
+}
+
+func (h *handler) handleBeaconRegisterRequest(send *protocol.Send) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := xpanic.Error(r, "handler.handleBeaconRegisterRequest")
+			h.log(logger.Fatal, err)
+		}
+	}()
+	var brr messages.BeaconRegisterRequest
+	err := msgpack.Unmarshal(send.Message[4:], &brr)
+	if err != nil {
+		const format = "node %X send invalid beacon register request: %X"
+		h.logf(logger.Exploit, format, send.RoleGUID, send.Message[4:])
+		return
+	}
+	spew.Dump(brr)
+}
+
+func (h *handler) handleNodeSendTestMessage(send *protocol.Send) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := xpanic.Error(r, "handler.handleNodeSendTestMessage")
+			h.log(logger.Fatal, err)
+		}
+	}()
+	if h.ctx.Test.NodeSend == nil {
+		return
+	}
+	var testMsg []byte
+	err := msgpack.Unmarshal(send.Message[4:], &testMsg)
+	if err != nil {
+		const format = "node %X send invalid test message: %X"
+		h.logf(logger.Exploit, format, send.RoleGUID, send.Message[4:])
+		return
+	}
+	select {
+	case h.ctx.Test.NodeSend <- testMsg:
+	case <-h.context.Done():
+	}
+}
+
+// ---------------------------------------Beacon Send----------------------------------------------
 
 func (h *handler) OnBeaconSend(send *protocol.Send) {
 	defer func() {
@@ -64,17 +143,31 @@ func (h *handler) OnBeaconSend(send *protocol.Send) {
 	}
 	switch convert.BytesToUint32(send.Message[:4]) {
 	case messages.CMDTest:
-		var testMsg []byte
-		err := msgpack.Unmarshal(send.Message[4:], &testMsg)
-		if err != nil {
-			h.logf(logger.Exploit, "beacon %X send invalid test message: %X",
-				send.RoleGUID, send.Message[4:])
-			return
-		}
-		if h.ctx.Test.BeaconSend != nil {
-			h.ctx.Test.BeaconSend <- testMsg
-		}
+		h.handleBeaconSendTestMessage(send)
 	default:
 		h.logf(logger.Exploit, "beacon %X send unknown message", send.RoleGUID)
+	}
+}
+
+func (h *handler) handleBeaconSendTestMessage(send *protocol.Send) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := xpanic.Error(r, "handler.handleBeaconSendTestMessage")
+			h.log(logger.Fatal, err)
+		}
+	}()
+	if h.ctx.Test.BeaconSend == nil {
+		return
+	}
+	var testMsg []byte
+	err := msgpack.Unmarshal(send.Message[4:], &testMsg)
+	if err != nil {
+		const format = "beacon %X send invalid test message: %X"
+		h.logf(logger.Exploit, format, send.RoleGUID, send.Message[4:])
+		return
+	}
+	select {
+	case h.ctx.Test.NodeSend <- testMsg:
+	case <-h.context.Done():
 	}
 }
