e070f032428236eb479c7941687b51d4fe0b396b
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-01-31 17:26:24 +0800

    register: update TestNodeRegister(), add time.AfterFunc, try to connect initial node

--- test/register_test.go
+++ test/register_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -20,12 +21,12 @@ func TestNodeRegister(t *testing.T) {
 	iListener, err := iNode.GetListener(InitialNodeListenerTag)
 	require.NoError(t, err)
 	iAddr := iListener.Addr()
-	bListener := bootstrap.Listener{
+	bListener := &bootstrap.Listener{
 		Mode:    iListener.Mode(),
 		Network: iAddr.Network(),
 		Address: iAddr.String(),
 	}
-	boot, key := generateBootstrap(t, &bListener)
+	boot, key := generateBootstrap(t, bListener)
 
 	// create common node
 	cNodeCfg := generateNodeConfig(t, "Common Node")
@@ -38,17 +39,31 @@ func TestNodeRegister(t *testing.T) {
 	cNode, err := node.New(cNodeCfg)
 	require.NoError(t, err)
 	go func() {
-		err = cNode.Main()
-		require.Error(t, err)
+		err := cNode.Main()
+		require.NoError(t, err)
 	}()
 
 	// read Node register request
 	select {
-	case req := <-ctrl.Test.NodeRegisterRequest:
-		spew.Dump(req)
+	case nrr := <-ctrl.Test.NodeRegisterRequest:
+		spew.Dump(nrr)
+		err = ctrl.AcceptRegisterNode(nrr, false)
+		require.NoError(t, err)
 	case <-time.After(3 * time.Second):
-		t.Fatal("read CTRL.Test.NodeRegisterRequestChannel timeout")
+		t.Fatal("read CTRL.Test.NodeRegisterRequest timeout")
 	}
+
+	timer := time.AfterFunc(10*time.Second, func() {
+		t.Fatal("node register timeout")
+	})
+	cNode.Wait()
+	timer.Stop()
+
+	// try to connect initial node
+	client, err := cNode.NewClient(context.Background(), bListener, iNode.GUID(), nil)
+	require.NoError(t, err)
+	err = client.Connect()
+	require.NoError(t, err)
 
 	// clean
 	cNode.Exit(nil)
