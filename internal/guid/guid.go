77192675777fdceec2e46ae5add9d30b3ec56d3b
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-02-01 21:18:26 +0800

    guid: improve GUID size, add type GUID, add common methods, add TestGUID and BenchmarkGUIDWithMapKey

--- internal/guid/guid.go
+++ internal/guid/guid.go
@@ -2,6 +2,8 @@ package guid
 
 import (
 	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
 	"os"
 	"sync"
 	"time"
@@ -10,18 +12,57 @@ import (
 	"project/internal/random"
 )
 
+// +------------+-------------+----------+------------------+------------+
+// | hash(part) | PID(hashed) |  random  | timestamp(int64) | ID(uint64) |
+// +------------+-------------+----------+------------------+------------+
+// |  20 bytes  |   4 bytes   |  8 bytes |      8 bytes     |  8 bytes   |
+// +------------+-------------+----------+------------------+------------+
+// head = hash + PID
+
 // Size is the generated GUID size
-// GUID = head + 8 bytes(random) + timestamp + add id
-// total size = 28 + 8 + 8 + 8 = 52 Bytes
-const Size int = 52
+const Size int = 20 + 4 + 8 + 8 + 8
+
+// GUID is the generated GUID
+type GUID [Size]byte
+
+// String is used to print GUID with prefix
+//
+// GUID: FD4960D3BE40D9CE66B02949E1E85B9082AA0016C39D3225
+//       2228B5F0502D7F3D94F0000000005E35B700000000000000
+func (guid GUID) String() string {
+	// 13 = len("GUID:  ") + len("      ") + len("\n")
+	dst := make([]byte, Size*2+13)
+	copy(dst, "GUID: ")
+	hex.Encode(dst[6:], guid[:Size/2])
+	copy(dst[6+Size:], "\n      ")
+	hex.Encode(dst[Size+13:], guid[Size/2:])
+	return string(dst)
+}
+
+// Hex is used to encode GUID to a hex string
+//
+// FD4960D3BE40D9CE66B02949E1E85B9082AA0016C39D3225
+// 2228B5F0502D7F3D94F0000000005E35B700000000000000
+func (guid GUID) Hex() string {
+	dst := make([]byte, Size*2+1) // add a "\n"
+	hex.Encode(dst, guid[:Size/2])
+	dst[Size] = 10 // "\n"
+	hex.Encode(dst[Size+1:], guid[Size/2:])
+	return string(dst)
+}
+
+// Timestamp is used to get timestamp in the GUID
+func (guid GUID) Timestamp() int64 {
+	return int64(binary.BigEndian.Uint64(guid[32:40]))
+}
 
 // Generator is a custom GUID generator
 type Generator struct {
 	now        func() time.Time
 	random     *random.Rand
-	head       []byte
-	id         uint64
-	guidQueue  chan []byte
+	head       []byte // hash + PID
+	id         uint64 // self add
+	guidQueue  chan *GUID
 	closeOnce  sync.Once
 	stopSignal chan struct{}
 	wg         sync.WaitGroup
@@ -34,9 +75,9 @@ func New(size int, now func() time.Time) *Generator {
 		stopSignal: make(chan struct{}),
 	}
 	if size < 1 {
-		g.guidQueue = make(chan []byte, 1)
+		g.guidQueue = make(chan *GUID, 1)
 	} else {
-		g.guidQueue = make(chan []byte, size)
+		g.guidQueue = make(chan *GUID, size)
 	}
 	if now != nil {
 		g.now = now
@@ -49,20 +90,19 @@ func New(size int, now func() time.Time) *Generator {
 	for i := 0; i < 4096; i++ {
 		hash.Write(g.random.Bytes(64))
 	}
-	g.head = make([]byte, 0, 28)
-	g.head = append(g.head, hash.Sum(nil)[:24]...)
-	g.head = append(g.head, convert.Int32ToBytes(int32(os.Getpid()))...)
+	g.head = make([]byte, 0, 24)
+	g.head = append(g.head, hash.Sum(nil)[:20]...)
+	hash.Write(convert.Int64ToBytes(int64(os.Getpid())))
+	g.head = append(g.head, hash.Sum(nil)[:4]...)
 	g.wg.Add(1)
 	go g.generate()
 	return &g
 }
 
 // Get is used to get a GUID, if generator closed, Get will return nil
-func (g *Generator) Get() []byte {
+func (g *Generator) Get() *GUID {
 	guid := <-g.guidQueue
-	if len(guid) != 0 { // chan not closed
-		copy(guid[36:44], convert.Int64ToBytes(g.now().Unix()))
-	}
+	copy(guid[32:40], convert.Int64ToBytes(g.now().Unix()))
 	return guid
 }
 
@@ -86,15 +126,15 @@ func (g *Generator) generate() {
 		}
 	}()
 	for {
-		guid := make([]byte, Size)
-		copy(guid, g.head)
-		copy(guid[28:36], g.random.Bytes(8))
+		guid := GUID{}
+		copy(guid[:], g.head)
+		copy(guid[24:32], g.random.Bytes(8))
 		// reserve timestamp
-		copy(guid[44:52], convert.Uint64ToBytes(g.id))
+		copy(guid[40:48], convert.Uint64ToBytes(g.id))
 		select {
 		case <-g.stopSignal:
 			return
-		case g.guidQueue <- guid:
+		case g.guidQueue <- &guid:
 			g.id++
 		}
 	}
--- internal/guid/guid_test.go
+++ internal/guid/guid_test.go
@@ -1,20 +1,56 @@
 package guid
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 
+	"project/internal/convert"
+	"project/internal/random"
 	"project/internal/testsuite"
 )
 
+func TestGUID(t *testing.T) {
+	t.Run("String", func(t *testing.T) {
+		guid := GUID{}
+		copy(guid[Size/2:], bytes.Repeat([]byte{1}, Size/2))
+		buf := bytes.Buffer{}
+		buf.WriteString("GUID: ")
+		buf.WriteString(strings.Repeat("00", Size/2))
+		buf.WriteString("\n      ")
+		buf.WriteString(strings.Repeat("01", Size/2))
+		require.Equal(t, buf.String(), guid.String())
+	})
+
+	t.Run("Hex", func(t *testing.T) {
+		guid := GUID{}
+		copy(guid[Size/2:], bytes.Repeat([]byte{1}, Size/2))
+		buf := bytes.Buffer{}
+		buf.WriteString(strings.Repeat("00", Size/2))
+		buf.WriteString("\n")
+		buf.WriteString(strings.Repeat("01", Size/2))
+		require.Equal(t, buf.String(), guid.Hex())
+	})
+
+	t.Run("Timestamp", func(t *testing.T) {
+		now := time.Now().Unix()
+		guid := GUID{}
+		copy(guid[32:40], convert.Int64ToBytes(now))
+		require.Equal(t, now, guid.Timestamp())
+	})
+}
+
 func TestGenerator(t *testing.T) {
-	t.Run("nil", func(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	t.Run("with no now function", func(t *testing.T) {
 		g := New(16, nil)
 		for i := 0; i < 4; i++ {
-			guid := g.Get()
-			require.Equal(t, Size, len(guid))
+			guid := g.Get()[:]
 			t.Log(guid)
 		}
 		g.Close()
@@ -24,8 +60,7 @@ func TestGenerator(t *testing.T) {
 	t.Run("with now()", func(t *testing.T) {
 		g := New(16, time.Now)
 		for i := 0; i < 4; i++ {
-			guid := g.Get()
-			require.Equal(t, Size, len(guid))
+			guid := g.Get()[:]
 			t.Log(guid)
 		}
 		g.Close()
@@ -35,16 +70,36 @@ func TestGenerator(t *testing.T) {
 	t.Run("zero size", func(t *testing.T) {
 		g := New(0, time.Now)
 		for i := 0; i < 4; i++ {
-			t.Log(g.Get())
+			t.Log(g.Get()[:])
 		}
 		g.Close()
 		// twice
 		g.Close()
 		testsuite.IsDestroyed(t, g)
 	})
+
+	t.Run("panic in generate()", func(t *testing.T) {
+		patchFunc := func(uint64) []byte {
+			panic(testsuite.ErrMonkey)
+		}
+		pg := testsuite.Patch(convert.Uint64ToBytes, patchFunc)
+		go func() {
+			time.Sleep(time.Second)
+			pg.Unpatch()
+		}()
+		g := New(0, time.Now)
+		for i := 0; i < 4; i++ {
+			t.Log(g.Get()[:])
+		}
+		g.Close()
+		testsuite.IsDestroyed(t, g)
+	})
 }
 
 func BenchmarkGenerator_Get(b *testing.B) {
+	gm := testsuite.MarkGoroutines(b)
+	defer gm.Compare()
+
 	g := New(512, nil)
 	b.ReportAllocs()
 	b.ResetTimer()
@@ -54,4 +109,23 @@ func BenchmarkGenerator_Get(b *testing.B) {
 	b.StopTimer()
 	g.Close()
 	testsuite.IsDestroyed(b, g)
+}
+
+func BenchmarkGUIDWithMapKey(b *testing.B) {
+	gm := testsuite.MarkGoroutines(b)
+	defer gm.Compare()
+
+	rand := random.New()
+	key := make([]GUID, b.N)
+	for i := 0; i < b.N; i++ {
+		b := rand.Bytes(Size)
+		copy(key[i][:], b)
+	}
+	m := make(map[GUID]int)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m[key[i]] = i
+	}
+	b.StopTimer()
 }
