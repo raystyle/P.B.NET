d471abe7e312361a99f8df671eb469f6e6cd4eb7
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-02-15 19:18:55 +0800

    module/shell/system: improve sendInterruptSignal() and createCommand(), update TestSystem()

--- internal/module/shell/system_test.go
+++ internal/module/shell/system_test.go
@@ -20,8 +20,12 @@ func TestSystem(t *testing.T) {
 	system, err := NewSystem("", nil, "")
 	require.NoError(t, err)
 
+	// check other interrupt will send this shell
+	pingOnly, err := NewSystem("", nil, "")
+	require.NoError(t, err)
+
 	wg := sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(2)
 	go func() {
 		defer wg.Done()
 		reader := mahonia.NewDecoder("GBK").NewReader(system)
@@ -31,12 +35,38 @@ func TestSystem(t *testing.T) {
 			if err != nil {
 				return
 			}
-			_, err = os.Stdout.Write(buf[:n])
+			data := make([]byte, n)
+			copy(data, buf[:n])
+			_, err = os.Stdout.Write(data)
 			require.NoError(t, err)
 		}
 	}()
-
 	// wait print welcome information
+	time.Sleep(1 * time.Second)
+	fmt.Println()
+
+	go func() {
+		defer wg.Done()
+		reader := mahonia.NewDecoder("GBK").NewReader(pingOnly)
+		buf := make([]byte, 512)
+		for {
+			n, err := reader.Read(buf)
+			if err != nil {
+				return
+			}
+			data := make([]byte, n)
+			copy(data, buf[:n])
+			data = append([]byte("ping only: "), data...)
+			_, err = os.Stdout.Write(data)
+			require.NoError(t, err)
+		}
+	}()
+	// wait print welcome information
+	time.Sleep(1 * time.Second)
+	_, err = pingOnly.Write([]byte("cd..\n"))
+	require.NoError(t, err)
+	_, err = pingOnly.Write([]byte("ping 114.114.114.114 -t\n"))
+	require.NoError(t, err)
 	time.Sleep(1 * time.Second)
 
 	command := []string{
@@ -75,6 +105,7 @@ func TestSystem(t *testing.T) {
 
 		"whoami\n",
 	}
+
 	for _, cmd := range command {
 		switch cmd {
 		case "interrupt":
@@ -82,7 +113,7 @@ func TestSystem(t *testing.T) {
 			require.NoError(t, err)
 			time.Sleep(1 * time.Second)
 		case "ping":
-			_, err = system.Write([]byte("ping 8.8.8.8 -t\n"))
+			_, err = system.Write([]byte("ping 114.114.114.114 -t\n"))
 			require.NoError(t, err)
 			time.Sleep(4 * time.Second)
 		default:
@@ -93,6 +124,13 @@ func TestSystem(t *testing.T) {
 	}
 
 	err = system.Close()
+	require.NoError(t, err)
+
+	// close ping only
+	err = pingOnly.Interrupt()
+	require.NoError(t, err)
+	time.Sleep(1 * time.Second)
+	err = pingOnly.Close()
 	require.NoError(t, err)
 
 	wg.Wait()
--- internal/module/shell/windows.go
+++ internal/module/shell/windows.go
@@ -3,8 +3,11 @@
 package shell
 
 import (
+	"os"
 	"os/exec"
+	"sync"
 	"syscall"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -19,6 +22,9 @@ func Shell(command string) ([]byte, error) {
 	return cmd.CombinedOutput()
 }
 
+// https://docs.microsoft.com/en-us/windows/win32/procthread/process-creation-flags
+const createNewConsole = 0x00000010
+
 func createCommand(path string, args []string) *exec.Cmd {
 	if path == "" {
 		path = "cmd.exe"
@@ -26,31 +32,29 @@ func createCommand(path string, args []string) *exec.Cmd {
 	cmd := exec.Command(path, args...) // #nosec
 	attr := syscall.SysProcAttr{
 		HideWindow:    true,
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+		CreationFlags: createNewConsole,
 	}
 	cmd.SysProcAttr = &attr
 	return cmd
 }
 
-// setHandler is used to hook handle interrupt signal
-var setHandler uintptr
+var (
+	// consoleCtrlHandler is used to hook handle interrupt signal
+	consoleCtrlHandler uintptr
+	// signalChan is used to confirm that has receive the interrupt signal
+	signalChan chan struct{}
+
+	globalMutex sync.Mutex
+)
 
 func init() {
-	setHandler = syscall.NewCallback(setConsoleCtrlHandler)
+	consoleCtrlHandler = syscall.NewCallback(handleConsoleCtrl)
+	signalChan = make(chan struct{}, 1)
 }
 
 // always return true
-func setConsoleCtrlHandler(_ uintptr) uintptr {
-	dll, err := syscall.LoadDLL("kernel32.dll")
-	if err != nil {
-		return uintptr(1)
-	}
-	proc, err := dll.FindProc("SetConsoleCtrlHandler")
-	if err != nil {
-		return uintptr(1)
-	}
-	// self delete handler
-	_, _, _ = proc.Call(setHandler, uintptr(0))
+func handleConsoleCtrl(_ uintptr) uintptr {
+	signalChan <- struct{}{}
 	return uintptr(1)
 }
 
@@ -59,28 +63,76 @@ func sendInterruptSignal(cmd *exec.Cmd) error {
 	if err != nil {
 		return err
 	}
-	// first call SetConsoleCtrlHandler, then call GenerateConsoleCtrlEvent
-	// interrupt signal will not send to this process.
-	// https://docs.microsoft.com/en-us/windows/console/setconsolectrlhandler
-	proc, err := dll.FindProc("SetConsoleCtrlHandler")
+	globalMutex.Lock()
+	defer globalMutex.Unlock()
+	var needAttach bool
+	// Detach self console for attach the process console
+	// https://docs.microsoft.com/en-us/windows/console/freeconsole
+	freeConsole, err := dll.FindProc("FreeConsole")
 	if err != nil {
 		return err
 	}
-	r1, _, err := proc.Call(setHandler, uintptr(1))
+	r1, _, err := freeConsole.Call()
+	if r1 != 0 {
+		needAttach = true
+	}
+
+	// Attach to the console of the process
+	// https://docs.microsoft.com/en-us/windows/console/attachconsole
+	attachConsole, err := dll.FindProc("AttachConsole")
+	if err != nil {
+		return err
+	}
+	pid := cmd.Process.Pid
+	r1, _, err = attachConsole.Call(uintptr(pid))
+	if r1 == 0 {
+		return errors.Errorf("failed to call AttachConsole: %s", err)
+	}
+	// After call SetConsoleCtrlHandler, then call GenerateConsoleCtrlEvent
+	// interrupt signal will not send to this process.
+	// https://docs.microsoft.com/en-us/windows/console/setconsolectrlhandler
+	setConsoleCtrlHandler, err := dll.FindProc("SetConsoleCtrlHandler")
+	if err != nil {
+		return err
+	}
+	r1, _, err = setConsoleCtrlHandler.Call(consoleCtrlHandler, uintptr(1))
 	if r1 == 0 {
 		return errors.Errorf("failed to call SetConsoleCtrlHandler: %s", err)
 	}
 	// Send the CTRL_C_EVENT to a console process group that shares
 	// the console associated with the calling process.
 	// https://docs.microsoft.com/en-us/windows/console/generateconsolectrlevent
-	proc, err = dll.FindProc("GenerateConsoleCtrlEvent")
+	generateConsoleCtrlEvent, err := dll.FindProc("GenerateConsoleCtrlEvent")
 	if err != nil {
 		return err
 	}
-	pid := cmd.Process.Pid
-	r1, _, err = proc.Call(syscall.CTRL_BREAK_EVENT, uintptr(pid))
+	r1, _, err = generateConsoleCtrlEvent.Call(syscall.CTRL_C_EVENT, uintptr(0))
 	if r1 == 0 {
 		return errors.Errorf("failed to call CTRL_C_EVENT: %s", err)
+	}
+	// wait receive the interrupt signal
+	timer := time.NewTimer(5 * time.Second)
+	defer timer.Stop()
+	select {
+	case <-signalChan:
+	case <-timer.C:
+		return errors.New("failed to receive the interrupt signal")
+	}
+	// TODO think it
+	time.Sleep(3 * time.Second)
+	// free attached console
+	r1, _, err = freeConsole.Call()
+	if r1 == 0 {
+		return errors.Errorf("failed to call FreeConsole(self): %s", err)
+	}
+	if !needAttach {
+		return nil
+	}
+	// attache self console
+	ppid := os.Getppid()
+	r1, _, err = attachConsole.Call(uintptr(ppid))
+	if r1 == 0 {
+		return errors.Errorf("failed to call AttachConsole(self): %s", err)
 	}
 	return nil
 }
