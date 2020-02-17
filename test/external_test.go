f2e785e5e0e1f909c2ab79d535d8d9c003ffb0e5
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-02-17 20:16:07 +0800

    external_test.go: add BenchmarkBytes_Compare() and BenchmarkBytes_Equal()

--- test/external_test.go
+++ test/external_test.go
@@ -1,10 +1,13 @@
 package test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/pelletier/go-toml"
 	"github.com/stretchr/testify/require"
+
+	"project/internal/guid"
 )
 
 func TestToml_Negative(t *testing.T) {
@@ -14,4 +17,94 @@ func TestToml_Negative(t *testing.T) {
 	err := toml.Unmarshal([]byte(`Num = -1`), &cfg)
 	require.Error(t, err)
 	require.Equal(t, uint(0), cfg.Num)
+}
+
+// fast
+func BenchmarkBytes_Compare_GUID(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, guid.Size)
+	bb := bytes.Repeat([]byte{0}, guid.Size)
+	benchmarkBytesCompare(b, aa, bb)
+}
+
+// slow
+func BenchmarkBytes_Equal_GUID(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, guid.Size)
+	bb := bytes.Repeat([]byte{0}, guid.Size)
+	benchmarkBytesEqual(b, aa, bb)
+}
+
+// slow
+func BenchmarkBytes_Compare_1Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 1)
+	bb := bytes.Repeat([]byte{0}, 1)
+	benchmarkBytesCompare(b, aa, bb)
+}
+
+// fast
+func BenchmarkBytes_Equal_1Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 1)
+	bb := bytes.Repeat([]byte{0}, 1)
+	benchmarkBytesEqual(b, aa, bb)
+}
+
+// slow
+func BenchmarkBytes_Compare_4Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 4)
+	bb := bytes.Repeat([]byte{0}, 4)
+	benchmarkBytesCompare(b, aa, bb)
+}
+
+// fast
+func BenchmarkBytes_Equal_4Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 4)
+	bb := bytes.Repeat([]byte{0}, 4)
+	benchmarkBytesEqual(b, aa, bb)
+}
+
+// slow
+func BenchmarkBytes_Compare_8Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 8)
+	bb := bytes.Repeat([]byte{0}, 8)
+	benchmarkBytesCompare(b, aa, bb)
+}
+
+// fast
+func BenchmarkBytes_Equal_8Bytes(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 8)
+	bb := bytes.Repeat([]byte{0}, 8)
+	benchmarkBytesEqual(b, aa, bb)
+}
+
+// slow
+func BenchmarkBytes_Compare_10xGUID(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 10*guid.Size)
+	bb := bytes.Repeat([]byte{0}, 10*guid.Size)
+	benchmarkBytesCompare(b, aa, bb)
+}
+
+// fast
+func BenchmarkBytes_Equal_10xGUID(b *testing.B) {
+	aa := bytes.Repeat([]byte{0}, 10*guid.Size)
+	bb := bytes.Repeat([]byte{0}, 10*guid.Size)
+	benchmarkBytesEqual(b, aa, bb)
+}
+
+func benchmarkBytesCompare(b *testing.B, aa, bb []byte) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		bytes.Compare(aa, bb)
+	}
+	b.StopTimer()
+}
+
+func benchmarkBytesEqual(b *testing.B, aa, bb []byte) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		bytes.Equal(aa, bb)
+	}
+	b.StopTimer()
 }
