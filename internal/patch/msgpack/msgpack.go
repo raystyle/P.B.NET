5e1ceb5ffe95fcd441b57d7f5f1b8d6d80eb5985
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-02-22 19:26:42 +0800

    patch/msgpack/msgpack.go: improve Unmarshal(), add go/cert_pool_patch.txt

--- internal/patch/msgpack/msgpack.go
+++ internal/patch/msgpack/msgpack.go
@@ -52,9 +52,8 @@ func Unmarshal(data []byte, v interface{}) error {
 		if strings.Contains(errStr, "unknown field") {
 			name := reflect.TypeOf(v).String()
 			return fmt.Errorf("%s in %s", errStr, name)
-		} else {
-			return err
 		}
+		return err
 	}
 	return nil
 }
