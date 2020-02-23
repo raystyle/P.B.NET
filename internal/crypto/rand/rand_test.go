cfc2f2e92f2109760bac076ddb707e41d245fece
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-02-23 12:44:23 +0800

    crypto/rand/rand_test.go: update TestReader_Read(): add sub test about read failed

--- internal/crypto/rand/rand_test.go
+++ internal/crypto/rand/rand_test.go
@@ -5,14 +5,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"project/internal/patch/monkey"
 )
 
 func TestReader_Read(t *testing.T) {
-	b1 := make([]byte, 32)
-	_, err := io.ReadFull(Reader, b1)
-	require.NoError(t, err)
-	b2 := make([]byte, 32)
-	_, err = io.ReadFull(Reader, b2)
-	require.NoError(t, err)
-	require.NotEqual(t, b1, b2)
+	t.Run("success", func(t *testing.T) {
+		b1 := make([]byte, 32)
+		_, err := io.ReadFull(Reader, b1)
+		require.NoError(t, err)
+		b2 := make([]byte, 32)
+		_, err = io.ReadFull(Reader, b2)
+		require.NoError(t, err)
+		require.NotEqual(t, b1, b2)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		patchFunc := func(_ io.Reader, _ []byte) (int, error) {
+			return 0, monkey.ErrMonkey
+		}
+		pg := monkey.Patch(io.ReadFull, patchFunc)
+		defer pg.Unpatch()
+		_, err := Reader.Read(make([]byte, 1024))
+		monkey.IsMonkeyError(t, err)
+	})
 }
