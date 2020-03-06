bf01e2401cf3ae50d219ce2c31aa30ab0b3779df
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-03-06 20:40:16 +0800

    module/shellcode/sehedule: add sehedule_test.go, add TestDoUseless()

---
+++ internal/module/shellcode/sehedule_test.go
@@ -0,0 +1,32 @@
+package shellcode
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"project/internal/patch/monkey"
+	"project/internal/random"
+)
+
+func TestDoUseless(t *testing.T) {
+	t.Run("panic", func(t *testing.T) {
+		patchFunc := func() *random.Rand {
+			panic("panic about monkey")
+		}
+		pg := monkey.Patch(random.New, patchFunc)
+		defer pg.Unpatch()
+		ch := make(chan []byte, 5120)
+		doUseless(context.Background(), ch)
+	})
+
+	t.Run("ctx.Done()", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(time.Second)
+			cancel()
+		}()
+		ch := make(chan []byte, 16)
+		doUseless(ctx, ch)
+	})
+}
