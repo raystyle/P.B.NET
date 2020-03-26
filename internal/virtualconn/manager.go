592f98e4f6332882a7b885440d45fd1a0d137e83
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-03-26 19:07:25 +0800

    virtualconn/manager.go: add ConnID, LocalPort, finish a part of Listen(), update tests

--- internal/virtualconn/manager.go
+++ internal/virtualconn/manager.go
@@ -2,8 +2,10 @@ package virtualconn
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
+	"net"
 	"sync"
 	"time"
 
@@ -11,13 +13,38 @@ import (
 	"project/internal/random"
 )
 
+// uint32
+const portSize = 4
+
 // ConnID = self GUID(local address) + port(uint32) +
 //          role GUID(remote address) + port(uint32)
-type ConnID [guid.Size + 4 + guid.Size + 4]byte
+type ConnID [guid.Size + portSize + guid.Size + portSize]byte
 
-// LocalGUID is used
-func (cid *ConnID) LocalGUID() {
+// NewConnID is used to create a connection id.
+func NewConnID(local *guid.GUID, lPort uint32, remote *guid.GUID, rPort uint32) *ConnID {
+	cid := ConnID{}
+	copy(cid[:guid.Size], local[:])
+	binary.BigEndian.PutUint32(cid[guid.Size:guid.Size+portSize], lPort)
+	copy(cid[guid.Size+4:], remote[:])
+	binary.BigEndian.PutUint32(cid[2*guid.Size+portSize:], rPort)
+	return &cid
+}
 
+// LocalGUID is used to get local GUID in the connection id.
+func (cid *ConnID) LocalGUID() *guid.GUID {
+	g := guid.GUID{}
+	copy(g[:], cid[:guid.Size])
+	return &g
+}
+
+// LocalPort is used to get local port in the connection id.
+func (cid *ConnID) LocalPort() uint32 {
+	return binary.BigEndian.Uint32(cid[guid.Size : guid.Size+portSize])
+}
+
+// LocalAddr is used to get local address in the connection id.
+func (cid *ConnID) LocalAddr() net.Addr {
+	return newVCAddr(cid.LocalGUID(), cid.LocalPort())
 }
 
 // State is used to show the virtual connection state.
@@ -34,7 +61,7 @@ func (s State) String() string {
 	}
 }
 
-// about state
+// about connection state
 const (
 	StateListen State = 1 + iota
 	StateESTABLISHED
@@ -49,8 +76,9 @@ type conn struct {
 }
 
 type sender struct {
-	roleGUID *guid.GUID
-	port     uint32
+	dstGUID *guid.GUID // only controller need it
+	dstPort uint32
+	srcPort uint32
 }
 
 func (s *sender) Send(ctx context.Context, data []byte) error {
@@ -58,51 +86,102 @@ func (s *sender) Send(ctx context.Context, data []byte) error {
 }
 
 type receiver struct {
+	data chan []byte
 }
 
 func (r *receiver) Receive(ctx context.Context) ([]byte, error) {
-	return nil, nil
+	select {
+	case data := <-r.data:
+		return data, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
 }
+
+func (r *receiver) pushData() {
+
+}
+
+// senderFunc is used to call Role.sender.Send().
+// data include source and destination port.
+// data = src port + dst port + payload
+type senderFunc func(ctx context.Context, guid *guid.GUID, data []byte) error
 
 // Manager is used to manage listeners and dial connection,
 // do keep-alive between connections.
 type Manager struct {
-	now  func() time.Time
+	local  *guid.GUID // self address(role GUID)
+	sender senderFunc
+	now    func() time.Time
+
+	// for select port
 	rand *random.Rand
 
-	// conns include all connections that Dial()
+	// conns include all connections that Listen() and Dial()
+	ports map[uint32]struct{}
 	conns map[ConnID]*conn
-
-	rwm sync.RWMutex
+	rwm   sync.RWMutex
 }
 
 // NewManager is used to create a virtual connection manager.
-func NewManager(now func() time.Time) *Manager {
+func NewManager(local *guid.GUID, sender senderFunc, now func() time.Time) *Manager {
 	manager := Manager{
-		now:  now,
-		rand: random.New(),
+		local:  local,
+		sender: sender,
+		now:    now,
+		rand:   random.New(),
 	}
 	return &manager
 }
 
-// Listen is used to bind and return a Listener and port
-func (m *Manager) Listen(local *guid.GUID) (*Listener, uint32) {
+// Listen is used to bind and return a Listener and port.
+// timeout is used to control Listener.Accept() timeout.
+// only the equal the remote guid can dial this Listener.
+func (m *Manager) Listen(remote *guid.GUID, timeout time.Duration, usage string) (*Listener, uint32) {
+	m.rwm.Lock()
+	defer m.rwm.Unlock()
 
-	return nil, 0
+	// select a random and doesn't exists port
+	var port uint32
+	for {
+		port = uint32(1 + m.rand.Int(1<<32-1))
+		_, ok := m.ports[port]
+		if !ok {
+			break
+		}
+	}
+	cid := NewConnID(m.local, port, remote, 0)
+
+	listener := NewListener(m.local, cid.LocalPort(), timeout)
+	// add to connection pool.
+	m.conns[*cid] = &conn{
+		Closer:   listener,
+		state:    StateListen,
+		usage:    usage,
+		lastUsed: m.now(),
+	}
+	return listener, port
 }
 
 // IncomeConn is used to notice manager that some one Dial,
-func (m *Manager) IncomeConn() {
+func (m *Manager) IncomeConn(remote *guid.GUID, srcPort, dstPort uint32) bool {
 
-}
-
-// Dial is
-func (m *Manager) Dial() {
-
+	return false
 }
 
 // DataArrival is used to push data to a virtual connection
 func (m *Manager) DataArrival() {
+
+}
+
+// Dial is
+// last replace the port
+func (m *Manager) Dial() {
+
+}
+
+// CloseConn is used to kill connection.
+func (m *Manager) CloseConn() {
 
 }
 
--- internal/virtualconn/manager_test.go
+++ internal/virtualconn/manager_test.go
@@ -1,1 +1,25 @@
 package virtualconn
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/convert"
+	"project/internal/guid"
+)
+
+func TestNewConnID(t *testing.T) {
+	local := guid.GUID{}
+	copy(local[:], bytes.Repeat([]byte{1}, guid.Size))
+	remote := guid.GUID{}
+	copy(remote[:], bytes.Repeat([]byte{2}, guid.Size))
+	cid := NewConnID(&local, 3, &remote, 4)
+	except := make([]byte, 0, 2*guid.Size+2*portSize)
+	except = append(except, bytes.Repeat([]byte{1}, guid.Size)...)
+	except = append(except, convert.Uint32ToBytes(3)...)
+	except = append(except, bytes.Repeat([]byte{2}, guid.Size)...)
+	except = append(except, convert.Uint32ToBytes(4)...)
+	require.Equal(t, except, cid[:])
+}
