5c9f1adafde115fe6967d85cc03db0c48b43772c
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-03-26 19:05:37 +0800

    logger: remove function HTTPRequest(), remove tests about HTTPRequest()

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,10 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
 	"time"
 )
 
@@ -227,75 +225,4 @@ func Conn(conn net.Conn) *bytes.Buffer {
 		conn.LocalAddr().Network(), conn.LocalAddr(),
 		conn.RemoteAddr().Network(), conn.RemoteAddr())
 	return &b
-}
-
-const (
-	// post data length in one line
-	bodyLineLength = 64
-
-	// <security> prevent too big resp.Body
-	maxBodyLength = 1024
-)
-
-// HTTPRequest is used to print http.Request.
-//
-// client: 127.0.0.1:1234
-// POST /index HTTP/1.1
-// Host: github.com
-// Accept: text/html
-// Connection: keep-alive
-// User-Agent: Mozilla
-//
-// post data...
-// post data...
-func HTTPRequest(r *http.Request) *bytes.Buffer {
-	buf := new(bytes.Buffer)
-	_, _ = fmt.Fprintf(buf, "client: %s\n", r.RemoteAddr)
-	// request
-	_, _ = fmt.Fprintf(buf, "%s %s %s", r.Method, r.RequestURI, r.Proto)
-	// host
-	_, _ = fmt.Fprintf(buf, "\nHost: %s", r.Host)
-	// header
-	for k, v := range r.Header {
-		_, _ = fmt.Fprintf(buf, "\n%s: %s", k, v[0])
-	}
-	if r.Body != nil {
-		rawBody := new(bytes.Buffer)
-		defer func() {
-			r.Body = ioutil.NopCloser(io.MultiReader(rawBody, r.Body))
-		}()
-		// start print
-		buffer := make([]byte, bodyLineLength)
-		// check body
-		n, err := io.ReadFull(r.Body, buffer)
-		if err != nil {
-			if n == 0 { // no body
-				return buf
-			}
-			// 0 < data size < bodyLineLength
-			_, _ = fmt.Fprintf(buf, "\n\n%s", buffer[:n])
-			rawBody.Write(buffer[:n])
-			return buf
-		}
-		// new line and write data
-		_, _ = fmt.Fprintf(buf, "\n\n%s", buffer)
-		rawBody.Write(buffer)
-		for {
-			if rawBody.Len() > maxBodyLength {
-				break
-			}
-			n, err := io.ReadFull(r.Body, buffer)
-			if err != nil {
-				// write last line
-				if n != 0 {
-					_, _ = fmt.Fprintf(buf, "\n%s", buffer[:n])
-					rawBody.Write(buffer[:n])
-				}
-				break
-			}
-			_, _ = fmt.Fprintf(buf, "\n%s", buffer)
-			rawBody.Write(buffer)
-		}
-	}
-	return buf
 }
--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,15 +1,10 @@
 package logger
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
@@ -92,70 +87,4 @@ func TestConn(t *testing.T) {
 	require.NoError(t, err)
 	fmt.Println(Conn(conn))
 	_ = conn.Close()
-}
-
-func TestHTTPRequest(t *testing.T) {
-	r, err := http.NewRequest(http.MethodGet, "https://github.com/", nil)
-	require.NoError(t, err)
-	r.RemoteAddr = "127.0.0.1:1234"
-	r.RequestURI = "/index"
-	r.Header.Set("User-Agent", "Mozilla")
-	r.Header.Set("Accept", "text/html")
-	r.Header.Set("Connection", "keep-alive")
-
-	fmt.Println("-----begin (GET and no body)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-
-	equalBody := func(b1, b2 io.Reader) {
-		d1, err := ioutil.ReadAll(b1)
-		require.NoError(t, err)
-		d2, err := ioutil.ReadAll(b2)
-		require.NoError(t, err)
-		require.Equal(t, d1, d2)
-	}
-
-	body := new(bytes.Buffer)
-	rawBody := bytes.NewReader(body.Bytes())
-	r.Body = ioutil.NopCloser(body)
-	fmt.Println("-----begin (GET with body but no data)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-	equalBody(rawBody, r.Body)
-
-	body.Reset()
-	body.WriteString(strings.Repeat("a", bodyLineLength-10))
-	rawBody = bytes.NewReader(body.Bytes())
-	r.Body = ioutil.NopCloser(body)
-	fmt.Println("-----begin (POST with data <bodyLineLength)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-	equalBody(rawBody, r.Body)
-
-	body.Reset()
-	body.WriteString(strings.Repeat("a", bodyLineLength))
-	rawBody = bytes.NewReader(body.Bytes())
-	r.Body = ioutil.NopCloser(body)
-	fmt.Println("-----begin (POST with data bodyLineLength)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-	equalBody(rawBody, r.Body)
-
-	body.Reset()
-	body.WriteString(strings.Repeat("a", 3*bodyLineLength-1))
-	rawBody = bytes.NewReader(body.Bytes())
-	r.Body = ioutil.NopCloser(body)
-	fmt.Println("-----begin (POST with data 3*bodyLineLength-1)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-	equalBody(rawBody, r.Body)
-
-	body.Reset()
-	body.WriteString(strings.Repeat("a", 100*bodyLineLength-1))
-	rawBody = bytes.NewReader(body.Bytes())
-	r.Body = ioutil.NopCloser(body)
-	fmt.Println("-----begin (POST with data 100*bodyLineLength-1)-----")
-	fmt.Println(HTTPRequest(r))
-	fmt.Printf("-----end-----\n\n")
-	equalBody(rawBody, r.Body)
 }
