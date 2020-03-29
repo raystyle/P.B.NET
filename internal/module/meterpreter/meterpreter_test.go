3a8d396c558f9d1760a7434030df83cdb0832ae3
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-03-29 13:27:21 +0800

    module/meterpreter: finish a part of ReverseTCP, add TestReverseTCP()

--- internal/module/meterpreter/meterpreter_test.go
+++ internal/module/meterpreter/meterpreter_test.go
@@ -1,1 +1,19 @@
 package meterpreter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/module/shellcode"
+)
+
+const (
+	testNetwork = "tcp"
+	testAddress = "127.0.0.1:8990"
+)
+
+func TestReverseTCP(t *testing.T) {
+	err := ReverseTCP(testNetwork, testAddress, shellcode.MethodCreateThread)
+	require.NoError(t, err)
+}
