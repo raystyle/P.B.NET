c7a84e5c44cfddbf1f7a68841e5fc8b564e1600f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-04-28 22:34:14 +0800

    msfrpc: rename plugins to plugin, remove code about element-ui in main.js

--- web/msfrpc/src/main.js
+++ web/msfrpc/src/main.js
@@ -1,30 +1,23 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
-import ElementUI from "element-ui"
 import Axios from "axios"
 import VueAxios from "vue-axios"
 import Routes from "./router/router"
+import vuetify from "./plugin/vuetify"
 import App from "./App"
 
 import "@mdi/font/css/materialdesignicons.min.css"
 import "material-design-icons-iconfont/dist/material-design-icons.css"
 import "vuetify/dist/vuetify.min.css"
 
-import "element-ui/lib/theme-chalk/index.css"
 import "./page/common/header"
-
-
-import vuetify from "./plugins/vuetify"
-
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(ElementUI);
-
 Vue.use(VueAxios, Axios);
 
-// fix "Avoided redundant navigation to current location:"
+// fix error about "Avoided redundant navigation to current location:"
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
     return originalPush.call(this, location).catch(err => err)
