250219ee1c7ba33f5d2c1d2c308be4ec9864debc
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-05-03 19:46:56 +0800

    testsuite/proxy_test.go: add TestInitHTTPServers() to simulate IPv6 Only network

--- internal/testsuite/proxy_test.go
+++ internal/testsuite/proxy_test.go
@@ -2,7 +2,17 @@ package testsuite
 
 import (
 	"testing"
+
+	"project/internal/nettool"
 )
+
+func TestInitHTTPServers(t *testing.T) {
+	IPv4Enabled = false
+	IPv6Enabled = true
+	defer func() { IPv4Enabled, IPv6Enabled = nettool.IPEnabled() }()
+
+	initHTTPServers(t)
+}
 
 func TestNopCloser(t *testing.T) {
 	closer := NewNopCloser()
