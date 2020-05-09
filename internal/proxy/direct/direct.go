bf8dcdfc182ead753b9450c341037b364b8e2221
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-05-09 11:40:29 +0800

    proxy/direct: update comment, add sub test to TestDirect(), update TestDirect().

--- internal/proxy/direct/direct.go
+++ internal/proxy/direct/direct.go
@@ -9,20 +9,20 @@ import (
 
 const dialTimeout = 30 * time.Second
 
-// Direct implement internal/proxy.client
+// Direct implement internal/proxy.client.
 type Direct struct{}
 
-// Dial is used to Dial with default dial timeout
+// Dial is used to Dial with default dial timeout.
 func (d Direct) Dial(network, address string) (net.Conn, error) {
 	return (&net.Dialer{Timeout: dialTimeout}).Dial(network, address)
 }
 
-// DialContext is used to DialContext with default dial timeout
+// DialContext is used to DialContext with default dial timeout.
 func (d Direct) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
 	return (&net.Dialer{Timeout: dialTimeout}).DialContext(ctx, network, address)
 }
 
-// DialTimeout is used to Dial with timeout, if timeout < 1, use the default dial timeout
+// DialTimeout is used to Dial with timeout, if timeout < 1, use the default dial timeout.
 func (d Direct) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
 	if timeout < 1 {
 		timeout = dialTimeout
@@ -30,25 +30,25 @@ func (d Direct) DialTimeout(network, address string, timeout time.Duration) (net
 	return (&net.Dialer{Timeout: timeout}).Dial(network, address)
 }
 
-// Connect is a padding function
+// Connect is a padding function.
 func (d Direct) Connect(_ context.Context, conn net.Conn, _, _ string) (net.Conn, error) {
 	return conn, nil
 }
 
-// HTTP is a padding function
+// HTTP is a padding function.
 func (d Direct) HTTP(_ *http.Transport) {}
 
-// Timeout is used to return timeout
+// Timeout is used to return timeout.
 func (d Direct) Timeout() time.Duration {
 	return dialTimeout
 }
 
-// Server is a padding function
+// Server is a padding function.
 func (d Direct) Server() (string, string) {
 	return "", ""
 }
 
-// Info is a padding function
+// Info is a padding function.
 func (d Direct) Info() string {
 	return "direct"
 }
--- internal/proxy/direct/direct_test.go
+++ internal/proxy/direct/direct_test.go
@@ -12,48 +12,53 @@ import (
 func TestDirect(t *testing.T) {
 	testsuite.InitHTTPServers(t)
 
-	d := Direct{}
+	direct := Direct{}
+	background := context.Background()
 
 	if testsuite.IPv4Enabled {
-		const network = "tcp4"
+		t.Run("IPv4", func(t *testing.T) {
+			const network = "tcp4"
 
-		addr := "127.0.0.1:" + testsuite.HTTPServerPort
-		conn, err := d.Dial(network, addr)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			addr := "127.0.0.1:" + testsuite.HTTPServerPort
+			conn, err := direct.Dial(network, addr)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
 
-		conn, err = d.DialContext(context.Background(), network, addr)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			conn, err = direct.DialContext(background, network, addr)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
 
-		addr = "localhost:" + testsuite.HTTPServerPort
-		conn, err = d.DialTimeout(network, addr, 0)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			addr = "localhost:" + testsuite.HTTPServerPort
+			conn, err = direct.DialTimeout(network, addr, 0)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
+		})
 	}
 
 	if testsuite.IPv6Enabled {
-		const network = "tcp6"
+		t.Run("IPv6", func(t *testing.T) {
+			const network = "tcp6"
 
-		addr := "[::1]:" + testsuite.HTTPServerPort
-		conn, err := d.Dial(network, addr)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			addr := "[::1]:" + testsuite.HTTPServerPort
+			conn, err := direct.Dial(network, addr)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
 
-		conn, err = d.DialContext(context.Background(), network, addr)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			conn, err = direct.DialContext(background, network, addr)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
 
-		addr = "localhost:" + testsuite.HTTPServerPort
-		conn, err = d.DialTimeout(network, addr, 0)
-		require.NoError(t, err)
-		testsuite.ProxyConn(t, conn)
+			addr = "localhost:" + testsuite.HTTPServerPort
+			conn, err = direct.DialTimeout(network, addr, 0)
+			require.NoError(t, err)
+			testsuite.ProxyConn(t, conn)
+		})
 	}
 
 	// padding
-	_, _ = d.Connect(context.Background(), nil, "", "")
-	d.HTTP(nil)
-	t.Log(d.Timeout())
-	t.Log(d.Server())
-	t.Log(d.Info())
+	_, _ = direct.Connect(context.Background(), nil, "", "")
+	direct.HTTP(nil)
+	t.Log(direct.Timeout())
+	t.Log(direct.Server())
+	t.Log(direct.Info())
 }
