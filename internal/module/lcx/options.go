f16e65d370a9ba795401546f841468caa3c92cb9
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-05-16 12:21:40 +0800

    module/lcx/options: add toml struct tag to Options, add TestOptions() and testdata.

--- internal/module/lcx/options.go
+++ internal/module/lcx/options.go
@@ -14,17 +14,17 @@ const (
 // Options contains all options about Tranner, Slaver and Listener.
 type Options struct {
 	// tran and listener
-	LocalNetwork string
-	LocalAddress string
+	LocalNetwork string `toml:"local_network"`
+	LocalAddress string `toml:"local_address"`
 
 	// tran and slave, connect target timeout
-	ConnectTimeout time.Duration
+	ConnectTimeout time.Duration `toml:"connect_timeout"`
 
 	// only slave, connect listener timeout
-	DialTimeout time.Duration
+	DialTimeout time.Duration `toml:"dial_timeout"`
 
 	// tran, slave and listener
-	MaxConns int
+	MaxConns int `toml:"max_conns"`
 }
 
 func (opts *Options) apply() *Options {
--- internal/module/lcx/options_test.go
+++ internal/module/lcx/options_test.go
@@ -1,14 +1,47 @@
 package lcx
 
 import (
+	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+
+	"project/internal/patch/toml"
+	"project/internal/testsuite"
 )
+
+func TestOptions(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/options.toml")
+	require.NoError(t, err)
+
+	// check unnecessary field
+	opts := new(Options)
+	err = toml.Unmarshal(data, opts)
+	require.NoError(t, err)
+
+	// check zero value
+	testsuite.CheckOptions(t, opts)
+
+	opts = opts.apply()
+	for _, testdata := range []struct {
+		except interface{}
+		actual interface{}
+	}{
+		{"tcp4", opts.LocalNetwork},
+		{"127.0.0.1:1099", opts.LocalAddress},
+		{15 * time.Second, opts.ConnectTimeout},
+		{30 * time.Second, opts.DialTimeout},
+		{100, opts.MaxConns},
+	} {
+		require.Equal(t, testdata.except, testdata.actual)
+	}
+}
 
 func TestOptions_Apply(t *testing.T) {
 	opts := new(Options)
 	opts = opts.apply()
+
 	for _, testdata := range []struct {
 		except interface{}
 		actual interface{}
