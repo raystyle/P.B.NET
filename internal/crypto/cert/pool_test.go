597d565c0a4c915470f1cd2bc263b0625cfeb0c3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-05-31 09:11:01 +0800

    crypto/cert/pool_test.go: add TestPool_PublicClient_Parallel() about add. delete, get and export.

--- internal/crypto/cert/pool_test.go
+++ internal/crypto/cert/pool_test.go
@@ -1181,6 +1181,157 @@ func TestPool_PublicClientCA_Parallel(t *testing.T) {
 	})
 }
 
+func TestPool_PublicClient_Parallel(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	t.Run("add", func(t *testing.T) {
+		var pool *Pool
+		pair1 := testGeneratePair(t)
+		pair2 := testGeneratePair(t)
+		cert1, key1 := pair1.Encode()
+		cert2, key2 := pair2.Encode()
+
+		init := func() {
+			pool = NewPool()
+		}
+		add1 := func() {
+			err := pool.AddPublicClientPair(cert1, key1)
+			require.NoError(t, err)
+		}
+		add2 := func() {
+			err := pool.AddPublicClientPair(cert2, key2)
+			require.NoError(t, err)
+		}
+		add3 := func() {
+			err := pool.AddPublicClientPair(nil, nil)
+			require.Error(t, err)
+		}
+		cleanup := func() {
+			require.Len(t, pool.GetPublicClientPairs(), 2)
+		}
+		testsuite.RunParallel(100, init, cleanup, add1, add2, add3)
+
+		testsuite.IsDestroyed(t, pool)
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		var pool *Pool
+		pair1 := testGeneratePair(t)
+		pair2 := testGeneratePair(t)
+		cert1, key1 := pair1.Encode()
+		cert2, key2 := pair2.Encode()
+
+		init := func() {
+			pool = NewPool()
+
+			err := pool.AddPublicClientPair(cert1, key1)
+			require.NoError(t, err)
+			err = pool.AddPublicClientPair(cert2, key2)
+			require.NoError(t, err)
+		}
+		delete1 := func() {
+			err := pool.DeletePublicClientCert(0)
+			require.NoError(t, err)
+		}
+		delete2 := func() {
+			err := pool.DeletePublicClientCert(0)
+			require.NoError(t, err)
+		}
+		delete3 := func() {
+			err := pool.DeletePublicClientCert(3)
+			require.Error(t, err)
+		}
+		cleanup := func() {
+			certs := pool.GetPublicClientPairs()
+			require.Len(t, certs, 0)
+		}
+		testsuite.RunParallel(100, init, cleanup, delete1, delete2, delete3)
+
+		testsuite.IsDestroyed(t, pool)
+	})
+
+	t.Run("get", func(t *testing.T) {
+		var pool *Pool
+		pair1 := testGeneratePair(t)
+		pair2 := testGeneratePair(t)
+		cert1, key1 := pair1.Encode()
+		cert2, key2 := pair2.Encode()
+
+		init := func() {
+			pool = NewPool()
+
+			err := pool.AddPublicClientPair(cert1, key1)
+			require.NoError(t, err)
+			err = pool.AddPublicClientPair(cert2, key2)
+			require.NoError(t, err)
+		}
+		get := func() {
+			certs := pool.GetPublicClientPairs()
+			require.Len(t, certs, 2)
+		}
+		testsuite.RunParallel(100, init, nil, get, get)
+
+		testsuite.IsDestroyed(t, pool)
+	})
+
+	t.Run("export", func(t *testing.T) {
+		var pool *Pool
+		pair1 := testGeneratePair(t)
+		pair2 := testGeneratePair(t)
+		cert1, key1 := pair1.Encode()
+		cert2, key2 := pair2.Encode()
+		cert1PEM, key1PEM := pair1.EncodeToPEM()
+		cert2PEM, key2PEM := pair2.EncodeToPEM()
+
+		init := func() {
+			pool = NewPool()
+
+			err := pool.AddPublicClientPair(cert1, key1)
+			require.NoError(t, err)
+			err = pool.AddPublicClientPair(cert2, key2)
+			require.NoError(t, err)
+		}
+		export1 := func() {
+			cert, key, err := pool.ExportPublicClientPair(0)
+			require.NoError(t, err)
+			require.Equal(t, cert1PEM, cert)
+			require.Equal(t, key1PEM, key)
+		}
+		export2 := func() {
+			cert, key, err := pool.ExportPublicClientPair(1)
+			require.NoError(t, err)
+			require.Equal(t, cert2PEM, cert)
+			require.Equal(t, key2PEM, key)
+		}
+		export3 := func() {
+			cert, key, err := pool.ExportPublicClientPair(2)
+			require.Error(t, err)
+			require.Nil(t, cert)
+			require.Nil(t, key)
+		}
+		cleanup := func() {
+			certs := pool.GetPublicClientPairs()
+			require.Len(t, certs, 2)
+		}
+		testsuite.RunParallel(100, init, cleanup, export1, export2, export3)
+
+		testsuite.IsDestroyed(t, pool)
+	})
+}
+
+func TestPool_PrivateRootCA_Parallel(t *testing.T) {
+
+}
+
+func TestPool_PrivateClientCA_Parallel(t *testing.T) {
+
+}
+
+func TestPool_PrivateClient_Parallel(t *testing.T) {
+
+}
+
 func TestPool_Parallel(t *testing.T) {
 	gm := testsuite.MarkGoroutines(t)
 	defer gm.Compare()
