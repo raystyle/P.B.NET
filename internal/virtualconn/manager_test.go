bb19ec44e36882e2487e4f9b8d898bbf2882505e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-06-17 20:19:24 +0800

    virtualconn/manager_test.go: fix typo that use expected to replace except.

--- internal/virtualconn/manager_test.go
+++ internal/virtualconn/manager_test.go
@@ -15,11 +15,14 @@ func TestNewConnID(t *testing.T) {
 	copy(local[:], bytes.Repeat([]byte{1}, guid.Size))
 	remote := guid.GUID{}
 	copy(remote[:], bytes.Repeat([]byte{2}, guid.Size))
+
 	cid := NewConnID(&local, 3, &remote, 4)
-	except := make([]byte, 0, 2*guid.Size+2*portSize)
-	except = append(except, bytes.Repeat([]byte{1}, guid.Size)...)
-	except = append(except, convert.Uint32ToBytes(3)...)
-	except = append(except, bytes.Repeat([]byte{2}, guid.Size)...)
-	except = append(except, convert.Uint32ToBytes(4)...)
-	require.Equal(t, except, cid[:])
+
+	expected := make([]byte, 0, 2*guid.Size+2*portSize)
+	expected = append(expected, bytes.Repeat([]byte{1}, guid.Size)...)
+	expected = append(expected, convert.Uint32ToBytes(3)...)
+	expected = append(expected, bytes.Repeat([]byte{2}, guid.Size)...)
+	expected = append(expected, convert.Uint32ToBytes(4)...)
+
+	require.Equal(t, expected, cid[:])
 }
