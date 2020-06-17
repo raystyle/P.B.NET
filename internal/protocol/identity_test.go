bf899d0826baa04528b3fce4d9e8366687150c99
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-06-17 20:18:40 +0800

    protocol/identity_test.go: fix typo that use expected to replace except.

--- internal/protocol/identity_test.go
+++ internal/protocol/identity_test.go
@@ -8,29 +8,29 @@ import (
 
 func TestRole_String(t *testing.T) {
 	for _, testdata := range [...]*struct {
-		expect string
-		actual uint8
+		expected string
+		actual   uint8
 	}{
 		{"controller", 0},
 		{"node", 1},
 		{"beacon", 2},
 		{"invalid role: 3", 3},
 	} {
-		require.Equal(t, testdata.expect, Role(testdata.actual).String())
+		require.Equal(t, testdata.expected, Role(testdata.actual).String())
 	}
 	t.Log(Role(5).Error())
 }
 
 func TestRole_Bytes(t *testing.T) {
 	for _, testdata := range [...]*struct {
-		expect []byte
-		actual uint8
+		expected []byte
+		actual   uint8
 	}{
 		{[]byte{0}, 0},
 		{[]byte{1}, 1},
 		{[]byte{2}, 2},
 		{[]byte{255}, 3},
 	} {
-		require.Equal(t, testdata.expect, Role(testdata.actual).Bytes())
+		require.Equal(t, testdata.expected, Role(testdata.actual).Bytes())
 	}
 }
