691b5d9d450aa3e17bab3ba8fd5f3540a2267054
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-06-21 14:24:03 +0800

    anko/goroot/unsafe: finish convert unsafe.Pointer() to other type, add TestUnsafe()

--- internal/anko/goroot/unsafe.go
+++ internal/anko/goroot/unsafe.go
@@ -18,8 +18,13 @@ func initUnsafe() {
 		// define variables
 
 		// define functions
-		"Sizeof":  reflect.ValueOf(sizeOf),
-		"Alignof": reflect.ValueOf(alignOf),
+		"Convert":          reflect.ValueOf(convert),
+		"ConvertWithType":  reflect.ValueOf(convertWithType),
+		"ArrayOf":          reflect.ValueOf(arrayOf),
+		"ArrayToSlice":     reflect.ValueOf(arrayToSlice),
+		"ByteArrayToSlice": reflect.ValueOf(byteArrayToSlice),
+		"Sizeof":           reflect.ValueOf(sizeOf),
+		"Alignof":          reflect.ValueOf(alignOf),
 	}
 	var (
 		pointer unsafe.Pointer
@@ -27,6 +32,39 @@ func initUnsafe() {
 	env.PackageTypes["unsafe"] = map[string]reflect.Type{
 		"Pointer": reflect.TypeOf(&pointer).Elem(),
 	}
+}
+
+// convert is used to force convert like
+// n := *(*uint32)(unsafe.Pointer(&Float32))
+//
+// you can use these code in anko script
+//
+// p = unsafe.Convert(&val, new(typ))
+// println(p.Interface().A)          // get value in struct
+// p.Set(reflect.ValueOf(newVal))    // set value
+//
+// newVal must the same type with typ
+// see more information in TestUnsafe()
+func convert(point *interface{}, typ interface{}) reflect.Value {
+	return convertWithType(point, reflect.TypeOf(typ))
+}
+
+func convertWithType(point *interface{}, typ reflect.Type) reflect.Value {
+	ptr := reflect.ValueOf(point).Elem().InterfaceData()[1]
+	return reflect.NewAt(typ, unsafe.Pointer(ptr)).Elem() // #nosec
+}
+
+// arrayOf will not create a point about array.
+func arrayOf(typ interface{}, size int) reflect.Type {
+	return reflect.ArrayOf(size, reflect.TypeOf(typ))
+}
+
+func arrayToSlice(array reflect.Value) reflect.Value {
+	return array.Slice(0, array.Len())
+}
+
+func byteArrayToSlice(array reflect.Value) []byte {
+	return arrayToSlice(array).Bytes()
 }
 
 func sizeOf(i interface{}) uintptr {
--- internal/anko/goroot/unsafe_test.go
+++ internal/anko/goroot/unsafe_test.go
@@ -1,8 +1,10 @@
 package goroot
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/require"
 
@@ -10,7 +12,7 @@ import (
 	"project/internal/testsuite"
 )
 
-func testRun(t *testing.T, s string, fail bool) {
+func testRun(t *testing.T, s string, fail bool, expected interface{}) {
 	src := strings.Repeat(s, 1)
 	stmt, err := anko.ParseSrc(src)
 	require.NoError(t, err)
@@ -25,9 +27,25 @@ func testRun(t *testing.T, s string, fail bool) {
 		require.NoError(t, err)
 		t.Log(val)
 	}
+	require.Equal(t, expected, val)
 
 	testsuite.IsDestroyed(t, env)
 	testsuite.IsDestroyed(t, stmt)
+}
+
+func TestUnsafeAboutStruct(t *testing.T) {
+	type s struct {
+		A int32
+		B int32
+	}
+	val := int64(256)
+
+	aa := (*s)(unsafe.Pointer(&val))
+	fmt.Println(aa.A)
+	fmt.Println(aa.B)
+
+	n := *(*[8]byte)(unsafe.Pointer(&val))
+	fmt.Println(n)
 }
 
 func TestUnsafe(t *testing.T) {
@@ -36,15 +54,69 @@ func TestUnsafe(t *testing.T) {
 
 	const src = `
 unsafe = import("unsafe")
+reflect = import("reflect")
 
-Float64 = 0.614
-Int64 = 123
+val = 256
 
-println(unsafe.Sizeof(Float64))
-println(unsafe.Sizeof(Int64))
+// sizeOf
+println(unsafe.Sizeof(val))
 
-println(unsafe.Alignof(Float64))
-println(unsafe.Alignof(Int64))
+// AlignOf
+println(unsafe.Alignof(val))
+
+// convert to struct
+// p := (*testStruct)(unsafe.Pointer(&Int64))
+
+ss = make(struct {
+	A int32,
+	B int32
+})
+
+p = unsafe.Convert(&val, ss)
+
+pv = p.Interface()
+println(pv)
+
+// byte order
+if !(pv.A == 256 || pv.B == 256) {
+	println(val)
+	return false
+}
+
+// cover
+p.Set(reflect.ValueOf(ss))
+if val != 0 {
+	println(val)
+	return false
+}
+
+
+// make [8]byte and test ConvertWithType
+//
+// p := (*[8]byte)(unsafe.Pointer(&Int64))
+
+val = 256
+typ = unsafe.ArrayOf(*new(byte), 8)
+p = unsafe.ConvertWithType(&val, typ)
+
+aaa = p.Interface()
+println(aaa[1])
+// can't call aaa[1] = 1
+
+slice = unsafe.ByteArrayToSlice(p)
+
+println(slice[:4], slice[4:])
+
+// cover
+for i = 0; i < 8; i++ {
+	slice[i] = 0
+}
+if val != 0 {
+	println(val)
+	return false
+}
+
+return true
 `
-	testRun(t, src, false)
+	testRun(t, src, false, true)
 }
