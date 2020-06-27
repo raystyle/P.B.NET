9ad38664feea9c2100ccd2acf103aa2850e419e0
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-06-27 10:57:45 +0800

    key.go: add const about random data size, improve comment and returned error.

--- controller/key.go
+++ controller/key.go
@@ -14,27 +14,6 @@ import (
 	"project/internal/security"
 )
 
-// SessionKeyFile is the session key file path.
-const SessionKeyFile = "key/session.key"
-
-func calculateAESKeyFromPassword(password []byte) ([]byte, []byte) {
-	hash := sha256.New()
-	hash.Write(password)
-	hash.Write([]byte{20, 18, 11, 27})
-	hashed := hash.Sum(nil)
-	// calculate more count
-	var n int
-	for i := 0; i < sha256.Size; i++ {
-		n += int(hashed[i])
-	}
-	for i := 0; i < 10000+n; i++ {
-		hash.Write(hashed)
-		hashed = hash.Sum(nil)
-	}
-	keyIV := hash.Sum(nil)
-	return keyIV, keyIV[:aes.IVSize]
-}
-
 // --------------------------------session file format--------------------------------
 //
 // +----------+------------+-------------+------------+---------------+--------------+
@@ -47,17 +26,32 @@ func calculateAESKeyFromPassword(password []byte) ([]byte, []byte) {
 // Hash value is sha256(random + ed25519 key + random + broadcast key + random)
 // Random data is not the multiple of the sha256.BlockSize(64 bytes)
 //
+// use flate to compress(random + ed25519 key + random + broadcast key + random)
+//
 // Session key is the Private Key + Broadcast Key
 // Private Key = ed25519 Private Key(64 Bytes)
 // Broadcast Key = AES Key(256 Bit) + AES IV (32 Bytes + 16 Bytes, AES CBC)
+
+// SessionKeyFilePath is the session key file path.
+const SessionKeyFilePath = "key/session.key"
+
+const (
+	randomSize7147 = 7147
+	randomSize2018 = 2018
+	randomSize1127 = 1127
+)
 
 // GenerateSessionKey is used to generate session key.
 func GenerateSessionKey(password []byte) ([]byte, error) {
 	keys, err := generateSessionKey()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, errors.WithMessage(err, "failed to generate session key")
 	}
-	return encryptSessionKey(keys, password)
+	file, err := encryptSessionKey(keys, password)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to encrypt session key")
+	}
+	return file, nil
 }
 
 func generateSessionKey() ([3][]byte, error) {
@@ -79,32 +73,36 @@ func generateSessionKey() ([3][]byte, error) {
 func encryptSessionKey(keys [3][]byte, password []byte) ([]byte, error) {
 	// make session key file
 	buf := bytes.NewBuffer(make([]byte, 0, 10240))
-	buf.Write(random.Bytes(7147)) // random data 1
-	buf.Write(keys[0])            // private key
-	buf.Write(random.Bytes(2018)) // random data 2
-	buf.Write(keys[1])            // aes key
-	buf.Write(keys[2])            // aes iv
-	thirdSize := 1127 + random.Int(1024)
+	buf.Write(random.Bytes(randomSize7147)) // random data 1
+	buf.Write(keys[0])                      // private key
+	buf.Write(random.Bytes(randomSize2018)) // random data 2
+	buf.Write(keys[1])                      // aes key
+	buf.Write(keys[2])                      // aes iv
+	thirdSize := randomSize1127 + random.Int(1024)
 	buf.Write(random.Bytes(thirdSize)) // random data 3
 	// compress
-	compressed := bytes.NewBuffer(make([]byte, 0, 5120))
+	compressed := bytes.NewBuffer(make([]byte, 0, buf.Len()/2))
 	writer, err := flate.NewWriter(compressed, flate.BestCompression)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, errors.Wrap(err, "failed to create deflate writer")
 	}
 	_, err = writer.Write(buf.Bytes())
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, errors.Wrap(err, "failed to compress session key data")
 	}
 	err = writer.Close()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, errors.Wrap(err, "failed to close deflate writer")
 	}
 	// encrypt file
 	aesKey, aesIV := calculateAESKeyFromPassword(password)
+	defer func() {
+		security.CoverBytes(aesKey)
+		security.CoverBytes(aesIV)
+	}()
 	fileEnc, err := aes.CBCEncrypt(compressed.Bytes(), aesKey, aesIV)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, errors.Wrap(err, "failed to encrypt compressed data")
 	}
 	// calculate file hash
 	hash := sha256.New()
@@ -113,41 +111,63 @@ func encryptSessionKey(keys [3][]byte, password []byte) ([]byte, error) {
 	return append(fileHash, fileEnc...), nil
 }
 
+func calculateAESKeyFromPassword(password []byte) ([]byte, []byte) {
+	hash := sha256.New()
+	hash.Write(password)
+	hash.Write([]byte{20, 18, 11, 27})
+	hashed := hash.Sum(nil)
+	// calculate more count
+	var n int
+	for i := 0; i < sha256.Size; i++ {
+		n += int(hashed[i])
+	}
+	for i := 0; i < 10000+n; i++ {
+		hash.Write(hashed)
+		hashed = hash.Sum(nil)
+	}
+	keyIV := hash.Sum(nil)
+	return keyIV, keyIV[:aes.IVSize]
+}
+
 // LoadSessionKey is used to decrypt session key file and
 // return session key (private key, aes key, aes iv).
 func LoadSessionKey(sessionKey, password []byte) ([3][]byte, error) {
 	var keys [3][]byte
 	if len(sessionKey) < sha256.Size+aes.BlockSize {
-		return keys, errors.New("invalid session key size")
+		return keys, errors.New("invalid session key file size")
 	}
 	memory := security.NewMemory()
 	defer memory.Flush()
 	// decrypt session key file
 	aesKey, aesIV := calculateAESKeyFromPassword(password)
-	fileDec, err := aes.CBCDecrypt(sessionKey[sha256.Size:], aesKey, aesIV)
+	compressed, err := aes.CBCDecrypt(sessionKey[sha256.Size:], aesKey, aesIV)
 	if err != nil {
-		return keys, errors.WithStack(err)
+		return keys, errors.Wrap(err, "failed to decrypt session key file")
 	}
 	// decompress
-	buf := bytes.NewBuffer(make([]byte, 0, len(fileDec)*2))
-	reader := flate.NewReader(bytes.NewReader(fileDec))
+	buf := bytes.NewBuffer(make([]byte, 0, len(compressed)*2))
+	reader := flate.NewReader(bytes.NewReader(compressed))
 	_, err = buf.ReadFrom(reader)
 	if err != nil {
-		return keys, errors.WithStack(err)
+		return keys, errors.Wrap(err, "failed to decompress session key file")
+	}
+	err = reader.Close()
+	if err != nil {
+		return keys, errors.Wrap(err, "failed to close deflate reader")
 	}
 	file := buf.Bytes()
 	// compare file hash
 	fileHash := sha256.Sum256(file)
 	if subtle.ConstantTimeCompare(sessionKey[:sha256.Size], fileHash[:]) != 1 {
-		return keys, errors.New("incorrect password")
+		return keys, errors.New("incorrect password or session key has been tampered")
 	}
 	// ed25519 private key
 	memory.Padding()
-	offset := 7147
+	offset := randomSize7147
 	privateKey := file[offset : offset+ed25519.PrivateKeySize]
 	// broadcast key: aes key
 	memory.Padding()
-	offset += ed25519.PrivateKeySize + 2018
+	offset += ed25519.PrivateKeySize + randomSize2018
 	aesKey = file[offset : offset+aes.Key256Bit]
 	// broadcast key: aes iv
 	memory.Padding()
