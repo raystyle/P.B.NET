23d9a292c6442ce4b2ac59e32fea4b64839379ec
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-07-07 20:39:14 +0800

    module/filemgr/compress.go: add dirToZipStat and DirToZipFile, update test.

--- internal/module/filemgr/compress.go
+++ internal/module/filemgr/compress.go
@@ -2,6 +2,7 @@ package filemgr
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -12,21 +13,101 @@ import (
 	"project/internal/system"
 )
 
-// DirToZipFile is used to compress file or directory to a zip file and write.
-func DirToZipFile() {
+type dirToZipStat struct {
+	srcAbs string
+	dstAbs string
+}
 
+func zipCheckSrcDst(src, dst string) (string, string, bool, error) {
+	// check the last is "*"
+	var wildcard bool
+	if l := len(src); l != 0 && src[l-1] == '*' {
+		wildcard = true
+		src = src[:l-1]
+	}
+	// get abs path
+	srcAbs, err := filepath.Abs(src)
+	if err != nil {
+		return "", "", false, err
+	}
+	dstAbs, err := filepath.Abs(dst)
+	if err != nil {
+		return "", "", false, err
+	}
+	// check src
+	if wildcard {
+		stat, err := os.Stat(srcAbs)
+		if err != nil {
+			return "", "", false, err
+		}
+		if !stat.IsDir() {
+			return "", "", false, errors.New("use wildcard but src is a file")
+		}
+	}
+	return srcAbs, dstAbs, wildcard, nil
+
+}
+
+// DirToZipFile is used to compress file or directory to a zip file and write.
+// if the src with "*" at the end, it will not create a root directory to the zip file.
+func DirToZipFile(src, dstZip string) error {
+
+	// create zip file
+	zipFile, err := system.OpenFile(src, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = zipFile.Close() }()
+	// if compress failed, delete the zip file.
+	var ok bool
+	defer func() {
+		if !ok {
+			_ = zipFile.Close()
+			_ = os.Remove(dstZip)
+		}
+	}()
+	writer := zip.NewWriter(zipFile)
+	// create root directory
+	// if !wildcard {
+	//
+	// }
+
+	err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		fmt.Println(path)
+
+		header := zip.FileHeader{
+			Name:     "",
+			Method:   zip.Deflate,
+			Modified: time.Time{},
+		}
+		writer.CreateHeader(&header)
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+	ok = true
+	return nil
 }
 
 // ZipFileToDir is used to decompress zip file to a directory.
 // If appear the same file name, it will return a error.
-func ZipFileToDir(src, dst string) error {
-	reader, err := zip.OpenReader(src)
+func ZipFileToDir(srcZip, dstDir string) error {
+	reader, err := zip.OpenReader(srcZip)
 	if err != nil {
 		return errors.Wrap(err, "failed to open zip file")
 	}
 	dirs := make([]*zip.File, 0, len(reader.File)/5)
 	for _, file := range reader.File {
-		err = zipWriteFile(file, dst)
+		err = zipWriteFile(file, dstDir)
 		if err != nil {
 			return err
 		}
@@ -35,7 +116,7 @@ func ZipFileToDir(src, dst string) error {
 		}
 	}
 	for _, dir := range dirs {
-		filename := filepath.Join(dst, dir.Name)
+		filename := filepath.Join(dstDir, dir.Name)
 		err = os.Chtimes(filename, time.Now(), dir.Modified)
 		if err != nil {
 			return errors.Wrap(err, "failed to change directory \"\" modification time")
--- internal/module/filemgr/compress_test.go
+++ internal/module/filemgr/compress_test.go
@@ -6,7 +6,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestDirToZipFile(t *testing.T) {
+
+	err := DirToZipFile("../filemgr/", "testdata/filemgr.zip")
+	require.NoError(t, err)
+
+	err = DirToZipFile("../filemgr/*", "testdata/filemgr.zip")
+	require.NoError(t, err)
+}
+
 func TestZipFileToDir(t *testing.T) {
+
 	const dir = "testdata/zip-file"
 	err := ZipFileToDir("testdata/file.zip", dir)
 	require.NoError(t, err)
