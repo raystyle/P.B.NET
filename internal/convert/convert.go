b616d1b0cf30be9bd795e8495f8a557791fb1329
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-08-15 10:22:37 +0800

    convert/convert.go: add convert number to bytes and bytes to number with little endian.

--- internal/convert/convert.go
+++ internal/convert/convert.go
@@ -11,49 +11,49 @@ import (
 	"unsafe"
 )
 
-// Int16ToBytes is used to convert int16 to bytes.
+// Int16ToBytes is used to convert int16 to bytes with big endian.
 func Int16ToBytes(Int16 int16) []byte {
 	b := make([]byte, 2)
 	binary.BigEndian.PutUint16(b, uint16(Int16))
 	return b
 }
 
-// Int32ToBytes is used to convert int32 to bytes.
+// Int32ToBytes is used to convert int32 to bytes with big endian.
 func Int32ToBytes(Int32 int32) []byte {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, uint32(Int32))
 	return b
 }
 
-// Int64ToBytes is used to convert int64 to bytes.
+// Int64ToBytes is used to convert int64 to bytes with big endian.
 func Int64ToBytes(Int64 int64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(Int64))
 	return b
 }
 
-// Uint16ToBytes is used to convert uint16 to bytes.
+// Uint16ToBytes is used to convert uint16 to bytes with big endian.
 func Uint16ToBytes(Uint16 uint16) []byte {
 	b := make([]byte, 2)
 	binary.BigEndian.PutUint16(b, Uint16)
 	return b
 }
 
-// Uint32ToBytes is used to convert uint32 to bytes.
+// Uint32ToBytes is used to convert uint32 to bytes with big endian.
 func Uint32ToBytes(Uint32 uint32) []byte {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, Uint32)
 	return b
 }
 
-// Uint64ToBytes is used to convert uint64 to bytes.
+// Uint64ToBytes is used to convert uint64 to bytes with big endian.
 func Uint64ToBytes(Uint64 uint64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, Uint64)
 	return b
 }
 
-// Float32ToBytes is used to convert float32 to bytes.
+// Float32ToBytes is used to convert float32 to bytes with big endian.
 func Float32ToBytes(Float32 float32) []byte {
 	b := make([]byte, 4)
 	n := *(*uint32)(unsafe.Pointer(&Float32)) // #nosec
@@ -61,7 +61,7 @@ func Float32ToBytes(Float32 float32) []byte {
 	return b
 }
 
-// Float64ToBytes is used to convert float64 to bytes.
+// Float64ToBytes is used to convert float64 to bytes with big endian.
 func Float64ToBytes(Float64 float64) []byte {
 	b := make([]byte, 8)
 	n := *(*uint64)(unsafe.Pointer(&Float64)) // #nosec
@@ -69,58 +69,138 @@ func Float64ToBytes(Float64 float64) []byte {
 	return b
 }
 
-// BytesToInt16 is used to convert bytes to int16.
+// BytesToInt16 is used to convert bytes to int16 with big endian.
 func BytesToInt16(Bytes []byte) int16 {
 	return int16(binary.BigEndian.Uint16(Bytes))
 }
 
-// BytesToInt32 is used to convert bytes to int32.
+// BytesToInt32 is used to convert bytes to int32 with big endian.
 func BytesToInt32(Bytes []byte) int32 {
 	return int32(binary.BigEndian.Uint32(Bytes))
 }
 
-// BytesToInt64 is used to convert bytes to int64.
+// BytesToInt64 is used to convert bytes to int64 with big endian.
 func BytesToInt64(Bytes []byte) int64 {
 	return int64(binary.BigEndian.Uint64(Bytes))
 }
 
-// BytesToUint16 is used to convert bytes to uint16.
+// BytesToUint16 is used to convert bytes to uint16 with big endian.
 func BytesToUint16(Bytes []byte) uint16 {
 	return binary.BigEndian.Uint16(Bytes)
 }
 
-// BytesToUint32 is used to convert bytes to uint32.
+// BytesToUint32 is used to convert bytes to uint32 with big endian.
 func BytesToUint32(Bytes []byte) uint32 {
 	return binary.BigEndian.Uint32(Bytes)
 }
 
-// BytesToUint64 is used to convert bytes to uint64.
+// BytesToUint64 is used to convert bytes to uint64 with big endian.
 func BytesToUint64(Bytes []byte) uint64 {
 	return binary.BigEndian.Uint64(Bytes)
 }
 
-// BytesToFloat32 is used to convert bytes to float32.
+// BytesToFloat32 is used to convert bytes to float32 with big endian.
 func BytesToFloat32(Bytes []byte) float32 {
 	b := binary.BigEndian.Uint32(Bytes)
 	return *(*float32)(unsafe.Pointer(&b)) // #nosec
 }
 
-// BytesToFloat64 is used to convert bytes to float64.
+// BytesToFloat64 is used to convert bytes to float64 with big endian.
 func BytesToFloat64(Bytes []byte) float64 {
 	b := binary.BigEndian.Uint64(Bytes)
 	return *(*float64)(unsafe.Pointer(&b)) // #nosec
 }
 
-// LEUint32ToBytes is used to convert uint32 to bytes with LittleEndian.
+// LEInt16ToBytes is used to convert int16 to bytes with little endian.
+func LEInt16ToBytes(Int16 int16) []byte {
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, uint16(Int16))
+	return b
+}
+
+// LEInt32ToBytes is used to convert int32 to bytes with little endian.
+func LEInt32ToBytes(Int32 int32) []byte {
+	b := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b, uint32(Int32))
+	return b
+}
+
+// LEInt64ToBytes is used to convert int64 to bytes with little endian.
+func LEInt64ToBytes(Int64 int64) []byte {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(Int64))
+	return b
+}
+
+// LEUint16ToBytes is used to convert uint16 to bytes with little endian.
+func LEUint16ToBytes(Uint16 uint16) []byte {
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, Uint16)
+	return b
+}
+
+// LEUint32ToBytes is used to convert uint32 to bytes with little endian.
 func LEUint32ToBytes(Uint32 uint32) []byte {
 	b := make([]byte, 4)
 	binary.LittleEndian.PutUint32(b, Uint32)
 	return b
 }
 
-// LEBytesToUint16 is used to convert uint32 to bytes with LittleEndian.
+// LEUint64ToBytes is used to convert uint64 to bytes with little endian.
+func LEUint64ToBytes(Uint64 uint64) []byte {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, Uint64)
+	return b
+}
+
+// LEFloat32ToBytes is used to convert float32 to bytes with little endian.
+func LEFloat32ToBytes(Float32 float32) []byte {
+	b := make([]byte, 4)
+	n := *(*uint32)(unsafe.Pointer(&Float32)) // #nosec
+	binary.LittleEndian.PutUint32(b, n)
+	return b
+}
+
+// LEBytesToInt16 is used to convert bytes to int16 with little endian.
+func LEBytesToInt16(Bytes []byte) int16 {
+	return int16(binary.LittleEndian.Uint16(Bytes))
+}
+
+// LEBytesToInt32 is used to convert bytes to int32 with little endian.
+func LEBytesToInt32(Bytes []byte) int32 {
+	return int32(binary.LittleEndian.Uint32(Bytes))
+}
+
+// LEBytesToInt64 is used to convert bytes to int64 with little endian.
+func LEBytesToInt64(Bytes []byte) int64 {
+	return int64(binary.LittleEndian.Uint64(Bytes))
+}
+
+// LEBytesToUint16 is used to convert bytes to uint16 with little endian.
 func LEBytesToUint16(Bytes []byte) uint16 {
 	return binary.LittleEndian.Uint16(Bytes)
+}
+
+// LEBytesToUint32 is used to convert bytes to uint32 with little endian.
+func LEBytesToUint32(Bytes []byte) uint32 {
+	return binary.LittleEndian.Uint32(Bytes)
+}
+
+// LEBytesToUint64 is used to convert bytes to uint64 with little endian.
+func LEBytesToUint64(Bytes []byte) uint64 {
+	return binary.LittleEndian.Uint64(Bytes)
+}
+
+// LEBytesToFloat32 is used to convert bytes to float32 with little endian.
+func LEBytesToFloat32(Bytes []byte) float32 {
+	b := binary.LittleEndian.Uint32(Bytes)
+	return *(*float32)(unsafe.Pointer(&b)) // #nosec
+}
+
+// LEBytesToFloat64 is used to convert bytes to float64 with little endian.
+func LEBytesToFloat64(Bytes []byte) float64 {
+	b := binary.LittleEndian.Uint64(Bytes)
+	return *(*float64)(unsafe.Pointer(&b)) // #nosec
 }
 
 // AbsInt64 is used to calculate the absolute value of the parameter.
