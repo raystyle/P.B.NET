db8d8ae4c37b38a0db92b9f901fe0e3b319e1a52
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-08-22 14:46:58 +0800

    module/wmi/object_test.go: add sub test about uint64, float32, float64, string and bool.

--- internal/module/wmi/object_test.go
+++ internal/module/wmi/object_test.go
@@ -179,6 +179,114 @@ func TestObject_AddProperty(t *testing.T) {
 		require.NoError(t, err)
 	})
 
+	t.Run("uint64", func(t *testing.T) {
+		const name = "uint64"
+
+		err := object.AddProperty(name, CIMTypeUint64, false)
+		require.NoError(t, err)
+
+		err = object.SetProperty(name, uint64(math.MaxUint64))
+		require.NoError(t, err)
+		val1, err := object.GetProperty(name)
+		require.NoError(t, err)
+		v1, err := strconv.ParseUint(val1.Value().(string), 10, 64)
+		require.NoError(t, err)
+		require.EqualValues(t, uint64(math.MaxUint64), v1)
+
+		err = object.SetProperty(name, uint64(0))
+		require.NoError(t, err)
+		val2, err := object.GetProperty(name)
+		require.NoError(t, err)
+		v2, err := strconv.ParseUint(val2.Value().(string), 10, 64)
+		require.NoError(t, err)
+		require.EqualValues(t, 0, v2)
+
+		err = object.RemoveProperty(name)
+		require.NoError(t, err)
+	})
+
+	t.Run("float32", func(t *testing.T) {
+		const name = "float32"
+
+		err := object.AddProperty(name, CIMTypeFloat32, false)
+		require.NoError(t, err)
+
+		err = object.SetProperty(name, float32(math.MaxFloat32))
+		require.NoError(t, err)
+		val1, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, math.MaxFloat32, val1.Value())
+
+		err = object.SetProperty(name, float32(math.SmallestNonzeroFloat32))
+		require.NoError(t, err)
+		val2, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, math.SmallestNonzeroFloat32, val2.Value())
+
+		err = object.RemoveProperty(name)
+		require.NoError(t, err)
+	})
+
+	t.Run("float64", func(t *testing.T) {
+		const name = "float64"
+
+		err := object.AddProperty(name, CIMTypeFloat64, false)
+		require.NoError(t, err)
+
+		err = object.SetProperty(name, math.MaxFloat64)
+		require.NoError(t, err)
+		val1, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, math.MaxFloat64, val1.Value())
+
+		err = object.SetProperty(name, math.SmallestNonzeroFloat64)
+		require.NoError(t, err)
+		val2, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, math.SmallestNonzeroFloat64, val2.Value())
+
+		err = object.RemoveProperty(name)
+		require.NoError(t, err)
+	})
+
+	t.Run("string", func(t *testing.T) {
+		const name = "string"
+
+		err := object.AddProperty(name, CIMTypeString, false)
+		require.NoError(t, err)
+
+		err = object.SetProperty(name, "test wmi")
+		require.NoError(t, err)
+		val1, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, "test wmi", val1.Value())
+
+		err = object.RemoveProperty(name)
+		require.NoError(t, err)
+	})
+
+	t.Run("bool", func(t *testing.T) {
+		const name = "string"
+
+		err := object.AddProperty(name, CIMTypeBool, false)
+		require.NoError(t, err)
+
+		err = object.SetProperty(name, true)
+		require.NoError(t, err)
+		val1, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, true, val1.Value())
+
+		err = object.SetProperty(name, false)
+		require.NoError(t, err)
+		val2, err := object.GetProperty(name)
+		require.NoError(t, err)
+		require.EqualValues(t, false, val2.Value())
+
+		err = object.RemoveProperty(name)
+		require.NoError(t, err)
+	})
+
 	t.Run("array", func(t *testing.T) {
 
 	})
