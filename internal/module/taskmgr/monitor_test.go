14688ca2e43d9415fc1c47b1b81d0803f0888654
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-08-27 21:04:27 +0800

    module/taskmgr/monitor_test.go: add TestMonitor_EventProcessCreated() and TestMonitor_EventProcessTerminated().

--- internal/module/taskmgr/monitor_test.go
+++ internal/module/taskmgr/monitor_test.go
@@ -3,6 +3,9 @@ package taskmgr
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -48,4 +51,98 @@ func testMonitorPrintTerminatedProcesses(processes []*Process) {
 	for _, process := range processes {
 		fmt.Printf("terminate process PID: %d Name: %s\n", process.PID, process.Name)
 	}
+}
+
+func TestMonitor_EventProcessCreated(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	path, err := os.Executable()
+	require.NoError(t, err)
+	name := filepath.Base(path)
+
+	var created bool
+
+	handler := func(_ context.Context, event uint8, data interface{}) {
+		if event != EventProcessCreated {
+			return
+		}
+		for _, process := range data.([]*Process) {
+			if process.Name == name {
+				created = true
+			}
+		}
+	}
+	monitor, err := NewMonitor(logger.Test, handler)
+	require.NoError(t, err)
+
+	// wait first auto refresh
+	time.Sleep(2 * defaultRefreshInterval)
+
+	// create process
+	cmd := exec.Command(path)
+	err = cmd.Start()
+	require.NoError(t, err)
+
+	// wait refresh
+	time.Sleep(2 * defaultRefreshInterval)
+
+	// terminate process
+	err = cmd.Process.Kill()
+	require.NoError(t, err)
+	err = cmd.Process.Release()
+	require.NoError(t, err)
+
+	monitor.Close()
+
+	testsuite.IsDestroyed(t, monitor)
+
+	require.True(t, created, "not find expected created process")
+}
+
+func TestMonitor_EventProcessTerminated(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	path, err := os.Executable()
+	require.NoError(t, err)
+	name := filepath.Base(path)
+
+	// create process
+	cmd := exec.Command(path)
+	err = cmd.Start()
+	require.NoError(t, err)
+
+	var terminated bool
+
+	handler := func(_ context.Context, event uint8, data interface{}) {
+		if event != EventProcessTerminated {
+			return
+		}
+		for _, process := range data.([]*Process) {
+			if process.Name == name {
+				terminated = true
+			}
+		}
+	}
+	monitor, err := NewMonitor(logger.Test, handler)
+	require.NoError(t, err)
+
+	// wait first auto refresh
+	time.Sleep(2 * defaultRefreshInterval)
+
+	// terminate process
+	err = cmd.Process.Kill()
+	require.NoError(t, err)
+	err = cmd.Process.Release()
+	require.NoError(t, err)
+
+	// wait refresh
+	time.Sleep(2 * defaultRefreshInterval)
+
+	monitor.Close()
+
+	testsuite.IsDestroyed(t, monitor)
+
+	require.True(t, terminated, "not find expected terminated process")
 }
