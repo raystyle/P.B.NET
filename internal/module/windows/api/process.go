ca8b2c01162651147168edb1f410d4b36e333d47
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-08-29 13:09:55 +0800

    module/windows/api/process.go: add OpenProcess(), NTQueryInformationProcess() and ReadProcessMemory().

--- internal/module/windows/api/process.go
+++ internal/module/windows/api/process.go
@@ -10,7 +10,11 @@ import (
 )
 
 var (
-	sizeofProcessEntry32 = uint32(unsafe.Sizeof(windows.ProcessEntry32{}))
+	modNTDLL    = windows.NewLazySystemDLL("ntdll.dll")
+	modKernel32 = windows.NewLazySystemDLL("kernel32.dll")
+
+	procNTQueryInformationProcess = modNTDLL.NewProc("NtQueryInformationProcess")
+	procReadProcessMemory         = modKernel32.NewProc("ReadProcessMemory")
 )
 
 // ProcessBasicInfo contains process basic information.
@@ -32,7 +36,7 @@ func GetProcessList() ([]*ProcessBasicInfo, error) {
 	defer func() { _ = windows.Close(snapshot) }()
 	processes := make([]*ProcessBasicInfo, 0, 64)
 	processEntry := &windows.ProcessEntry32{
-		Size: sizeofProcessEntry32,
+		Size: uint32(unsafe.Sizeof(windows.ProcessEntry32{})),
 	}
 	err = windows.Process32First(snapshot, processEntry)
 	if err != nil {
@@ -74,4 +78,114 @@ func GetProcessIDByName(n string) ([]uint32, error) {
 		return nil, newAPIErrorf(name, nil, "%q is not found", n)
 	}
 	return pid, nil
+}
+
+// OpenProcess is used to open process by PID and return process handle.
+func OpenProcess(desiredAccess uint32, inheritHandle bool, pid uint32) (windows.Handle, error) {
+	const name = "OpenProcess"
+	handle, err := windows.OpenProcess(desiredAccess, inheritHandle, pid)
+	if err != nil {
+		return 0, newAPIError(name, "failed to open process", err)
+	}
+	return handle, nil
+}
+
+// information class about NTQueryInformationProcess.
+const (
+	InfoClassProcessBasicInformation     uint8 = 0
+	InfoClassProcessDebugPort            uint8 = 7
+	InfoClassProcessWow64Information     uint8 = 26
+	InfoClassProcessImageFileName        uint8 = 27
+	InfoClassProcessBreakOnTermination   uint8 = 29
+	InfoClassProcessSubsystemInformation uint8 = 75
+)
+
+// ProcessBasicInformation is an equivalent representation of
+// PROCESS_BASIC_INFORMATION in the Windows API.
+type ProcessBasicInformation struct {
+	ExitStatus                   uintptr
+	PEBBaseAddress               uintptr
+	AffinityMask                 uintptr
+	BasePriority                 uintptr
+	UniqueProcessID              *uint32
+	InheritedFromUniqueProcessID uintptr
+}
+
+// LoadFromBuffer is used to set structure fields form result buffer.
+func (pbi *ProcessBasicInformation) LoadFromBuffer(buf []byte) {
+	*pbi = *(*ProcessBasicInformation)(unsafe.Pointer(&buf[0]))
+}
+
+// NTQueryInformationProcess is used to query process information.
+func NTQueryInformationProcess(handle windows.Handle, infoClass uint8, buf []byte) (uint32, error) {
+	const name = "NTQueryInformationProcess"
+	var size uint32
+	ret, _, err := procNTQueryInformationProcess.Call(
+		uintptr(handle), uintptr(infoClass), uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(len(buf)), uintptr(unsafe.Pointer(&size)),
+	)
+	if ret != windows.NO_ERROR {
+		err := err.(windows.Errno)
+		if err == windows.ERROR_INSUFFICIENT_BUFFER {
+			return size, err
+		}
+		return 0, newAPIError(name, "failed to query process information", err)
+	}
+	return size, nil
+}
+
+// ReadProcessMemory is used to read memory from process.
+func ReadProcessMemory(handle windows.Handle, address uintptr, buf []byte) (int, error) {
+	const name = "ReadProcessMemory"
+	var n uint
+	ret, _, err := procReadProcessMemory.Call(
+		uintptr(handle), address, uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(uint(len(buf))), uintptr(unsafe.Pointer(&n)),
+	)
+	if ret != 1 {
+		return 0, newAPIError(name, "failed to read process memory", err)
+	}
+
+	return int(n), nil
+}
+
+// reference:
+// https://docs.microsoft.com/en-us/windows/win32/api/winternl/ns-winternl-peb
+
+// PEB is the process environment block.
+type PEB struct {
+	InheritedAddressSpace    bool
+	ReadImageFileExecOptions bool
+	BeingDebugged            bool
+	Spare                    bool
+	Mutant                   uintptr
+	ImageBaseAddress         uintptr
+	LoaderData               *PEBLDRData
+	ProcessParameters        uintptr
+}
+
+// PEBLDRData is
+type PEBLDRData struct {
+	Length      uint32
+	Initialized bool
+	SsHandle    uintptr
+	// InMemoryOrderModuleList
+}
+
+// LDRDataTableEntry is
+type LDRDataTableEntry struct {
+	InLoadOrderLinks           *LDRDataTableEntry
+	InMemoryOrderLinks         *LDRDataTableEntry
+	InInitializationOrderLinks *LDRDataTableEntry
+}
+
+// Reserved1 [16]byte
+//	Reserved2 [10]uintptr
+//	ImagePathName
+//	CommandLine
+
+// LSAUnicodeString is
+type LSAUnicodeString struct {
+	Length        uint16
+	MaximumLength uint16
 }
