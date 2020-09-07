e8a0372557ec427420cc2ec45afb1e4de9934d76
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-09-07 20:42:38 +0800

    code/module/windows/privilege: add generateTestRtlEnableDisable() for generate tests.

--- script/code/module/windows/privilege/ntdll.go
+++ script/code/module/windows/privilege/ntdll.go
@@ -2,12 +2,17 @@ package privilege
 
 import (
 	"fmt"
+	"os"
 	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/stretchr/testify/require"
 )
 
 func generateRtlEnableDisable(privilege, comment string) {
 	// <p0> without "SE"
-	const template = `
+	const tpl = `
 // RtlEnable<p0> is used to enable <c> privilege that call RtlAdjustPrivilege.
 func RtlEnable<p0>() (bool, error) {
 	return RtlAdjustPrivilege(<p>, true, false)
@@ -18,8 +23,55 @@ func RtlDisable<p0>() (bool, error) {
 	return RtlAdjustPrivilege(<p>, false, false)
 }
 `
-	src := strings.ReplaceAll(template, "<p0>", privilege[2:])
+	src := strings.ReplaceAll(tpl, "<p0>", privilege[2:])
 	src = strings.ReplaceAll(src, "<c>", comment)
 	src = strings.ReplaceAll(src, "<p>", privilege)
 	fmt.Print(src)
+}
+
+func generateTestRtlEnableDisable(t *testing.T, privilege string) {
+	const src = `
+func TestRtlEnable{{.P0}}(t *testing.T) {
+	if !testIsElevated() {
+		return
+	}
+	previous, err := RtlEnable{{.P0}}()
+	require.NoError(t, err)
+	{{if .First}}require.True(t, previous){{else}}require.False(t, previous){{end}}
+	
+	testRestorePrivilege(t, {{.P}}, previous, true)
+}
+
+func TestRtlDisable{{.P0}}(t *testing.T) {
+	if !testIsElevated() {
+		return
+	}
+	first, err := RtlEnable{{.P0}}()
+	require.NoError(t, err)
+	{{if .First}}require.True(t, first){{else}}require.False(t, first){{end}}
+
+	previous, err := RtlDisable{{.P0}}()
+	require.NoError(t, err)
+	require.True(t, previous)
+	
+	testRestorePrivilege(t, {{.P}}, first, false)
+}
+`
+	type p struct {
+		P0    string // privilege name without "SE"
+		P     string // full privilege name
+		First bool   // special
+	}
+	param := p{
+		P0: privilege[2:],
+		P:  privilege,
+	}
+	if privilege == "SEDebug" {
+		param.First = true
+	}
+	tpl := template.New("execute")
+	_, err := tpl.Parse(src)
+	require.NoError(t, err)
+	err = tpl.Execute(os.Stdout, &param)
+	require.NoError(t, err)
 }
--- script/code/module/windows/privilege/ntdll_test.go
+++ script/code/module/windows/privilege/ntdll_test.go
@@ -23,3 +23,19 @@ func TestGenerateRtlEnableDisable(t *testing.T) {
 		generateRtlEnableDisable(item.privilege, item.comment)
 	}
 }
+
+func TestGenerateTestRtlEnableDisable(t *testing.T) {
+	for _, privilege := range []string{
+		"SESecurity",
+		"SELoadDriver",
+		"SESystemTime",
+		"SESystemProf",
+		"SEBackup",
+		"SEShutdown",
+		"SEDebug",
+		"SESystemEnv",
+		"SERemoteShutdown",
+	} {
+		generateTestRtlEnableDisable(t, privilege)
+	}
+}
