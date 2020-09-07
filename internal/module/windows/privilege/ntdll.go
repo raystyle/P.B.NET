7d57c006cf806e9fa99727217c30ca6151e056f5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-09-07 20:02:25 +0800

    module/windows/privilege/ntdll: add more privilege and function about enable,disable

--- internal/module/windows/privilege/ntdll.go
+++ internal/module/windows/privilege/ntdll.go
@@ -17,12 +17,18 @@ var (
 	procRtlAdjustPrivilege = modNTDLL.NewProc("RtlAdjustPrivilege")
 )
 
+// about privilege id
 const (
-	seLoadDriver uint32 = 20
-	seSystemTime uint32 = 20
-	seBackup     uint32 = 20
-	seShutdown   uint32 = 20
-	seDebug      uint32 = 20
+	SECreateToken    uint32 = 2
+	SESecurity       uint32 = 8
+	SELoadDriver     uint32 = 10
+	SESystemTime     uint32 = 12
+	SESystemProf     uint32 = 13
+	SEBackup         uint32 = 17
+	SEShutdown       uint32 = 19
+	SEDebug          uint32 = 20
+	SESystemEnv      uint32 = 22
+	SERemoteShutdown uint32 = 24
 )
 
 // RtlAdjustPrivilege is used to adjust privilege with procRtlAdjustPrivilege.
@@ -45,7 +51,102 @@ func RtlAdjustPrivilege(id uint32, enable, currentThread bool) (bool, error) {
 	return previous, nil
 }
 
+// RtlEnableCreateToken is used to enable create token privilege that call RtlAdjustPrivilege.
+func RtlEnableCreateToken() (bool, error) {
+	return RtlAdjustPrivilege(SECreateToken, true, false)
+}
+
+// RtlDisableCreateToken is used to disable create token privilege that call RtlAdjustPrivilege.
+func RtlDisableCreateToken() (bool, error) {
+	return RtlAdjustPrivilege(SECreateToken, false, false)
+}
+
+// RtlEnableSecurity is used to enable security privilege that call RtlAdjustPrivilege.
+func RtlEnableSecurity() (bool, error) {
+	return RtlAdjustPrivilege(SESecurity, true, false)
+}
+
+// RtlDisableSecurity is used to disable security privilege that call RtlAdjustPrivilege.
+func RtlDisableSecurity() (bool, error) {
+	return RtlAdjustPrivilege(SESecurity, false, false)
+}
+
+// RtlEnableLoadDriver is used to enable load driver privilege that call RtlAdjustPrivilege.
+func RtlEnableLoadDriver() (bool, error) {
+	return RtlAdjustPrivilege(SELoadDriver, true, false)
+}
+
+// RtlDisableLoadDriver is used to disable load driver privilege that call RtlAdjustPrivilege.
+func RtlDisableLoadDriver() (bool, error) {
+	return RtlAdjustPrivilege(SELoadDriver, false, false)
+}
+
+// RtlEnableSystemTime is used to enable system time privilege that call RtlAdjustPrivilege.
+func RtlEnableSystemTime() (bool, error) {
+	return RtlAdjustPrivilege(SESystemTime, true, false)
+}
+
+// RtlDisableSystemTime is used to disable system time privilege that call RtlAdjustPrivilege.
+func RtlDisableSystemTime() (bool, error) {
+	return RtlAdjustPrivilege(SESystemTime, false, false)
+}
+
+// RtlEnableSystemProf is used to enable system profile privilege that call RtlAdjustPrivilege.
+func RtlEnableSystemProf() (bool, error) {
+	return RtlAdjustPrivilege(SESystemProf, true, false)
+}
+
+// RtlDisableSystemProf is used to disable system profile privilege that call RtlAdjustPrivilege.
+func RtlDisableSystemProf() (bool, error) {
+	return RtlAdjustPrivilege(SESystemProf, false, false)
+}
+
+// RtlEnableBackup is used to enable backup privilege that call RtlAdjustPrivilege.
+func RtlEnableBackup() (bool, error) {
+	return RtlAdjustPrivilege(SEBackup, true, false)
+}
+
+// RtlDisableBackup is used to disable backup privilege that call RtlAdjustPrivilege.
+func RtlDisableBackup() (bool, error) {
+	return RtlAdjustPrivilege(SEBackup, false, false)
+}
+
+// RtlEnableShutdown is used to enable shutdown privilege that call RtlAdjustPrivilege.
+func RtlEnableShutdown() (bool, error) {
+	return RtlAdjustPrivilege(SEShutdown, true, false)
+}
+
+// RtlDisableShutdown is used to disable shutdown privilege that call RtlAdjustPrivilege.
+func RtlDisableShutdown() (bool, error) {
+	return RtlAdjustPrivilege(SEShutdown, false, false)
+}
+
 // RtlEnableDebug is used to enable debug privilege that call RtlAdjustPrivilege.
 func RtlEnableDebug() (bool, error) {
-	return RtlAdjustPrivilege(seDebug, true, false)
+	return RtlAdjustPrivilege(SEDebug, true, false)
+}
+
+// RtlDisableDebug is used to disable debug privilege that call RtlAdjustPrivilege.
+func RtlDisableDebug() (bool, error) {
+	return RtlAdjustPrivilege(SEDebug, false, false)
+}
+
+// RtlEnableSystemEnv is used to enable system environment privilege that call RtlAdjustPrivilege.
+func RtlEnableSystemEnv() (bool, error) {
+	return RtlAdjustPrivilege(SESystemEnv, true, false)
+}
+
+// RtlDisableSystemEnv is used to disable system environment privilege that call RtlAdjustPrivilege.
+func RtlDisableSystemEnv() (bool, error) {
+	return RtlAdjustPrivilege(SESystemEnv, false, false)
+}
+
+// RtlEnableRemoteShutdown is used to enable remote shutdown privilege that call RtlAdjustPrivilege.
+func RtlEnableRemoteShutdown() (bool, error) {
+	return RtlAdjustPrivilege(SERemoteShutdown, true, false)
+}
+
+// RtlDisableRemoteShutdown is used to disable remote shutdown privilege that call RtlAdjustPrivilege.
+func RtlDisableRemoteShutdown() (bool, error) {
+	return RtlAdjustPrivilege(SERemoteShutdown, false, false)
 }
--- internal/module/windows/privilege/ntdll_test.go
+++ internal/module/windows/privilege/ntdll_test.go
@@ -7,8 +7,23 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+func TestRtlAdjustPrivilege(t *testing.T) {
+	_, err := RtlAdjustPrivilege(0, true, true)
+	require.Error(t, err)
+}
+
 func testIsElevated() bool {
 	return windows.GetCurrentProcessToken().IsElevated()
+}
+
+func testResetPrivilege(t *testing.T, previous bool, id uint32) {
+	if previous {
+		_, err := RtlAdjustPrivilege(id, true, false)
+		require.NoError(t, err)
+	} else {
+		_, err := RtlAdjustPrivilege(id, false, false)
+		require.NoError(t, err)
+	}
 }
 
 func TestRtlEnableDebug(t *testing.T) {
@@ -17,5 +32,22 @@ func TestRtlEnableDebug(t *testing.T) {
 	}
 	previous, err := RtlEnableDebug()
 	require.NoError(t, err)
-	require.False(t, previous)
+	require.True(t, previous)
+
+	testResetPrivilege(t, previous, SEDebug)
+}
+
+func TestRtlDisableDebug(t *testing.T) {
+	if !testIsElevated() {
+		return
+	}
+	first, err := RtlEnableDebug()
+	require.NoError(t, err)
+	require.True(t, first)
+
+	previous, err := RtlDisableDebug()
+	require.NoError(t, err)
+	require.True(t, previous)
+
+	testResetPrivilege(t, first, SEDebug)
 }
