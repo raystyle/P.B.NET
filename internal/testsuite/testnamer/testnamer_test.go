7be6faf5b328e0d2a6d94181d363e7b05ceac9b7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-09-10 19:19:58 +0800

    testsuite/testnamer/testnamer_test.go: add three tests about new namer.

--- internal/testsuite/testnamer/testnamer_test.go
+++ internal/testsuite/testnamer/testnamer_test.go
@@ -1,1 +1,46 @@
 package testnamer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/internal/testsuite"
+)
+
+func TestNewNamer(t *testing.T) {
+	namer := NewNamer()
+
+	err := namer.Load(nil)
+	require.NoError(t, err)
+
+	for i := 0; i < 10; i++ {
+		word, err := namer.Generate(nil)
+		require.NoError(t, err)
+		t.Log(word)
+	}
+
+	testsuite.IsDestroyed(t, namer)
+}
+
+func TestNewNamerWithLoadFailed(t *testing.T) {
+	namer := NewNamerWithLoadFailed()
+
+	err := namer.Load(nil)
+	require.Error(t, err)
+
+	testsuite.IsDestroyed(t, namer)
+}
+
+func TestNewNamerWithGenerateFailed(t *testing.T) {
+	namer := NewNamerWithGenerateFailed()
+
+	err := namer.Load(nil)
+	require.NoError(t, err)
+
+	word, err := namer.Generate(nil)
+	require.Error(t, err)
+	require.Zero(t, word)
+
+	testsuite.IsDestroyed(t, namer)
+}
