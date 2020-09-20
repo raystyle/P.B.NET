fed143cf1a4e3f4701e7257bfd5d2bed22d11041
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-09-20 14:09:45 +0800

    code/anko/parser/parser_test.go: finish TestGenerateParser() for generate anko parser.

--- script/code/anko/parser/parser_test.go
+++ script/code/anko/parser/parser_test.go
@@ -1,1 +1,53 @@
 package parser
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"project/script/internal/exec"
+)
+
+func TestGenerateParser(t *testing.T) {
+	// change current directory to anko/parser
+	err := os.Chdir("../../../../external/anko/parser")
+	require.NoError(t, err)
+	// generate parser.go
+	output, _, err := exec.Run("goyacc", "-o", "parser.go", "parser.go.y")
+	require.NoError(t, err)
+	fmt.Println(output)
+	// remove output file
+	err = os.Remove("y.output")
+	require.NoError(t, err)
+	// format generated code
+	output, code, err := exec.Run("gofmt", "-s", "-w", "parser.go")
+	require.NoError(t, err)
+	require.Equal(t, 0, code, output)
+	// add directive comment about development tools.
+	parser, err := ioutil.ReadFile("parser.go")
+	require.NoError(t, err)
+	// replace code
+	str := string(parser)
+	for _, item := range [...]*struct {
+		target      string
+		replacement string
+	}{
+		{
+			"func yyErrorMessage",
+			"//gocyclo:ignore\nfunc yyErrorMessage",
+		},
+		{
+			"func (yyrcvr *yyParserImpl) Parse",
+			"//gocyclo:ignore\nfunc (yyrcvr *yyParserImpl) Parse",
+		},
+	} {
+		str = strings.ReplaceAll(str, item.target, item.replacement)
+	}
+	// save code
+	err = ioutil.WriteFile("parser.go", []byte(str), 0600)
+	require.NoError(t, err)
+}
