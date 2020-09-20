a0a99429731ebda352d0cfa1b32c8aedcc892411
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-09-20 10:51:04 +0800

    internal/config/config.go: add Load() to load configuration file, merge print wd.

--- script/internal/config/config.go
+++ script/internal/config/config.go
@@ -1,11 +1,21 @@
 package config
 
 import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 	"os/exec"
 	"strings"
+
+	"project/internal/logger"
+	"project/internal/patch/json"
+
+	"project/script/internal/log"
 )
 
-// Config contains configuration about install, build, test and race.
+// Config contains configuration about install, build, develop, test and race.
 type Config struct {
 	Common struct {
 		GoRootLatest  string `json:"go_root_latest"`
@@ -29,6 +39,54 @@ type Config struct {
 
 	Race struct {
 	} `json:"race"`
+}
+
+// Load is used to load configuration file.
+func Load(path string, config *Config) bool {
+	// print current directory
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Println(logger.Error, err)
+		return false
+	}
+	log.Println(logger.Info, "current directory:", dir)
+	// load config file
+	data, err := ioutil.ReadFile(path) // #nosec
+	if err != nil {
+		log.Println(logger.Error, "failed to load config file:", err)
+		return false
+	}
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		log.Println(logger.Error, "failed to load config:", err)
+		return false
+	}
+	log.Println(logger.Info, "load configuration file successfully")
+	log.Println(logger.Info, "Go latest root path:", config.Common.GoRootLatest)
+	log.Println(logger.Info, "Go 1.10.8 root path:", config.Common.GoRoot1108)
+	// set proxy and TLS configuration
+	tr := http.DefaultTransport.(*http.Transport)
+	proxyURL := config.Common.ProxyURL
+	if proxyURL != "" {
+		URL, err := url.Parse(proxyURL)
+		if err != nil {
+			log.Println(logger.Error, "invalid proxy url:", err)
+			return false
+		}
+		tr.Proxy = http.ProxyURL(URL)
+		// set os environment for build
+		err = os.Setenv("HTTP_PROXY", proxyURL)
+		if err != nil {
+			log.Println(logger.Error, "failed to set os env:", err)
+			return false
+		}
+		log.Println(logger.Info, "set proxy url:", proxyURL)
+	}
+	if config.Common.SkipTLSVerify {
+		tr.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // #nosec
+		log.Println(logger.Warning, "skip tls verify")
+	}
+	return true
 }
 
 // GoRoot is used to get the go root path.
