8eb7be0dcd970cacf2f10424980da14a6dc88cc7
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-09-24 19:39:26 +0800

    anko/vm/expr.go: update runInfoStruct.invokeExpr(), add MapExpr(), improve code style.

--- external/anko/vm/expr.go
+++ external/anko/vm/expr.go
@@ -76,6 +76,83 @@ func (runInfo *runInfoStruct) invokeExpr() {
 		}
 		runInfo.rv = slice
 
+		// MapExpr
+	case *ast.MapExpr:
+		if expr.TypeData == nil {
+			var i int
+			var key reflect.Value
+			m := make(map[interface{}]interface{}, len(expr.Keys))
+			for i, runInfo.expr = range expr.Keys {
+				runInfo.invokeExpr()
+				if runInfo.err != nil {
+					return
+				}
+				key = runInfo.rv
+
+				runInfo.expr = expr.Values[i]
+				runInfo.invokeExpr()
+				if runInfo.err != nil {
+					return
+				}
+
+				m[key.Interface()] = runInfo.rv.Interface()
+			}
+			runInfo.rv = reflect.ValueOf(m)
+			return
+		}
+
+		t := makeType(runInfo, expr.TypeData)
+		if runInfo.err != nil {
+			runInfo.rv = nilValue
+			return
+		}
+		if t == nil {
+			runInfo.err = newStringError(expr, "cannot make type nil")
+			runInfo.rv = nilValue
+			return
+		}
+
+		runInfo.rv, runInfo.err = makeValue(t)
+		if runInfo.err != nil {
+			runInfo.rv = nilValue
+			return
+		}
+
+		var i int
+		var key reflect.Value
+		m := runInfo.rv
+		keyType := t.Key()
+		valueType := t.Elem()
+		for i, runInfo.expr = range expr.Keys {
+			runInfo.invokeExpr()
+			if runInfo.err != nil {
+				return
+			}
+			key, runInfo.err = convertReflectValueToType(runInfo.rv, keyType)
+			if runInfo.err != nil {
+				const format = "cannot use type %s as type %s as map key"
+				runInfo.err = newStringError(expr, fmt.Sprintf(format, key.Type(), keyType))
+				runInfo.rv = nilValue
+				return
+			}
+
+			runInfo.expr = expr.Values[i]
+			runInfo.invokeExpr()
+			if runInfo.err != nil {
+				return
+			}
+			runInfo.rv, runInfo.err = convertReflectValueToType(runInfo.rv, valueType)
+			if runInfo.err != nil {
+				const format = "cannot use type %s as type %s as map value"
+				runInfo.err = newStringError(expr, fmt.Sprintf(format, runInfo.rv.Type(), valueType))
+				runInfo.rv = nilValue
+				return
+			}
+
+			m.SetMapIndex(key, runInfo.rv)
+		}
+		runInfo.rv = m
+
 	default:
 		runInfo.err = newStringError(expr, "unknown expression")
 		runInfo.rv = nilValue
