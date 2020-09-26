bdd9c2ad0a6631fbb594670ba9d02b8e8ea8de80
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-09-26 16:14:02 +0800

    anko/vm/operator.go: update runInfoStruct.invokeOperator(), add BinaryOperator.

--- external/anko/vm/operator.go
+++ external/anko/vm/operator.go
@@ -1,5 +1,98 @@
 package vm
 
-func (runInfo *runInfoStruct) invokeOperator() {
+import (
+	"reflect"
 
+	"project/external/anko/ast"
+)
+
+func (runInfo *runInfoStruct) invokeOperator() {
+	switch operator := runInfo.operator.(type) {
+
+	// BinaryOperator
+	case *ast.BinaryOperator:
+		runInfo.expr = operator.LHS
+		runInfo.invokeExpr()
+		if runInfo.err != nil {
+			return
+		}
+		if runInfo.rv.Kind() == reflect.Interface && !runInfo.rv.IsNil() {
+			runInfo.rv = runInfo.rv.Elem()
+		}
+
+		switch operator.Operator {
+		case "||":
+			if toBool(runInfo.rv) {
+				runInfo.rv = trueValue
+				return
+			}
+		case "&&":
+			if !toBool(runInfo.rv) {
+				runInfo.rv = falseValue
+				return
+			}
+		default:
+			runInfo.err = newStringError(operator, "unknown operator")
+			runInfo.rv = nilValue
+			return
+		}
+
+		runInfo.expr = operator.RHS
+		runInfo.invokeExpr()
+		if runInfo.err != nil {
+			return
+		}
+		if runInfo.rv.Kind() == reflect.Interface && !runInfo.rv.IsNil() {
+			runInfo.rv = runInfo.rv.Elem()
+		}
+
+		if toBool(runInfo.rv) {
+			runInfo.rv = trueValue
+		} else {
+			runInfo.rv = falseValue
+		}
+
+	// ComparisonOperator
+	case *ast.ComparisonOperator:
+		runInfo.expr = operator.LHS
+		runInfo.invokeExpr()
+		if runInfo.err != nil {
+			return
+		}
+		if runInfo.rv.Kind() == reflect.Interface && !runInfo.rv.IsNil() {
+			runInfo.rv = runInfo.rv.Elem()
+		}
+		lhsV := runInfo.rv
+
+		runInfo.expr = operator.RHS
+		runInfo.invokeExpr()
+		if runInfo.err != nil {
+			return
+		}
+		if runInfo.rv.Kind() == reflect.Interface && !runInfo.rv.IsNil() {
+			runInfo.rv = runInfo.rv.Elem()
+		}
+
+		switch operator.Operator {
+		case "==":
+			runInfo.rv = reflect.ValueOf(equal(lhsV, runInfo.rv))
+		case "!=":
+			runInfo.rv = reflect.ValueOf(!equal(lhsV, runInfo.rv))
+		case "<":
+			runInfo.rv = reflect.ValueOf(toFloat64(lhsV) < toFloat64(runInfo.rv))
+		case "<=":
+			runInfo.rv = reflect.ValueOf(toFloat64(lhsV) <= toFloat64(runInfo.rv))
+		case ">":
+			runInfo.rv = reflect.ValueOf(toFloat64(lhsV) > toFloat64(runInfo.rv))
+		case ">=":
+			runInfo.rv = reflect.ValueOf(toFloat64(lhsV) >= toFloat64(runInfo.rv))
+		default:
+			runInfo.err = newStringError(operator, "unknown operator")
+			runInfo.rv = nilValue
+		}
+
+	default:
+		runInfo.err = newStringError(operator, "unknown operator")
+		runInfo.rv = nilValue
+	}
 }
