fdd28e27426262714c3d9841fef04f0390a94d0f
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-10-01 09:39:03 +0800

    anko/vm/containers_test.go: add TestStructs(), improve code style.

--- external/anko/vm/containers_test.go
+++ external/anko/vm/containers_test.go
@@ -1341,8 +1341,6 @@ func TestMakeSlicesAndMaps(t *testing.T) {
 }
 
 func TestMakeSlicesData(t *testing.T) {
-	t.Parallel()
-
 	stmts, err := parser.ParseSrc("make(slice)")
 	if err != nil {
 		t.Errorf("ParseSrc error - received %v - expected: %v", err, nil)
@@ -1407,8 +1405,6 @@ func TestMakeSlicesData(t *testing.T) {
 }
 
 func TestMakeMapsData(t *testing.T) {
-	t.Parallel()
-
 	stmts, err := parser.ParseSrc("make(aMap)")
 	if err != nil {
 		t.Errorf("ParseSrc error - received %v - expected: %v", err, nil)
@@ -1455,4 +1451,301 @@ func TestMakeMapsData(t *testing.T) {
 	if b.Get("b") != "b" {
 		t.Errorf("Get value - received %#v - expected: %#v", b.Get("b"), "b")
 	}
+}
+
+func TestStructs(t *testing.T) {
+	t.Parallel()
+
+	tests := []Test{
+		{Script: `a["B"]`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{}},
+			RunError: fmt.Errorf("type struct does not support index operation"),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{}}},
+		{Script: `a.C`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{}},
+			RunError: fmt.Errorf("no member named 'C' for struct"),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{}}},
+
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: nil, B: nil}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: nil, B: nil}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: int32(1), B: int32(2)}},
+			RunOutput: int32(2),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: int32(1), B: int32(2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: int64(1), B: int64(2)}},
+			RunOutput: int64(2),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: float32(1.1), B: float32(2.2)}},
+			RunOutput: float32(2.2),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: float32(1.1), B: float32(2.2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: 1.1, B: 2.2}},
+			RunOutput: 2.2,
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: 1.1, B: 2.2}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A interface{}
+			B interface{}
+		}{A: "a", B: "b"}},
+			RunOutput: "b",
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: "a", B: "b"}}},
+
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A bool
+			B bool
+		}{}},
+			RunOutput: false,
+			Output: map[string]interface{}{"a": struct {
+				A bool
+				B bool
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A int32
+			B int32
+		}{}},
+			RunOutput: int32(0),
+			Output: map[string]interface{}{"a": struct {
+				A int32
+				B int32
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A int64
+			B int64
+		}{}},
+			RunOutput: int64(0),
+			Output: map[string]interface{}{"a": struct {
+				A int64
+				B int64
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A float32
+			B float32
+		}{}},
+			RunOutput: float32(0),
+			Output: map[string]interface{}{"a": struct {
+				A float32
+				B float32
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A float64
+			B float64
+		}{}},
+			RunOutput: float64(0),
+			Output: map[string]interface{}{"a": struct {
+				A float64
+				B float64
+			}{}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A string
+			B string
+		}{}},
+			RunOutput: "",
+			Output: map[string]interface{}{"a": struct {
+				A string
+				B string
+			}{}}},
+
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A bool
+			B bool
+		}{A: true, B: true}},
+			RunOutput: true,
+			Output: map[string]interface{}{"a": struct {
+				A bool
+				B bool
+			}{A: true, B: true}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A int32
+			B int32
+		}{A: int32(1), B: int32(2)}},
+			RunOutput: int32(2),
+			Output: map[string]interface{}{"a": struct {
+				A int32
+				B int32
+			}{A: int32(1), B: int32(2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A int64
+			B int64
+		}{A: int64(1), B: int64(2)}},
+			RunOutput: int64(2),
+			Output: map[string]interface{}{"a": struct {
+				A int64
+				B int64
+			}{A: int64(1), B: int64(2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A float32
+			B float32
+		}{A: float32(1.1), B: float32(2.2)}},
+			RunOutput: float32(2.2),
+			Output: map[string]interface{}{"a": struct {
+				A float32
+				B float32
+			}{A: float32(1.1), B: float32(2.2)}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A float64
+			B float64
+		}{A: 1.1, B: 2.2}},
+			RunOutput: 2.2,
+			Output: map[string]interface{}{"a": struct {
+				A float64
+				B float64
+			}{A: 1.1, B: 2.2}}},
+		{Script: `a.B`, Input: map[string]interface{}{"a": struct {
+			A string
+			B string
+		}{A: "a", B: "b"}},
+			RunOutput: "b",
+			Output: map[string]interface{}{"a": struct {
+				A string
+				B string
+			}{A: "a", B: "b"}}},
+
+		{Script: `a.C = 3`, Input: map[string]interface{}{
+			"a": struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(2)},
+		},
+			RunError: fmt.Errorf("no member named 'C' for struct"),
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(2)}}},
+
+		{Script: `a.B = 3`, Input: map[string]interface{}{"a": &struct {
+			A interface{}
+			B interface{}
+		}{A: int64(1), B: int64(2)}},
+			RunOutput: int64(3),
+			Output: map[string]interface{}{"a": &struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(3)}}},
+
+		{Script: `a.B = 3; a = *a`, Input: map[string]interface{}{"a": &struct {
+			A interface{}
+			B interface{}
+		}{A: int64(1), B: int64(2)}},
+			RunOutput: struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(3)},
+			Output: map[string]interface{}{"a": struct {
+				A interface{}
+				B interface{}
+			}{A: int64(1), B: int64(3)}}},
+
+		// nil tests
+		{Script: `a.A = nil; a.B = nil; a.C = nil;`, Input: map[string]interface{}{"a": &struct {
+			A *bool
+			B *int32
+			C *int64
+		}{A: new(bool), B: new(int32), C: new(int64)}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A *bool
+				B *int32
+				C *int64
+			}{A: nil, B: nil, C: nil}}},
+		{Script: `a.A = nil; a.B = nil; a.C = nil;`, Input: map[string]interface{}{"a": &struct {
+			A *float32
+			B *float64
+			C *string
+		}{A: new(float32), B: new(float64), C: new(string)}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A *float32
+				B *float64
+				C *string
+			}{A: nil, B: nil, C: nil}}},
+		{Script: `a.A = nil; a.B = nil; a.C = nil;`, Input: map[string]interface{}{"a": &struct {
+			A interface{}
+			B []interface{}
+			C [][]interface{}
+		}{A: "a", B: []interface{}{"b"}, C: [][]interface{}{{"c"}}}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A interface{}
+				B []interface{}
+				C [][]interface{}
+			}{A: nil, B: nil, C: nil}}},
+		{Script: `a.A = nil; a.B = nil; a.C = nil; a.D = nil;`, Input: map[string]interface{}{"a": &struct {
+			A map[string]string
+			B map[string]interface{}
+			C map[interface{}]string
+			D map[interface{}]interface{}
+		}{A: map[string]string{"a": "a"}, B: map[string]interface{}{"b": "b"}, C: map[interface{}]string{"c": "c"}, D: map[interface{}]interface{}{"d": "d"}}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A map[string]string
+				B map[string]interface{}
+				C map[interface{}]string
+				D map[interface{}]interface{}
+			}{A: nil, B: nil, C: nil, D: nil}}},
+		{Script: `a.A.AA = nil;`, Input: map[string]interface{}{"a": &struct {
+			A struct{ AA *int64 }
+		}{A: struct{ AA *int64 }{AA: new(int64)}}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A struct{ AA *int64 }
+			}{A: struct{ AA *int64 }{AA: nil}}}},
+		{Script: `a.A = nil;`, Input: map[string]interface{}{"a": &struct {
+			A *struct{ AA *int64 }
+		}{A: &struct{ AA *int64 }{AA: new(int64)}}},
+			RunOutput: nil,
+			Output: map[string]interface{}{"a": &struct {
+				A *struct{ AA *int64 }
+			}{A: nil}}},
+	}
+	runTests(t, tests, nil, &Options{Debug: true})
 }
