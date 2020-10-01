4bd87bce0487dce1c19bfaf46a9fb7d6485a57d4
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-10-01 10:55:13 +0800

    anko/vm/example_containers_test.go: add Example_vmStructs() and Example_vmModules().

--- external/anko/vm/example_containers_test.go
+++ external/anko/vm/example_containers_test.go
@@ -154,3 +154,106 @@ println(a["b"])
 	// 1
 	// 2
 }
+
+func Example_vmStructs() {
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("define error: %v\n", err)
+	}
+
+	script := `
+a = make(struct {
+	A int64,
+	B float64
+})
+println(a)
+
+a.A = 1
+println(a)
+println(a.A)
+
+a.B = 2.5
+println(a)
+println(a.B)
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("execute error: %v\n", err)
+	}
+
+	// output:
+	// {0 0}
+	// {1 0}
+	// 1
+	// {1 2.5}
+	// 2.5
+}
+
+func Example_vmModules() {
+
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("define error: %v\n", err)
+	}
+
+	script := `
+module rectangle {
+	_length = 1
+	_width = 1
+
+	func setLength (length) {
+		if length <= 0 {
+			return
+		}
+		_length = length
+	}
+
+	func setWidth (width) {
+		if width <= 0 {
+			return
+		}
+		_width = width
+	}
+
+	func area () {
+		return _length * _width
+	}
+
+	func perimeter () {
+		return 2 * (_length + _width)
+	}
+ }
+
+rectangle1 = rectangle
+
+rectangle1.setLength(4)
+rectangle1.setWidth(5)
+
+println(rectangle1.area())
+println(rectangle1.perimeter())
+
+rectangle2 = rectangle
+
+rectangle2.setLength(2)
+rectangle2.setWidth(4)
+
+println(rectangle2.area())
+println(rectangle2.perimeter())
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("execute error: %v\n", err)
+	}
+
+	// output:
+	// 20
+	// 18
+	// 8
+	// 12
+}
