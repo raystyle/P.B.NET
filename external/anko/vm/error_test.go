0cc8471dac32d73f247aabfae72e37945d7215c9
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-10-01 10:16:24 +0800

    anko/vm/error_test.go: add TestNewError() and TestNewStringError().

--- external/anko/vm/error_test.go
+++ external/anko/vm/error_test.go
@@ -1,1 +1,28 @@
 package vm
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestNewError(t *testing.T) {
+	err := newError(nil, nil)
+	if err != nil {
+		t.Fatal("err is not nil")
+	}
+	err = newError(nil, errors.New("foo"))
+	if err == nil {
+		t.Fatal("err is nil")
+	}
+}
+
+func TestNewStringError(t *testing.T) {
+	err := newStringError(nil, "")
+	if err != nil {
+		t.Fatal("err is not nil")
+	}
+	err = newStringError(nil, "foo")
+	if err == nil {
+		t.Fatal("err is nil")
+	}
+}
