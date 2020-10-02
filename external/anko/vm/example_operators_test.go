81ea6d4a1496e78dad39f62d006829e516a92fb9
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-10-02 19:49:55 +0800

    anko/vm/example_operators_test.go: add Example_vmBasicOperators() and Example_vmComparisonOperators().

--- external/anko/vm/example_operators_test.go
+++ external/anko/vm/example_operators_test.go
@@ -1,1 +1,176 @@
-package vm
+package vm_test
+
+import (
+	"fmt"
+	"log"
+
+	"project/external/anko/env"
+	"project/external/anko/vm"
+)
+
+func Example_vmBasicOperators() {
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("define error: %v\n", err)
+	}
+
+	script := `
+a = nil
+println(a)
+a = true
+println(a)
+
+println("")
+
+a = 2 + 1
+println(a)
+a = 2 - 1
+println(a)
+a = 2 * 1
+println(a)
+a = 4 / 2
+println(a)
+
+println("")
+
+a = 1
+a++
+println(a)
+a--
+println(a)
+
+println("")
+
+a = 1
+a += 1
+println(a)
+a -= 1
+println(a)
+a *= 4
+println(a)
+a /= 2
+println(a)
+
+println("")
+
+a = 1 & 3
+println(a)
+a = 1 | 2
+println(a)
+
+println("")
+
+a = 2 << 3
+println(a)
+a = 8 >> 2
+println(a)
+a = 7 % 3
+println(a)
+
+println("")
+
+a = 2 - (-2)
+println(a)
+a = ^2
+println(a)
+a = "a" * 4
+println(a)
+a = !true
+println(a)
+
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("execute error: %v\n", err)
+	}
+
+	// output:
+	// <nil>
+	// true
+	//
+	// 3
+	// 1
+	// 2
+	// 2
+	//
+	// 2
+	// 1
+	//
+	// 2
+	// 1
+	// 4
+	// 2
+	//
+	// 1
+	// 3
+	//
+	// 16
+	// 2
+	// 1
+	//
+	// 4
+	// -3
+	// aaaa
+	// false
+
+}
+
+func Example_vmComparisonOperators() {
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("define error: %v\n", err)
+	}
+
+	script := `
+a = nil == nil
+println(a)
+a = "a" != "a"
+println(a)
+a = 1 == 1.0
+println(a)
+a = !true
+println(a)
+
+println("")
+
+a = 1 < 2
+println(a)
+a = 1 > 3
+println(a)
+a = 2 <= 2
+println(a)
+a = 2 >= 3
+println(a)
+
+println("")
+a = 1 == 2 && 1 == 1
+println(a)
+a = 1 == 2 || 1 == 1
+println(a)
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("execute error: %v\n", err)
+	}
+
+	// output:
+	// true
+	// false
+	// true
+	// false
+	//
+	// true
+	// false
+	// true
+	// false
+	//
+	// false
+	// true
+
+}
