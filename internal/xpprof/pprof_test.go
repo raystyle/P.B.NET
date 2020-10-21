3baf0d48e32068294e1f1899a3c4a207e8bc1d25
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-10-21 20:00:26 +0800

    xpprof/pprof_test.go: add TestHandler_authenticate(), update some tests.

--- internal/xpprof/pprof_test.go
+++ internal/xpprof/pprof_test.go
@@ -1,10 +1,12 @@
 package xpprof
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -62,6 +64,7 @@ func testFetch(t *testing.T, url string, rt http.RoundTripper, server io.Closer)
 
 		testsuite.IsDestroyed(t, server)
 	}()
+
 	client := http.Client{
 		Transport: rt,
 	}
@@ -69,10 +72,15 @@ func testFetch(t *testing.T, url string, rt http.RoundTripper, server io.Closer)
 
 	resp, err := client.Get(url)
 	require.NoError(t, err)
-	defer func() { _ = resp.Body.Close() }()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
 	b, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 	fmt.Println(string(b))
+
+	err = resp.Body.Close()
+	require.NoError(t, err)
 }
 
 func TestHTTPServer(t *testing.T) {
@@ -100,11 +108,98 @@ func TestHTTPSServer(t *testing.T) {
 	t.Log("pprof https address:\n", addresses)
 	t.Log("pprof https info:\n", server.Info())
 
-	URL := fmt.Sprintf("http://admin:123456@%s/debug/pprof/", addresses[0])
+	URL := fmt.Sprintf("https://admin@%s/debug/pprof/", addresses[0])
 	transport := new(http.Transport)
 	var err error
 	transport.TLSClientConfig, err = tlsConfig.Apply()
 	require.NoError(t, err)
 
-	testFetch(t, URL, nil, server)
+	testFetch(t, URL, transport, server)
+}
+
+func TestHandler_authenticate(t *testing.T) {
+	gm := testsuite.MarkGoroutines(t)
+	defer gm.Compare()
+
+	server := testGenerateHTTPServer(t)
+	address := server.Addresses()[0].String()
+
+	client := http.Client{}
+	defer client.CloseIdleConnections()
+
+	t.Run("only username", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "http://"+address, nil)
+		require.NoError(t, err)
+		userInfo := url.User("admin")
+		auth := base64.StdEncoding.EncodeToString([]byte(userInfo.String()))
+		req.Header.Set("Authorization", "Basic "+auth)
+
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+
+		require.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		require.NoError(t, err)
+		err = resp.Body.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("invalid username/password", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "http://"+address, nil)
+		require.NoError(t, err)
+		userInfo := url.UserPassword("admin1", "123")
+		auth := base64.StdEncoding.EncodeToString([]byte(userInfo.String()))
+		req.Header.Set("Authorization", "Basic "+auth)
+
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		require.NoError(t, err)
+		err = resp.Body.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("invalid basic base64 data", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "http://"+address, nil)
+		require.NoError(t, err)
+		req.Header.Set("Authorization", "Basic foo")
+
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		require.NoError(t, err)
+		err = resp.Body.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("no authenticate header", func(t *testing.T) {
+		resp, err := client.Get("http://" + address)
+		require.NoError(t, err)
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		require.NoError(t, err)
+		err = resp.Body.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("unsupported authenticate method", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "http://"+address, nil)
+		require.NoError(t, err)
+		req.Header.Set("Authorization", "method not-support")
+
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		require.NoError(t, err)
+		err = resp.Body.Close()
+		require.NoError(t, err)
+	})
+
+	err := server.Close()
+	require.NoError(t, err)
+
+	testsuite.IsDestroyed(t, server)
 }
