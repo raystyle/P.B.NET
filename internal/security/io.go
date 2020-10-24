314730070a0147766474737ddf971b6f5a814058
For-ACGN <38399410+for-acgn@users.noreply.github.com>
2020-10-24 14:46:36 +0800

    security/io.go: add LimitReadAllWithError() for read all with limited size, add TestLimitReadAllWithError().

--- internal/security/io.go
+++ internal/security/io.go
@@ -1,12 +1,44 @@
 package security
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 )
 
 // LimitReadAll is used to read all with limited size.
+// if out of size it will not return an error.
 func LimitReadAll(r io.Reader, size int64) ([]byte, error) {
 	lr := io.LimitReader(r, size)
 	return ioutil.ReadAll(lr)
+}
+
+type limitedReader struct {
+	r io.Reader // underlying reader
+	n int64     // max bytes remaining
+}
+
+func (l *limitedReader) Read(p []byte) (n int, err error) {
+	if l.n <= 0 {
+		// try to read again for make sure
+		// it can read new data
+		n, err = l.r.Read(p)
+		if err == io.EOF && n == 0 {
+			return 0, io.EOF
+		}
+		return 0, errors.New("limit read all is not finished")
+	}
+	if int64(len(p)) > l.n {
+		p = p[0:l.n]
+	}
+	n, err = l.r.Read(p)
+	l.n -= int64(n)
+	return
+}
+
+// LimitReadAllWithError is used to read all with limited size.
+// if out of size it will not return an error.
+func LimitReadAllWithError(r io.Reader, size int64) ([]byte, error) {
+	lr := limitedReader{r: r, n: size}
+	return ioutil.ReadAll(&lr)
 }
--- internal/security/io_test.go
+++ internal/security/io_test.go
@@ -8,24 +8,47 @@ import (
 )
 
 func TestLimitReadAll(t *testing.T) {
-	t.Run("read = size", func(t *testing.T) {
+	t.Run("limitation = size", func(t *testing.T) {
 		buf := bytes.NewBuffer(make([]byte, 1024))
 		b, err := LimitReadAll(buf, 1024)
 		require.NoError(t, err)
 		require.Equal(t, make([]byte, 1024), b)
 	})
 
-	t.Run("read < size", func(t *testing.T) {
+	t.Run("limitation < size", func(t *testing.T) {
 		buf := bytes.NewBuffer(make([]byte, 1024))
 		b, err := LimitReadAll(buf, 512)
 		require.NoError(t, err)
 		require.Equal(t, make([]byte, 512), b)
 	})
 
-	t.Run("read > size", func(t *testing.T) {
+	t.Run("limitation > size", func(t *testing.T) {
 		buf := bytes.NewBuffer(make([]byte, 1024))
 		b, err := LimitReadAll(buf, 2048)
 		require.NoError(t, err)
 		require.Equal(t, make([]byte, 1024), b)
+	})
+}
+
+func TestLimitReadAllWithError(t *testing.T) {
+	t.Run("limitation = size", func(t *testing.T) {
+		buf := bytes.NewBuffer(make([]byte, 123))
+		b, err := LimitReadAllWithError(buf, 123)
+		require.NoError(t, err)
+		require.Equal(t, make([]byte, 123), b)
+	})
+
+	t.Run("limitation < size", func(t *testing.T) {
+		buf := bytes.NewBuffer(make([]byte, 456))
+		b, err := LimitReadAllWithError(buf, 123)
+		require.Error(t, err)
+		require.Equal(t, make([]byte, 123), b)
+	})
+
+	t.Run("limitation > size", func(t *testing.T) {
+		buf := bytes.NewBuffer(make([]byte, 123))
+		b, err := LimitReadAllWithError(buf, 456)
+		require.NoError(t, err)
+		require.Equal(t, make([]byte, 123), b)
 	})
 }
